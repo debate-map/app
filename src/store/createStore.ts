451633de32268795eff31577347b5c9910474e24
Venryx <venryx@gmail.com>
2017-03-15 08:43:14 -0700

    * Added basic persistence of store state.
    Former-commit-id: cc83b8763892e14e538148db30b0ac69af81351a

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -4,6 +4,7 @@ import {MakeRootReducer} from "./reducers";
 import {createBrowserHistory} from "react-router/node_modules/history";
 import {reduxFirebase, getFirebase} from "react-redux-firebase";
 import {firebase as fbConfig, reduxFirebase as reduxConfig} from "../config";
+import {persistStore, autoRehydrate} from "redux-persist";
 //import {version} from "../../package.json";
 let {version} = require("../../package.json");
 
@@ -39,10 +40,14 @@ export default function(initialState = {}, history) {
 		(compose as any)(
 			applyMiddleware(...middleware),
 			reduxFirebase(fbConfig, reduxConfig),
+			autoRehydrate(),
 			...enhancers
 		)
 	) as any;
 	store.asyncReducers = {};
+
+	// begin periodically persisting the store
+	persistStore(store, {whitelist: ["main"]});
 
 	if (module.hot) {
 		module.hot.accept("./reducers", () => {
