351c31825abe86648d1c36373a6aaecf5facc8f0
Venryx <venryx@gmail.com>
2017-03-19 08:06:54 -0700

    * Positive arguments now start above center, and negative arguments start below center. : )
    Former-commit-id: 4cbcea21ec080f0033f0db11dcb3ba2b1ddb1a73

--- src/routes/@Shared/Maps/MapNodeUI.tsx
+++ src/routes/@Shared/Maps/MapNodeUI.tsx
@@ -1,4 +1,5 @@
-import {BaseComponent, Div, Span, Instant} from "../../../Frame/UI/ReactGlobals";
+import {Vector2i} from "react-vmenu/dist/Helpers/General";
+import {BaseComponent, Div, Span, Instant, FindDOM} from "../../../Frame/UI/ReactGlobals";
 import {MapNode, MapNodeType, MapNodeType_Info} from "./MapNode";
 import {firebaseConnect, helpers} from "react-redux-firebase";
 import {connect} from "react-redux";
@@ -46,7 +47,7 @@ interface Props {map: Map, nodeID: number, node: MapNode, path?: MapNodePath,
 		nodeChildren: GetNodes(state, {nodeIDs: (node.children || {}).VKeys().Select(a=>a.KeyToInt)})
 	};
 }) as any)
-export default class MapNodeUI extends BaseComponent<Props, {}> {
+export default class MapNodeUI extends BaseComponent<Props, {childrenHeight: number, upChildrenHeight: number}> {
 	//static contextTypes = {map: PropTypes.object};
 
 	/*shouldComponentUpdate(oldProps: Props, newProps: Props) {
@@ -59,26 +60,64 @@ export default class MapNodeUI extends BaseComponent<Props, {}> {
 
 	render() {
 		let {map, nodeID, node, path, nodeView, nodeChildren, children} = this.props;
+		let {childrenHeight, upChildrenHeight} = this.state;
 		/*let {map} = this.context;
 		if (map == null) return <div>Loading map, deep...</div>; // not sure why this occurs*/
 		//Log("Updating MapNodeUI:" + nodeID);
+
+		let separateChildren = node.type == MapNodeType.Thesis;
+		let upChildren = node.type == MapNodeType.Thesis ? nodeChildren.Where(a=>a.type == MapNodeType.SupportingArgument) : [];
+		let downChildren = node.type == MapNodeType.Thesis ? nodeChildren.Where(a=>a.type == MapNodeType.OpposingArgument) : [];
+
+		let fontSize = nodeTypeFontSizes[node.type] || 14;
+		var textPreview = $(`<a style="fontSize: ${fontSize}; whiteSpace: initial;">${node.title}</a>`);
+		var textWidth = V.GetContentWidth(textPreview);
+
+		let minWidth = node.type == MapNodeType.Thesis ? 350 : 100;
+		let maxWidth = node.type == MapNodeType.Thesis ? 500 : 200;
+		let width = textWidth.KeepBetween(minWidth, maxWidth);
+		let lines = (textWidth / maxWidth).CeilingTo(1);
+		let height = (lines * 17) + 10; // (lines * line-height) + top-plus-bottom-padding
+
 		return (
-			<div className="clickThrough" style={{display: "flex", padding: "5px 0"}}>
+			<div className="clickThrough" style={{position: "relative", display: "flex", padding: "5px 0"}}>
 				<div className="clickThrough" style={{
-					zIndex: 1, transform: "translateX(0)", // fixes z-index issue
+					zIndex: 1, //transform: "translateX(0)", // fixes z-index issue
+					paddingTop: separateChildren ? (upChildrenHeight|0) - (height / 2) : ((childrenHeight|0) / 2) - (height / 2),
 				}}>
-					<MapNodeUI_Inner map={map} node={node} nodeView={nodeView} path={path}/>
+					<MapNodeUI_Inner map={map} node={node} nodeView={nodeView} path={path} width={width}/>
 				</div>
-				<div className="clickThrough"
-						style={{
+				{node.type != MapNodeType.Thesis &&
+					<div className="clickThrough" style={{
+						zIndex: 2, marginLeft: 10,
+						display: nodeView && nodeView.expanded ? "flex" : "none", flexDirection: "column", //transform: "translateY(calc(-50% + 14px))",
+					}} ref={c=>c && c.clientHeight != this.state.childrenHeight && this.setState({childrenHeight: c.clientHeight})}>
+						{nodeChildren.map((child, index)=> {
+							let childID = node.children.VKeys()[index].KeyToInt;
+							return <MapNodeUI key={index} map={map} nodeID={childID} node={child} path={path.Extend(childID)}/>;
+						})}
+					</div>}
+				{node.type == MapNodeType.Thesis &&
+					<div style={{display: "flex", flexDirection: "column"}}>
+						<div className="clickThrough" style={{
+							zIndex: 2, marginLeft: 10,
+							display: nodeView && nodeView.expanded ? "flex" : "none", flexDirection: "column", //transform: "translateY(calc(-50% + 14px))",
+						}} ref={c=>c && c.clientHeight != this.state.upChildrenHeight && this.setState({upChildrenHeight: c.clientHeight})}>
+							{upChildren.map((child, index)=> {
+								let childID = node.children.VKeys()[index].KeyToInt;
+								return <MapNodeUI key={index} map={map} nodeID={childID} node={child} path={path.Extend(childID)}/>;
+							})}
+						</div>
+						<div className="clickThrough" style={{
 							zIndex: 2, marginLeft: 10,
 							display: nodeView && nodeView.expanded ? "flex" : "none", flexDirection: "column", //transform: "translateY(calc(-50% + 14px))",
 						}}>
-					{/*nodeView && nodeView.expanded &&*/ nodeChildren.map((child, index)=> {
-						let childID = node.children.VKeys()[index].KeyToInt;
-						return <MapNodeUI key={index} map={map} nodeID={childID} node={child} path={path.Extend(childID)}/>;
-					})}
-				</div>
+							{downChildren.map((child, index)=> {
+								let childID = node.children.VKeys()[index].KeyToInt;
+								return <MapNodeUI key={index} map={map} nodeID={childID} node={child} path={path.Extend(childID)}/>;
+							})}
+						</div>
+					</div>}
 			</div>
 		);
 	}
@@ -103,7 +142,7 @@ let nodeTypeFontSizes = {
 	Category: 16
 }
 
-type MapNodeUI_Inner_Props = {map: Map, node: MapNode, nodeView: MapNodeView, path: MapNodePath} & Partial<{selectedNodeID: number, userID: string}>;
+type MapNodeUI_Inner_Props = {map: Map, node: MapNode, nodeView: MapNodeView, path: MapNodePath, width: number} & Partial<{selectedNodeID: number, userID: string}>;
 @firebaseConnect()
 @(connect((state: RootState, props: MapNodeUI_Inner_Props)=> ({
 	selectedNodeID: GetSelectedNodeID(state, props),
@@ -112,24 +151,24 @@ type MapNodeUI_Inner_Props = {map: Map, node: MapNode, nodeView: MapNodeView, pa
 class MapNodeUI_Inner extends BaseComponent<MapNodeUI_Inner_Props, {}> {
 	//static contextTypes = {store: PropTypes.object.isRequired};
 	render() {
-		let {firebase, map, node, nodeView, path, selectedNodeID, userID} = this.props;
+		let {firebase, map, node, nodeView, path, width, selectedNodeID, userID} = this.props;
 		//let {dispatch} = this.context.store;
 		let backgroundColor = nodeTypeBackgroundColors[node.type];
 		//let enemyBackgroundColor = nodeTypeBackgroundColors_enemy[node.type] || "150,150,150";
 		let fontSize = nodeTypeFontSizes[node.type] || 14;
-		let minWidth = node.type == MapNodeType.Thesis ? 350 : 100;
-		let maxWidth = node.type == MapNodeType.Thesis ? 500 : 200;
+		/*let minWidth = node.type == MapNodeType.Thesis ? 350 : 100;
+		let maxWidth = node.type == MapNodeType.Thesis ? 500 : 200;*/
 		let barSize = 5;
 		let fillPercent = path.nodeIDs.length <= 2 ? 1 : .9;
 		return (
 			<div style={{
 				display: "flex", position: "relative", borderRadius: 5, cursor: "pointer", zIndex: 1,
-				top: "50%", transform: "translateY(calc(-50% - .5px))", // -.5px is added so we end with integer (which avoids anti-aliasing)
+				//top: "50%", transform: "translateY(calc(-50% - .5px))", // -.5px is added so we end with integer (which avoids anti-aliasing)
 				//boxShadow: "0 0 1px rgba(255,255,255,.5)",
 				/*boxShadow: "rgba(0, 0, 0, 1) 0px 0px 100px",
 				filter: "drop-shadow(rgba(0,0,0,1) 0px 0px 3px) drop-shadow(rgba(0,0,0,.35) 0px 0px 3px)",*/
 				//boxShadow: "rgba(0, 0, 0, 1) 0px 0px 1px",
-				boxShadow: `rgba(0,0,0,1) 0px 0px 2px`,
+				boxShadow: `rgba(0,0,0,1) 0px 0px 2px`, width,
 			}}>
 				{nodeView && nodeView.selected && <MapNodeUI_LeftBox map={map} node={node} nodeView={nodeView} path={path} backgroundColor={backgroundColor}/>}
 				<div style={{position: "absolute", transform: "translateX(-100%)", width: 1, height: 28}}/> {/* fixes click-gap */}
@@ -140,18 +179,16 @@ class MapNodeUI_Inner extends BaseComponent<MapNodeUI_Inner_Props, {}> {
 					<div style={{position: "absolute", bottom: -barSize - 1, width: .9 * minWidth, height: barSize, background: `rgba(${backgroundColor},1)`}}/>,
 				]*/}
 
-				<div
-						style={{
-							display: "flex", zIndex: 2, //background: `rgba(${backgroundColor},.7)`,
-							background: "rgba(0,0,0,.7)", borderRadius: 5, cursor: "pointer",
-						}}
-						onClick={()=> {
-							if (selectedNodeID != node._key.KeyToInt)
-								store.dispatch(new ACTSelectMapNode({mapID: map._key.KeyToInt, path}));
-						}}>
+				<div style={{
+					display: "flex", zIndex: 2, width: "100%", //background: `rgba(${backgroundColor},.7)`,
+					background: "rgba(0,0,0,.7)", borderRadius: 5, cursor: "pointer",
+				}}>
 					<div style={{
-						position: "relative", minWidth: minWidth - 20, maxWidth: maxWidth - 20,
+						position: "relative", width: "100%", //minWidth: minWidth - 20, maxWidth: maxWidth - 20,
 						padding: 5, //node.type == MapNodeType.Category || node.type == MapNodeType.Package ? 5 : "3px 5px",
+					}} onClick={()=> {
+						if (selectedNodeID != node._key.KeyToInt)
+							store.dispatch(new ACTSelectMapNode({mapID: map._key.KeyToInt, path}));
 					}}>
 						<div style={{
 							position: "absolute", zIndex: 0, left: 0, top: 0, bottom: 0,
@@ -160,6 +197,71 @@ class MapNodeUI_Inner extends BaseComponent<MapNodeUI_Inner_Props, {}> {
 						<a style={{position: "relative", zIndex: 1, fontSize, whiteSpace: "initial"}}>
 							{node.title}
 						</a>
+						<VMenu contextMenu={true} onBody={true}>
+							{MapNodeType_Info.for[node.type].childTypes.map(childType=> {
+								let childTypeInfo = MapNodeType_Info.for[childType];
+								return (
+									<VMenuItem key={childType} text={`Add ${childTypeInfo.displayName}`} style={styles.vMenuItem} onClick={e=> {
+										if (e.button != 0) return;
+										let title = "";
+										let boxController = ShowMessageBox({
+											title: `Add ${childTypeInfo.displayName}`, cancelButton: true,
+											messageUI: ()=>(
+												<div style={{padding: "10px 0"}}>
+													Title: <TextInput value={title} onChange={val=>DN(title = val, boxController.UpdateUI())}/>
+												</div>
+											),
+											onOK: ()=> {
+												/*firebase.Ref(`/nodes`).update({
+													[node._key]: {
+														children: {[newID.IntToKey]: {}},
+													},
+													[newID.IntToKey]: new MapNode({
+														type: childType, title,
+														creator: userID, approved: true,
+													}),
+												});*/
+												firebase.Ref("nodes").transaction(nodes=> {
+													if (!nodes) return nodes;
+
+													let newID = (nodes as Object).Props.Select(a=>a.name.KeyToInt).Max() + 1;
+													nodes[node._key].children = {
+														...nodes[node._key].children,
+														[newID.IntToKey]: {_: true}
+													};
+													nodes[newID.IntToKey] = new MapNode({
+														type: childType, title,
+														creator: userID, approved: true,
+													});
+													return nodes;
+												}, undefined, false);
+											}
+										});
+									}}/>
+								);
+							})}
+							<VMenuItem text="Delete" style={styles.vMenuItem} onClick={e=> {
+								if (e.button != 0) return;
+								firebase.Ref("nodes").once("value", (snapshot: DataSnapshot)=> {
+									let nodes = (snapshot.val() as Object).Props.Select(a=>a.value.Extended({_key: a.name}));
+									let parentNodes = nodes.Where(a=>a.children && a.children[node._key]);
+									let s_ifParents = parentNodes.length > 1 ? "s" : "";
+									ShowMessageBox({
+										title: `Delete "${node.title}"`, cancelButton: true,
+										message: `Delete the node "${node.title}", and its link${s_ifParents} with ${parentNodes.length} parent-node${s_ifParents}?`,
+										onOK: ()=> {
+											firebase.Ref("nodes").transaction(nodes=> {
+												if (!nodes) return nodes;
+												for (let parent of parentNodes)
+													nodes[parent._key].children[node._key] = null;
+												nodes[node._key] = null;
+												return nodes;
+											}, undefined, false);
+										}
+									});
+								});
+							}}/>
+						</VMenu>
 					</div>
 					<Button //text={nodeView && nodeView.expanded ? "-" : "+"} size={28}
 							style={{
@@ -179,71 +281,6 @@ class MapNodeUI_Inner extends BaseComponent<MapNodeUI_Inner_Props, {}> {
 						{nodeView && nodeView.expanded ? "-" : "+"}
 					</Button>
 				</div>
-				<VMenu contextMenu={true} onBody={true}>
-					{MapNodeType_Info.for[node.type].childTypes.map(childType=> {
-						let childTypeInfo = MapNodeType_Info.for[childType];
-						return (
-							<VMenuItem key={childType} text={`Add ${childTypeInfo.displayName}`} style={styles.vMenuItem} onClick={e=> {
-								if (e.button != 0) return;
-								let title = "";
-								let boxController = ShowMessageBox({
-									title: `Add ${childTypeInfo.displayName}`, cancelButton: true,
-									messageUI: ()=>(
-										<div style={{padding: "10px 0"}}>
-											Title: <TextInput value={title} onChange={val=>DN(title = val, boxController.UpdateUI())}/>
-										</div>
-									),
-									onOK: ()=> {
-										/*firebase.Ref(`/nodes`).update({
-											[node._key]: {
-												children: {[newID.IntToKey]: {}},
-											},
-											[newID.IntToKey]: new MapNode({
-												type: childType, title,
-												creator: userID, approved: true,
-											}),
-										});*/
-										firebase.Ref("nodes").transaction(nodes=> {
-											if (!nodes) return nodes;
-
-											let newID = (nodes as Object).Props.Select(a=>a.name.KeyToInt).Max() + 1;
-											nodes[node._key].children = {
-												...nodes[node._key].children,
-												[newID.IntToKey]: {_: true}
-											};
-											nodes[newID.IntToKey] = new MapNode({
-												type: childType, title,
-												creator: userID, approved: true,
-											});
-											return nodes;
-										}, undefined, false);
-									}
-								});
-							}}/>
-						);
-					})}
-					<VMenuItem text="Delete" style={styles.vMenuItem} onClick={e=> {
-						if (e.button != 0) return;
-						firebase.Ref("nodes").once("value", (snapshot: DataSnapshot)=> {
-							let nodes = (snapshot.val() as Object).Props.Select(a=>a.value.Extended({_key: a.name}));
-							let parentNodes = nodes.Where(a=>a.children && a.children[node._key]);
-							let s_ifParents = parentNodes.length > 1 ? "s" : "";
-							ShowMessageBox({
-								title: `Delete "${node.title}"`, cancelButton: true,
-								message: `Delete the node "${node.title}", and its link${s_ifParents} with ${parentNodes.length} parent-node${s_ifParents}?`,
-								onOK: ()=> {
-									firebase.Ref("nodes").transaction(nodes=> {
-										if (!nodes) return nodes;
-										for (let parent of parentNodes)
-											nodes[parent._key].children[node._key] = null;
-										nodes[node._key] = null;
-										return nodes;
-									}, undefined, false);
-								}
-							});
-						});
-					}}/>
-				</VMenu>
 			</div>
 		);
 	}
