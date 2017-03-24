83b78e55382dccf92f7a52a06df318f348c4d5cd
Venryx <venryx@gmail.com>
2017-03-24 06:22:55 -0700

    * Node connection lines are smoother.
    Former-commit-id: 133514dec5fc0616a792636fa5d1128ae0a2ee33

--- src/routes/@Shared/Maps/MapNode/NodeConnectorBackground.tsx
+++ src/routes/@Shared/Maps/MapNode/NodeConnectorBackground.tsx
@@ -18,10 +18,28 @@ export default class NodeConnectorBackground extends BaseComponent<{node: MapNod
 						? nodeTypeBackgroundColors[node.type]
 						: nodeTypeBackgroundColors[child.type];
 
+					/*var start = mainBoxOffset;
+					var startControl = start.Plus(30, 0);
+					let end = childOffset;
+					let endControl = childOffset.Plus(-30, 0);
+					return <path key={"connectorLine_" + index} style={{stroke: `rgba(${backgroundColor},1)`, strokeWidth: 3, fill: "none"}}
+						d={`M${start.x},${start.y} C${startControl.x},${startControl.y} ${endControl.x},${endControl.y} ${end.x},${end.y}`}/>;*/
+					/*var start = mainBoxOffset;
+					var startControl = start.Plus(15, 0);
+					let end = childOffset;
+					let endControl = childOffset.Plus(-15, 0);
+					let middleControl = start.Plus(end).Times(.5);
+					return <path key={"connectorLine_" + index} style={{stroke: `rgba(${backgroundColor},1)`, strokeWidth: 3, fill: "none"}}
+						d={`M${start.x},${start.y} Q${startControl.x},${startControl.y} ${middleControl.x},${middleControl.y} T${end.x},${end.y}`}/>;*/
 					var start = mainBoxOffset;
 					var startControl = start.Plus(30, 0);
 					let end = childOffset;
 					let endControl = childOffset.Plus(-30, 0);
+
+					let middleControl = start.Plus(end).Times(.5); // average start-and-end to get middle-control
+					startControl = startControl.Plus(middleControl).Times(.5); // average with middle-control
+					endControl = endControl.Plus(middleControl).Times(.5); // average with middle-control
+
 					return <path key={"connectorLine_" + index} style={{stroke: `rgba(${backgroundColor},1)`, strokeWidth: 3, fill: "none"}}
 						d={`M${start.x},${start.y} C${startControl.x},${startControl.y} ${endControl.x},${endControl.y} ${end.x},${end.y}`}/>;
 				})}
