aeda65ece01b9b63bf38efadc321078919c263bd
Venryx <venryx@gmail.com>
2017-06-30 07:51:36 -0700

    * Moved JQuery file to Resources folder. (included from html page directly)
    This speeds it up by ~5s. However, it probably means it's never minified anymore, which might be worse...
    
    
    Former-commit-id: ac8b723b837d7e585898142e3026d08c5e22d625

--- Source/Frame/General/Start_1.ts
+++ Source/Frame/General/Start_1.ts
@@ -1,4 +1,5 @@
-import JQuery from "../JQuery/JQuery3.1.0";
+//import JQuery from "../JQuery/JQuery3.1.0";
+let JQuery = jQuery; // include JQuery file directly from html, for faster building
 import React from "react";
 
 G({JQuery, jQuery: JQuery, $: JQuery}); declare global { const JQuery; /*const jQuery; const $;*/ }
