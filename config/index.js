b5eab4fa2a411dc97d7ef3f50735fdc176c522a3
Venryx <venryx@gmail.com>
2018-01-08 19:20:26 -0800

    * Switched devtool from "source-map" to "cheap-source-map". Loses TS code, but cuts contribution to rebuild time from 6s to 1s.
    Former-commit-id: ce62c4506646dd945c66321aacf983c7c17d120c

--- config/index.js
+++ config/index.js
@@ -72,12 +72,12 @@ const config = {
 		],
 	},
 	// list of types: https://webpack.js.org/configuration/devtool
-	//compiler_devtool: "source-map", // shows: original (in error.stack, shows bundle line)
-	//compiler_devtool: "cheap-module-eval-source-map", // shows: original (in error.stack, shows eval/transpiled-to-js-but-in-module line)
-	//compiler_devtool: "cheap-module-source-map", // in webpack-2 at least, shows: transpiled-to-js
-	//compiler_devtool: "cheap-source-map", // shows: transpiled-to-js
-	//compiler_devtool: "eval", // shows: transpiled-to-js
-	compiler_devtool: "source-map", // shows: [...]
+	// *: All "eval" ones don't work anymore with new tsc setup -- they don't show original files
+	//compiler_devtool: "source-map", // shows: original (in error.stack, shows bundle line) [6s/rebuild]
+	//compiler_devtool: "cheap-module-eval-source-map", // *shows: original (in error.stack, shows eval/transpiled-to-js-but-in-module line)
+	//compiler_devtool: "cheap-module-source-map", // shows: original [however, for some reason it misses lots of lines -- at least in async functions]
+	compiler_devtool: "cheap-source-map", // shows: transpiled-to-js [.8s/rebuild]
+	//compiler_devtool: "eval", // *shows: transpiled-to-js
 	compiler_fail_on_warning: false,
 	compiler_quiet: false,
 	compiler_public_path: "/",
