c1aa9043457be720ab1dd84049af25455eb67357
Venryx <venryx@gmail.com>
2021-07-07 20:24:15 -0700

    * Fixed webpack compile error, by changing way that knex is obtained by mobx-graphlink.

--- Packages/server/Source/Utils/LibIntegrations/MobXGraphlink.ts
+++ Packages/server/Source/Utils/LibIntegrations/MobXGraphlink.ts
@@ -2,7 +2,7 @@ import {GraphDBShape, systemUserID, systemUserName} from "dm_common";
 import "web-vcore/nm/mobx"; // import mobx before we declare the module below, otherwise vscode auto-importer gets confused at path to mobx
 import {Graphlink, SetDefaultGraphOptions, ProvideReactModule} from "web-vcore/nm/mobx-graphlink.js";
 import React from "react";
-import {PoolClient} from "pg";
+import Knex from "knex";
 import {RootState, store} from "../Store/Store.js";
 import {apolloClient} from "./Apollo.js";
 
@@ -19,6 +19,7 @@ export function InitGraphlink() {
 	graph.Initialize({
 		rootStore: store,
 		apollo: apolloClient as any, // the "as any" is needed if "mobx-graphlink" is npm-linked from "web-vcore"
+		knexModule: Knex,
 		//pgClient, // rather than set it here, update the field each time pgClient is recreated (ie. in pgPool's "connect" handler) 
 	});
 	graph.userInfo = {id: systemUserID, displayName: systemUserName};
