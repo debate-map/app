ebd602c1f8243a86b75e120478d025b7bc053be4
Venryx <venryx@gmail.com>
2021-08-07 04:24:02 -0700

    * Removed the need for the "package_forDocker_temp.json" file.

--- YVTConfig.js
+++ YVTConfig.js
@@ -1,13 +1,23 @@
 // at dev-time, we import from "web-vcore/nm/XXX", so no need for vlibs as direct deps;
 // 	and at docker-build time, we also don't need the symlinks, since we want to use the strict npm-versions of all packages anyway
-process.env.VLIBS_USE_SYMLINKS = "off";
-
-//const wvcPackageJSONObj = require("web-vcore/package.json");
+/*process.env.VLIBS_USE_SYMLINKS = "off";
 const yvtConfigObj = require("web-vcore/YVTConfig.cjs").config;
+console.log("Collected yvt-config from web-vcore:", yvtConfigObj);*/
 
-console.log("Collected yvt-config from web-vcore:", yvtConfigObj);
+//const inDocker = process.env.USERNAME == null;
+const inDocker = require("fs").existsSync("/.dockerenv");
 
 exports.config = {
 	//dependencyOverrideGroups: wvcPackageJSONObj.dependencyOverrideGroups,
-	dependencyOverrideGroups: yvtConfigObj.dependencyOverrideGroups,
+	//dependencyOverrideGroups: yvtConfigObj.dependencyOverrideGroups,
+	dependencyOverrideGroups: [
+		!inDocker && process.env.VLIBS_USER == "venryx" && {
+			overrides_forSelf: {
+				//"web-vcore": "portal:./Portals/web-vcore",
+				//"@pg-lq/postgraphile-plugin": "portal:./Portals/@pg-lq_postgraphile-plugin",
+				"web-vcore": "link:../../@Modules/web-vcore/Main",
+				"@pg-lq/postgraphile-plugin": "link:../../@Modules/postgraphile-live-query/Main/Packages/postgraphile-plugin",
+			}
+		}
+	],
 };
