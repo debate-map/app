193d3fa14ad1748b887bb05ffc51818efd54e908
Venryx <venryx@gmail.com>
2021-09-17 22:34:34 -0700

    * Fixed that client's "dev" (webpack) script wasn't running. * Fixed that the "yarn install"-like command run in Dockerfile, wasn't having the "inlineBuilds" flag enabled, causing logs from the patch-applying and such to be not shown. (fixed by making a chance to the plugin-workspace-tools.js file)

--- Packages/client/Scripts/Bin/Server.js
+++ Packages/client/Scripts/Bin/Server.js
@@ -1,7 +1,9 @@
 import {Serve} from "web-vcore/Scripts_Dist/Bin/Server.js";
-//import {webpackConfig} from "../Build/WebpackConfig.js";
 import {config} from "../Config.js";
-import {createRequire} from "module";
-const require = createRequire(import.meta.url);
 
-Serve(config, DEV ? require("../Build/WebpackConfig.js").webpackConfig : null);
+import {webpackConfig} from "../Build/WebpackConfig.js";
+Serve(config, webpackConfig);
+// commented; we must use "import ...", because "require(...)" only works for commonjs modules
+/*import {createRequire} from "module";
+const require = createRequire(import.meta.url);
+Serve(config, DEV ? require("../Build/WebpackConfig.js").webpackConfig : null);*/
