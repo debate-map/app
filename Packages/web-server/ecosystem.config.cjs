89e075b3765ede97dd7a5c661b72954f5d19fc69
Venryx <venryx@gmail.com>
2021-12-22 17:10:01 -0800

    * Cleaned up (and made consistent) the web/app server ecosystem.config.cjs files.

--- Packages/web-server/ecosystem.config.cjs
+++ Packages/web-server/ecosystem.config.cjs
@@ -4,55 +4,30 @@ const DEV = process.env.ENV == "dev";
 const k8sServiceHost = process.env.KUBERNETES_SERVICE_HOST;
 const inLocalK8s = DEV;
 console.log("Preparing to run web-server. @devMode:", DEV, "@serverHost:", k8sServiceHost, "@inLocalK8s:", inLocalK8s);
-//console.log("Env:", process.env);
 
+const nodeArgs = [
+	`--experimental-specifier-resolution=node`,
+	//`--heapsnapshot-near-heap-limit=3`,
+	//`--inspect`,
+];
 module.exports = {
 	apps: [{
 		name: "main",
 
-		...DEV ? {
-			//script: "node --loader ts-node/esm.mjs --experimental-specifier-resolution=node ./Dist/Main.js; sleep infinity", // sleep forever after, so if errors, kubernetes doesn't instantly restart it
-			script: "node --experimental-specifier-resolution=node ./Dist/Main.js; sleep infinity", // sleep forever after, so if errors, kubernetes doesn't instantly restart it
-			interpreter: null,
-		} : {
-			script: "./Dist/Main.js",
-			//node_args: "--loader ts-node/esm.mjs --experimental-specifier-resolution=node",
-			node_args: "--experimental-specifier-resolution=node",
-		},
+		// sleep a few seconds before starting; kinda ugly, but only way I know atm to keep pm2 from cutting off the start of the logs, for the first run after pod-deployment
+		script: `echo sleep0; sleep 1; echo sleep1; sleep 1; echo sleep2; sleep 1; echo sleep3; sleep 1; echo sleep4; sleep 1; echo sleep5; node ${nodeArgs.join(" ")} ./Dist/Main.js`,
+		interpreter: null,
 
-		// temp; the stop_exit_codes solution doesn't seem to work for every case, so use back-off on retries at least
-		exp_backoff_restart_delay: 500,
-		
-		// disable restart-on-error (that's kubernetes' job);
-		//stop_exit_codes: [0],
-		stop_exit_codes: (()=>{
-			const result = [0];
-			// have pm2 view *every* exit-code as a "stop"/to-ignore one (other than 42; that's our restart code)
-			result.includes = stopCodeFound=>stopCodeFound != 42;
-			return result;
-		})(),
+		autorestart: false,
 
-		//watch: true, // watch:true doesn't work fsr (it ignores node_modules)
-		// disable watching in remote k8s instances, because not all support it (eg. ovh)
-		//watch: inLocalK8s ? "**" : false,
 		watch: "**",
-		/*watch: [
-			"Packages/web-server",
-			"Packages/client/Source_JS",
-			"Packages/common/Dist",
-			...nodeModuleWatchPaths,
-		],*/
-		ignore: null,
-		ignore_watch: [],
 		watch_options: {
-			cwd: '/dm_repo',
+			cwd: "/dm_repo",
 		},
-		
-		/*env_production: {
-			NODE_ENV: "production"
-		},
-		env_development: {
-			NODE_ENV: "development"
-		}*/
+		ignore: null,
+		// this is claimed to be regex-based (https://pm2.keymetrics.io/docs/usage/application-declaration), but appears to actually be blob-based
+		ignore_watch: [
+			"**/newrelic_agent.log",
+		],
 	}],
 };
