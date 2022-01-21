afc6467b6c6c961f7bcc7b7f901f0ff5cd79d440
Venryx <venryx@gmail.com>
2022-01-21 05:08:31 -0800

    *

--- Packages/app-server-rs/src/pgclient.rs
+++ Packages/app-server-rs/src/pgclient.rs
@@ -15,7 +15,7 @@ async fn q(client: &Client, query: &str) -> Vec<SimpleQueryRow> {
 
 /**
  * There appear to be three ways to use replication slots:
- * 1) "CREATE_REPLICATION_SLOT" followed by "pg_catalog.pg_logical_slot_get_changes".
+ * 1) "CREATE_REPLICATION_SLOT" followed by "pg_logical_slot_get_changes()".
  * 2) "CREATE_REPLICATION_SLOT" followed by "START_REPLICATION" (with stream listener).
  * 3) Connecting to postgres pod through shell, then running "pg_recvlogical".
  * In this function, we use approach 2.
@@ -65,12 +65,12 @@ pub async fn start_streaming_changes() -> Result<(), tokio_postgres::Error> {
         }
         // type: keepalive message
         else if event[0] == b'k' {
-            let lastByte = event.last().unwrap();
-            let timeoutImminent = lastByte == &1;
-            println!("Got keepalive message:{:x?} @timeoutImminent:{}", event, timeoutImminent);
-            if timeoutImminent {
+            let last_byte = event.last().unwrap();
+            let timeout_imminent = last_byte == &1;
+            println!("Got keepalive message:{:x?} @timeoutImminent:{}", event, timeout_imminent);
+            if timeout_imminent {
                 // not sure if sending the client system's "time since 2000-01-01" is actually necessary, but lets do as postgres asks just in case
-                let SECONDS_FROM_UNIX_EPOCH_TO_2000 = 946684800;
+                const SECONDS_FROM_UNIX_EPOCH_TO_2000: u128 = 946684800;
                 let time_since_2000: u64 = (SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_micros() - (SECONDS_FROM_UNIX_EPOCH_TO_2000 * 1000 * 1000)).try_into().unwrap();
                 
                 // see here for format details: https://www.postgresql.org/docs/10/protocol-replication.html
