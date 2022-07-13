9828a0d237df2b10aca5cbea5e06851e88d4a876
Venryx <venryx@gmail.com>
2022-07-12 20:22:08 -0700

    * MS clone-history button shows the true number of clones currently present, rather than the total number of clones ever created (well, as recorded in the clone-history tags anyway). [in the future, this will be unnecessary because tags will be cleaned up at time of node-deletion, but this is an ok workaround for now]

--- Packages/client/Source/UI/@Shared/Maps/MapNode/NodeUI/CloneHistoryButton.tsx
+++ Packages/client/Source/UI/@Shared/Maps/MapNode/NodeUI/CloneHistoryButton.tsx
@@ -1,8 +1,10 @@
-import {GetNodeTagComps, GetNodeTags, MapNodeL3, TagComp_CloneHistory} from "dm_common";
+import {GetNode, GetNodeTagComps, GetNodeTags, MapNodeL3, TagComp_CloneHistory} from "dm_common";
+import {Clone} from "js-vextensions";
 import React from "react";
 import {store} from "Store";
 import {SearchResultRow} from "UI/@Shared/NavBar/SearchPanel.js";
 import {ES, Observer} from "web-vcore";
+import {MapWithBailHandling} from "web-vcore/.yalc/mobx-graphlink";
 import {Button, Column, Row, Select} from "web-vcore/nm/react-vcomponents.js";
 import {BaseComponent, BaseComponentPlus} from "web-vcore/nm/react-vextensions.js";
 import {BoxController, ShowMessageBox} from "web-vcore/nm/react-vmessagebox.js";
@@ -14,13 +16,20 @@ export class CloneHistoryButton extends BaseComponent<{node: MapNodeL3}, {}> {
 		const {node} = this.props;
 		if (!store.main.maps.showCloneHistoryButtons) return null;
 
-		const cloneHistoryComps_withEmpties = GetNodeTagComps(node.id).filter(a=>a instanceof TagComp_CloneHistory) as TagComp_CloneHistory[];
-		// todo: probably filter out nodes here in chain that were deleted (since it's not cleaned up by deletion command atm)
-		const cloneHistoryComps = cloneHistoryComps_withEmpties.filter(a=>a.cloneChain.length > 1);
-		if (cloneHistoryComps.length == 0) return null;
+		const cloneHistoryComps_raw = GetNodeTagComps(node.id).filter(a=>a instanceof TagComp_CloneHistory) as TagComp_CloneHistory[];
+		// "clean" our copy of the clone-histories, to ignore entries pointing to a node that doesn't exist
+		// (else, someone could see the clone-history button as signifying there's a clone, eg. so deletion is safe, when in fact that clone was already deleted)
+		// (in the future, this will be solved by a thorough cleanup of tags at the point of node-deletion)
+		const cloneHistoryComps_cleaned = cloneHistoryComps_raw.map(comp=>{
+			const cleaned = Clone(comp) as TagComp_CloneHistory;
+			cleaned.cloneChain = MapWithBailHandling(cleaned.cloneChain, id=>GetNode(id)?.id).filter(a=>a != null) as string[];
+			return cleaned;
+		});
+		const cloneHistoryComps_final = cloneHistoryComps_cleaned.filter(a=>a.cloneChain.length > 1);
+		if (cloneHistoryComps_final.length == 0) return null;
 		//const mainCloneHistoryComp: TagComp_CloneHistory|null = cloneHistoryComps.Max(a=>a.cloneChain.length);
-		const historiesWithEntriesBeforeUs = cloneHistoryComps.filter(a=>a.cloneChain.indexOf(node.id) > 0);
-		const historiesWithEntriesAfterUs = cloneHistoryComps.filter(a=>a.cloneChain.indexOf(node.id) < a.cloneChain.length - 1);
+		const historiesWithEntriesBeforeUs = cloneHistoryComps_final.filter(a=>a.cloneChain.indexOf(node.id) > 0);
+		const historiesWithEntriesAfterUs = cloneHistoryComps_final.filter(a=>a.cloneChain.indexOf(node.id) < a.cloneChain.length - 1);
 
 		return (
 			<div style={{position: "absolute", right: "calc(100% - 35px)", top: 0, bottom: 0, display: "flex"}}>
