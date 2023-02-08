be8dc9e70bc227d90e07137ca69b95e34a032785
Venryx <venryx@gmail.com>
2023-02-08 10:49:38 -0800

    * Fixed that update_node_rating_summaries was not disabling RLS during its execution. (meaning that it would not include ratings that were private -- the summary is supposed to include all ratings, thus allowing a user to impact the rating distribution, but without it being public who made which rating)

--- Packages/app-server/src/db/commands/_shared/update_node_rating_summaries.rs
+++ Packages/app-server/src/db/commands/_shared/update_node_rating_summaries.rs
@@ -31,64 +31,68 @@ use super::jsonb_utils::jsonb_set;
 use super::rating_processor::get_argument_impact_pseudo_ratings;
 
 pub async fn update_node_rating_summaries(ctx: &AccessorContext<'_>, _actor: &User, node_id: String, rating_type: NodeRatingType) -> Result<(), Error> {
-	let ratingTypeInfo = get_rating_type_info(rating_type);
-	let ratings = get_node_ratings_base(ctx, &node_id, Some(rating_type), None).await?;
-	let ratings_in_each_range = ratingTypeInfo.valueRanges.iter().map(|range|{
-		return ratings.iter().filter(|a| rating_value_is_in_range(a.value, range)).cloned().collect_vec();
-	}).collect_vec();
-
-	let new_summary = RatingSummary {
-		average: if ratings.len() > 0 { Some(average(&ratings.into_iter().map(|a|a.value).collect_vec())) } else { None },
-		countsByRange: ratingTypeInfo.valueRanges.iter().enumerate().map(|(i, _range)| ratings_in_each_range[i].len() as i64).collect_vec(),
-	};
-
-	jsonb_set(&ctx.tx, "nodes", &node_id, "extras", vec!["ratingSummaries".to_owned(), enum_to_string(&rating_type)], Some(serde_json::to_value(new_summary)?)).await?;
-
-	let argument_nodes =
-		if rating_type == NodeRatingType::relevance { vec![get_node(ctx, &node_id).await?] }
-		else if rating_type == NodeRatingType::truth { get_node_parents(ctx, &node_id).await?.into_iter().filter(|a|a.r#type == NodeType::argument).collect_vec() }
-		else { vec![] };
-	for argument in argument_nodes {
-		let premises = get_node_children(ctx, &argument.id).await?.into_iter().filter(|a|a.r#type == NodeType::claim).collect_vec();
-		let ratingTypeInfo_impact = get_rating_type_info(NodeRatingType::impact);
-		let ratings_impact = get_argument_impact_pseudo_ratings(ctx, &argument, &premises, None, false).await?;
-		let ratings_impact_inEachRange = ratingTypeInfo_impact.valueRanges.iter().map(|range| {
-			return ratings_impact.iter().filter(|rating| rating_value_is_in_range(rating.value, range)).cloned().collect_vec();
+	ctx.with_rls_disabled(|| async {
+		let rating_type_info = get_rating_type_info(rating_type);
+		let ratings = get_node_ratings_base(ctx, &node_id, Some(rating_type), None).await?;
+		let ratings_in_each_range = rating_type_info.valueRanges.iter().map(|range|{
+			return ratings.iter().filter(|a| rating_value_is_in_range(a.value, range)).cloned().collect_vec();
 		}).collect_vec();
-		//const average_forUsersRatingAll = ratings_impact.length ? ratings_impact.map(a=>a.value).Average() : null;
 
-		// For the "impact" rating-type, we calculate the "average" a bit differently than normal.
-		// Rather than a pure average of the "impact" pseudo-ratings, we use: [average of argument's relevance] * [average of premise-1's truth] * [...]
-		// Why? Because the "impact" pseudo-ratings exclude users that only rated one of the above rating-groups; this alternate approach utilizes all the ratings.
-		let arg_relevance_ratings = get_node_ratings_base(ctx, argument.id.as_str(), Some(NodeRatingType::relevance), None).await?;
-		let premise_truth_rating_sets = {
-			let mut sets = vec![];
-			for premise in premises {
-				sets.push(get_node_ratings_base(ctx, premise.id.as_str(), Some(NodeRatingType::truth), None).await?);
-			}
-			sets
+		let new_summary = RatingSummary {
+			average: if ratings.len() > 0 { Some(average(&ratings.into_iter().map(|a|a.value).collect_vec())) } else { None },
+			countsByRange: rating_type_info.valueRanges.iter().enumerate().map(|(i, _range)| ratings_in_each_range[i].len() as i64).collect_vec(),
 		};
-		let rating_value_sets: Vec<Vec<f64>> = {
-			let mut sets = vec![];
-			sets.push(arg_relevance_ratings.iter().map(|rating| rating.value).collect_vec());
-			for set in premise_truth_rating_sets {
-				sets.push(set.iter().map(|rating| rating.value).collect_vec());
-			}
-			sets
-		};
-		let rating_value_sets_multiplied = rating_value_sets.iter().fold(1f64, |result, set| {
-			if set.len() == 0 { return 0f64; } // if there are no ratings in this set, then we can't calculate an overall score, so have it become 0
-			return result * (average(set) / 100f64); // else, there is a valid average for this set, so do the multiplication like normal
-		});
-		let average_loose = rating_value_sets_multiplied * 100f64;
 
-		let new_impact_summary = RatingSummary {
-			average: Some(average_loose),
-			countsByRange: ratingTypeInfo_impact.valueRanges.iter().enumerate().map(|(i, _range)| ratings_impact_inEachRange[i].len() as i64).collect_vec(),
-		};
-		
-		jsonb_set(&ctx.tx, "nodes", argument.id.as_str(), "extras", vec!["ratingSummaries".to_owned(), "impact".to_owned()], Some(serde_json::to_value(new_impact_summary)?)).await?;
-	}
+		jsonb_set(&ctx.tx, "nodes", &node_id, "extras", vec!["ratingSummaries".to_owned(), enum_to_string(&rating_type)], Some(serde_json::to_value(new_summary)?)).await?;
+
+		let argument_nodes =
+			if rating_type == NodeRatingType::relevance { vec![get_node(ctx, &node_id).await?] }
+			else if rating_type == NodeRatingType::truth { get_node_parents(ctx, &node_id).await?.into_iter().filter(|a|a.r#type == NodeType::argument).collect_vec() }
+			else { vec![] };
+		for argument in argument_nodes {
+			let premises = get_node_children(ctx, &argument.id).await?.into_iter().filter(|a|a.r#type == NodeType::claim).collect_vec();
+			let ratingTypeInfo_impact = get_rating_type_info(NodeRatingType::impact);
+			let ratings_impact = get_argument_impact_pseudo_ratings(ctx, &argument, &premises, None, false).await?;
+			let ratings_impact_inEachRange = ratingTypeInfo_impact.valueRanges.iter().map(|range| {
+				return ratings_impact.iter().filter(|rating| rating_value_is_in_range(rating.value, range)).cloned().collect_vec();
+			}).collect_vec();
+			//const average_forUsersRatingAll = ratings_impact.length ? ratings_impact.map(a=>a.value).Average() : null;
+
+			// For the "impact" rating-type, we calculate the "average" a bit differently than normal.
+			// Rather than a pure average of the "impact" pseudo-ratings, we use: [average of argument's relevance] * [average of premise-1's truth] * [...]
+			// Why? Because the "impact" pseudo-ratings exclude users that only rated one of the above rating-groups; this alternate approach utilizes all the ratings.
+			let arg_relevance_ratings = get_node_ratings_base(ctx, argument.id.as_str(), Some(NodeRatingType::relevance), None).await?;
+			let premise_truth_rating_sets = {
+				let mut sets = vec![];
+				for premise in premises {
+					sets.push(get_node_ratings_base(ctx, premise.id.as_str(), Some(NodeRatingType::truth), None).await?);
+				}
+				sets
+			};
+			let rating_value_sets: Vec<Vec<f64>> = {
+				let mut sets = vec![];
+				sets.push(arg_relevance_ratings.iter().map(|rating| rating.value).collect_vec());
+				for set in premise_truth_rating_sets {
+					sets.push(set.iter().map(|rating| rating.value).collect_vec());
+				}
+				sets
+			};
+			let rating_value_sets_multiplied = rating_value_sets.iter().fold(1f64, |result, set| {
+				if set.len() == 0 { return 0f64; } // if there are no ratings in this set, then we can't calculate an overall score, so have it become 0
+				return result * (average(set) / 100f64); // else, there is a valid average for this set, so do the multiplication like normal
+			});
+			let average_loose = rating_value_sets_multiplied * 100f64;
+
+			let new_impact_summary = RatingSummary {
+				average: Some(average_loose),
+				countsByRange: ratingTypeInfo_impact.valueRanges.iter().enumerate().map(|(i, _range)| ratings_impact_inEachRange[i].len() as i64).collect_vec(),
+			};
+			
+			jsonb_set(&ctx.tx, "nodes", argument.id.as_str(), "extras", vec!["ratingSummaries".to_owned(), "impact".to_owned()], Some(serde_json::to_value(new_impact_summary)?)).await?;
+		}
+
+		Ok(())
+	}, Some("Failed to update node-rating-summaries for node.")).await?;
 
 	Ok(())
 }
