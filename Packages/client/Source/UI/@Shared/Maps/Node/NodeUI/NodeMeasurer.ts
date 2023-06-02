ab67ec3b918294379c6592fdeef0c5514c1501e2
Venryx <venryx@gmail.com>
2023-06-02 05:05:43 -0700

    * MS GetMeasurementInfoForNode uses canvas.measureText, rather than creating a dom-node and reading el.offsetWidth. This way scales much better, because it stays (at least roughly) the same speed, even with hundreds/thousands of nodes visible. (whereas the dom approach just keeps getting slower and slower)
    Dom approach: starts at ~0.4ms, grows to >10ms
    Canvas approach: starts at ~0.2ms, stays roughly the same throughout (possibly grows to ~0.3ms, but not certain)

--- Packages/client/Source/UI/@Shared/Maps/Node/NodeUI/NodeMeasurer.ts
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeUI/NodeMeasurer.ts
@@ -25,24 +25,34 @@ export const GetMeasurementInfoForNode = CreateAccessor(function GetMeasurementI
 
 	const displayText = GetNodeDisplayText(node, path, map);
 	const fontSize = GetFontSizeForNode(node, path);
-	// Why not using ConvertStyleObjectToCSSString here? Because it's not needed, and this saves ~70ms over 20s map-load
+	//const elForFontCSSGrabbing = GetAutoElement(`<span style="position: absolute; white-space: nowrap;">`) as HTMLElement;
+
+	/*const start1 = performance.now();
 	const expectedTextWidth_tester = GetAutoElement(`<span style="position: absolute; font-size: ${fontSize}px; white-space: nowrap;">`) as HTMLElement;
 	expectedTextWidth_tester.innerText = displayText;
-	let expectedTextWidth = expectedTextWidth_tester.offsetWidth;
-	/*expectedTextWidth_tester.innerText = `${displayText}...`;
-	const expectedTextWidth2 = expectedTextWidth_tester.getBoundingClientRect().width;*/
+	const expectedTextWidth1 = expectedTextWidth_tester.offsetWidth;
+	const end1 = performance.now();*/
+
+	//const start2 = performance.now();
+	// Why not using ConvertStyleObjectToCSSString here? Because it's not needed, and this saves ~70ms over 20s map-load
+	let expectedTextWidth = GetTextWidth(displayText, GetCanvasFont(`${fontSize}px`));
+	/*const end2 = performance.now();
+	console.log(`Widths: ${expectedTextWidth1} ${expectedTextWidth}`);
+	console.log(`Times: ${(end1 - start1).toLocaleString("en-US")} ${(end2 - start2).toLocaleString("en-US")}`);*/
 
 	let noteWidth = 0;
 	if (node.current.phrasing.note) {
-		const noteWidth_tester = GetAutoElement(`<span style="position: absolute; margin-left: 15px; font-size: 11px; white-space: nowrap;">`) as HTMLElement;
+		/*const noteWidth_tester = GetAutoElement(`<span style="position: absolute; margin-left: 15px; font-size: 11px; white-space: nowrap;">`) as HTMLElement;
 		noteWidth_tester.innerText = node.current.phrasing.note;
-		noteWidth = Math.max(noteWidth, GetContentSize(noteWidth_tester).width);
+		noteWidth = Math.max(noteWidth, GetContentSize(noteWidth_tester).width);*/
+		noteWidth = Math.max(noteWidth, GetTextWidth(node.current.phrasing.note, GetCanvasFont("11px")) + 15);
 	}
 	const titleAttachment = GetTitleIntegratedAttachment(node.current);
 	if (titleAttachment?.equation && titleAttachment?.equation.explanation) {
-		const noteWidth_tester = GetAutoElement(`<span style="position: absolute; margin-left: 15px; font-size: 11px; white-space: nowrap;">`) as HTMLElement;
+		/*const noteWidth_tester = GetAutoElement(`<span style="position: absolute; margin-left: 15px; font-size: 11px; white-space: nowrap;">`) as HTMLElement;
 		noteWidth_tester.innerText = titleAttachment?.equation.explanation;
-		noteWidth = Math.max(noteWidth, GetContentSize(noteWidth_tester).width);
+		noteWidth = Math.max(noteWidth, GetContentSize(noteWidth_tester).width);*/
+		noteWidth = Math.max(noteWidth, GetTextWidth(titleAttachment?.equation.explanation, GetCanvasFont("11px")) + 15);
 	}
 	expectedTextWidth += noteWidth;
 
@@ -73,6 +83,7 @@ export const GetMeasurementInfoForNode = CreateAccessor(function GetMeasurementI
 	const width = node.current.displayDetails?.widthOverride || expectedBoxWidth.KeepBetween(nodeTypeInfo.minWidth, maxWidth_final);
 
 	let expectedHeight = -1;
+	// todo: update this block to use canvas-based measurement as well
 	if (calcHeight) {
 		const maxTextWidth = width - expectedOtherStuffWidth;
 		const expectedTextHeight_tester = GetAutoElement(`<a id="nodeHeightTester" style="position: absolute; white-space: initial; display: inline-block;">`) as HTMLElement;
@@ -86,3 +97,25 @@ export const GetMeasurementInfoForNode = CreateAccessor(function GetMeasurementI
 
 	return {expectedBoxWidth, width, expectedHeight};
 });
+
+let textMeasurementCanvas: HTMLCanvasElement;
+let textMeasurementCanvasContext: CanvasRenderingContext2D;
+function GetTextWidth(text: string, font: string) {
+	// re-use canvas object for better performance
+	const canvas = textMeasurementCanvas ?? (textMeasurementCanvas = document.createElement("canvas"));
+	const context = textMeasurementCanvasContext ?? (textMeasurementCanvasContext = canvas.getContext("2d")!);
+
+	context.font = font;
+	const metrics = context.measureText(text);
+	return metrics.width;
+}
+
+function GetCSSStyle(element: HTMLElement, prop: string) {
+	return window.getComputedStyle(element, null).getPropertyValue(prop);
+}
+function GetCanvasFont(fontSize?: string, fontFamily?: string, fontWeight?: number, elForValFallbacks: HTMLElement = document.body) {
+	const fontSize_final = fontSize || GetCSSStyle(elForValFallbacks, "font-size") || "14px";
+	const fontFamily_final = fontFamily || GetCSSStyle(elForValFallbacks, "font-family") || "Arial, sans-serif";
+	const fontWeight_final = fontWeight || GetCSSStyle(elForValFallbacks, "font-weight") || "normal";
+	return `${fontWeight_final} ${fontSize_final} ${fontFamily_final}`;
+}
