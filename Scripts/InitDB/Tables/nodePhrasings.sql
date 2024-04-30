be64b54bde3b82e77de4c3bf47a00111e31bc3ca
Marc-Antoine Parent <maparent@acm.org>
2024-04-30 11:04:16 -0400

    search through views

--- Scripts/InitDB/Tables/nodePhrasings.sql
+++ Scripts/InitDB/Tables/nodePhrasings.sql
@@ -11,13 +11,17 @@ CREATE TABLE app."nodePhrasings" (
 	note text,
 	terms jsonb[] NOT NULL,
 	"references" text[] NOT NULL,
-	
+
 	-- we could add text_negation and/or text_narrative to this, but it might be better to just wait until the search-system revamp (that of integrating semantic search)
 	phrasing_tsvector tsvector GENERATED ALWAYS AS (app.phrasings_to_tsv(text_base, text_question)) STORED NOT NULL,
 	"c_accessPolicyTargets" text[] NOT NULL
 );
 ALTER TABLE ONLY app."nodePhrasings" ADD CONSTRAINT "v1_draft_nodePhrasings_pkey" PRIMARY KEY (id);
 ALTER TABLE app."nodePhrasings" DROP CONSTRAINT IF EXISTS "c_accessPolicyTargets_check", ADD CONSTRAINT "c_accessPolicyTargets_check" CHECK (cardinality("c_accessPolicyTargets") > 0);
+
+-- extra index for RLS-friendly view
+DROP INDEX IF EXISTS node_revisions_access_idx;
+CREATE INDEX node_phrasings_access_idx ON app."nodePhrasings" USING gin ("c_accessPolicyTargets");
 
 -- extra index for local_search func
 DROP INDEX IF EXISTS node_phrasings_node_idx;
@@ -29,3 +33,14 @@ CREATE INDEX node_phrasings_node_idx ON app."nodePhrasings" USING btree (node);
 DROP INDEX IF EXISTS node_phrasings_text_en_idx;
 CREATE INDEX node_phrasings_text_en_idx ON app."nodePhrasings" USING gin (phrasing_tsvector);
 -- CREATE INDEX node_phrasings_text_en_idx ON app."nodePhrasings" USING gin (app.phrasings_to_tsv(text_base, text_question));
+
+
+CREATE OR REPLACE VIEW app.my_node_phrasings AS
+    WITH q1 AS (
+        SELECT array_agg(concat(id, ':nodes')) AS pol
+        FROM app."accessPolicies"
+        WHERE coalesce(("permissions_userExtends" -> current_setting('app.current_user_id') -> 'nodes' -> 'access')::boolean,
+            ("permissions" -> 'nodes' -> 'access')::boolean))
+        SELECT app."nodePhrasings".* FROM app."nodePhrasings" JOIN q1 ON (is_user_admin(current_setting('app.current_user_id')) OR ("c_accessPolicyTargets" && q1.pol));
+
+GRANT SELECT ON app.my_node_phrasings TO PUBLIC;
