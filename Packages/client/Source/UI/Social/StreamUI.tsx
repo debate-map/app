e097553646dca4c172ee06fef4c6639961deddf2
Alvinosh <alvihysa89@gmail.com>
2024-05-02 17:25:45 +0200

    * Fixed Find in Map File

--- Packages/client/Source/UI/Social/StreamUI.tsx
+++ Packages/client/Source/UI/Social/StreamUI.tsx
@@ -1,18 +1,19 @@
-import {GetUserHidden, Me, MeID, SetUserData_Hidden, GetCommandRuns, CommandRun, GetUser, AddChildNode, NodeType, GetNode, GetNodeL2, GetNodeL3, AddNodeRevision, GetNodeRevision, AsNodeL3, AsNodeL2, GetAccessPolicy, CommandRun_commandNameValues, NodeL3, GetMap, globalMapID} from "dm_common";
+import {GetUserHidden, Me, MeID, SetUserData_Hidden, GetCommandRuns, CommandRun, GetUser, AddChildNode, NodeType, GetNode, GetNodeL2, GetNodeL3, AddNodeRevision, GetNodeRevision, AsNodeL3, AsNodeL2, GetAccessPolicy, CommandRun_commandNameValues, NodeL3, GetMap, globalMapID, Map, GetNodeLinks} from "dm_common";
 import React from "react";
 import {store} from "Store";
 import {NodeBox} from "UI/@Shared/Maps/Node/NodeBox";
 import useResizeObserver from "use-resize-observer";
 import {RunCommand_UpdateUserHidden} from "Utils/DB/Command";
-import {HSLA, InfoButton, Link, Observer, RunInAction_Set, TextPlus} from "web-vcore";
-import {Assert, ModifyString} from "web-vcore/nm/js-vextensions";
-import {Command, RunInAction} from "web-vcore/nm/mobx-graphlink";
+import {HSLA, InfoButton, Link, Observer, RunInAction_Set, TextPlus, LogWarning} from "web-vcore";
+import {Assert, ModifyString, SleepAsync} from "web-vcore/nm/js-vextensions";
+import {Command, GetAsync, RunInAction, UUID} from "web-vcore/nm/mobx-graphlink";
 import moment from "web-vcore/nm/moment";
 import {Button, CheckBox, Column, Pre, Row, Text} from "web-vcore/nm/react-vcomponents";
-import {BaseComponentPlus} from "web-vcore/nm/react-vextensions";
+import {BaseComponentPlus, UseState} from "web-vcore/nm/react-vextensions";
 import {ShowMessageBox} from "web-vcore/nm/react-vmessagebox";
 import {ScrollView} from "web-vcore/nm/react-vscrollview";
 import {GetOpenMapID} from "../../Store/main.js";
+import {JumpToNode} from "../@Shared/NavBar/SearchPanel.js";
 
 @Observer
 export class StreamUI extends BaseComponentPlus({panel: false} as {panel?: boolean}, {}) {
@@ -71,11 +72,12 @@ class CommandRunUI extends BaseComponentPlus({} as {run: CommandRun, index: numb
 		let messageUI: JSX.Element;
 		let messageUI_row2: JSX.Element|n;
 		let node_final:NodeL3|n;
+		let mapId: string|n;
 		if (run.commandName == "addChildNode") {
 			const payload = run.commandInput as (typeof AddChildNode)["prototype"]["payload"];
 			const returnData = run.commandResult as (typeof AddChildNode)["prototype"]["returnData"];
 			const parent = GetNode(payload.parentID);
-
+			mapId = payload.mapID;
 			//const node = GetNodeL2(returnData.nodeID);
 			//const node = GetNodeL3(returnData.nodeID);
 			const node = GetNode(returnData.nodeID);
@@ -104,6 +106,7 @@ class CommandRunUI extends BaseComponentPlus({} as {run: CommandRun, index: numb
 			const revision = GetNodeRevision(returnData.id);
 			const node = GetNode(payload.revision.node);
 			const accessPolicy = GetAccessPolicy(node?.accessPolicy);
+			mapId = payload.mapID;
 			if (revision != null && node != null && accessPolicy) {
 				const nodeL2 = AsNodeL2(node, revision, accessPolicy);
 				node_final = AsNodeL3(nodeL2, null);
@@ -126,22 +129,38 @@ class CommandRunUI extends BaseComponentPlus({} as {run: CommandRun, index: numb
 
 		const openMapID = GetOpenMapID();
 		const openMap = GetMap(openMapID);
-		const map = GetMap(node_final!.rootNodeForMap);
+		const map = GetMap(mapId);
 		const inCurrentMap = openMap?.id === map?.id;
+		const [findingPath, setFindingPath] = UseState(false);
 
-		console.log("nodeFinal:", node_final);
+		const buttonText = (isInMap, isLoading)=>{
+			if (inCurrentMap) {
+				if (findingPath) {
+					return "Finding path...";
+				}
+				return "Find in Map";
+			}
+			return "Go to Map";
+
+		};
 
 		return (
 			<Column ref={a=>rootRef(a?.DOM_HTML ?? null)}>
-				<Row mt={index > 0 ? 10 : 0} mb={run.commandName == "addChildNode" ? 30 : 0} style={{background: HSLA(0, 0, 1, .2), borderRadius: 5, fontSize: 13}}>
+				<Row mt={index > 0 ? 10 : 0} mb={node_final?.type == "claim" ? 30 : 0} style={{background: HSLA(0, 0, 1, .2), borderRadius: 5, fontSize: 13}}>
 					<Column sel ml={5} mr={5} p={5} center style={{justifyContent: "center"}}>
 						<Pre>{moment(run.runTime).format("YYYY-MM-DD")}</Pre>
 						<Pre>{moment(run.runTime).format("HH:mm:ss")}</Pre>
 					</Column>
 					{messageUI!}
-					<Button ml="auto" text={inCurrentMap ? "Find in Map" : "Go to Map"} style={{flexShrink: 0}} onClick={()=>{
+					{map?.id &&
+					<Button ml="auto" text={buttonText(inCurrentMap, findingPath)} style={{flexShrink: 0}} onClick={()=>{
 						if (inCurrentMap) {
-							// const path = 
+							setFindingPath(true);
+							 FindNodePath(node_final!.id).then(path=>{
+								if (path[0]) {
+									JumpToNode(openMapID!, path[0]);
+								}
+							 }).finally(()=>setFindingPath(false));
 							// JumpToNode(openMapID!, resultPath);
 						} else {
 							if (map == null) return; // still loading
@@ -155,8 +174,9 @@ class CommandRunUI extends BaseComponentPlus({} as {run: CommandRun, index: numb
 							});
 						}
 
-					}}/>
-					<Button ml={5} text="Details" style={{flexShrink: 0}} onClick={()=>{
+					}}/>}
+					<Button ml={map?.id ? 5 : "auto"} text="Details" style={{flexShrink: 0}} onClick={()=>{
+						console.log(node_final, map);
 						ShowMessageBox({
 							title: "Command-run details (JSON)",
 							message: ()=>{
@@ -173,4 +193,44 @@ class CommandRunUI extends BaseComponentPlus({} as {run: CommandRun, index: numb
 			</Column>
 		);
 	}
+}
+
+// TODO: move this to a more appropriate file, not sure where
+// this function is extremely similar to StartFindingPathsFromRootsToX from the SearchResultRow function. 
+// however this function does not need to cause updates on each depth, so it is simpler. 
+// for now i think we can  get away with a bit of duplication, but if we need to update this function, we should consider 
+// a small refactor to make it more reusable.
+async function FindNodePath(targetNodeY: UUID) {
+	const searchDepth = 100;
+
+	const upPathCompletions = [] as string[];
+	let upPathAttempts = [`${targetNodeY}`];
+	for (let depth = 0; depth < searchDepth; depth++) {
+		const newUpPathAttempts = [] as string[];
+		for (const upPath of upPathAttempts) {
+			const nodeID = upPath.split("/").First();
+			const node = await GetAsync(()=>GetNodeL2(nodeID));
+			if (node == null) {
+				LogWarning(`Could not find node #${nodeID}, as parent of #${upPath.split("/").XFromLast(1)}.`);
+				continue;
+			}
+
+			if (node.rootNodeForMap != null) {
+				upPathCompletions.push(upPath);
+			}
+
+			const parentLinks = await GetAsync(()=>GetNodeLinks(null, node.id));
+			const parentIDs = parentLinks.map(a=>a.parent);
+			for (const parentID of parentIDs) {
+				const newUpPath = `${parentID}/${upPath}`;
+				newUpPathAttempts.push(newUpPath);
+			}
+		}
+		upPathAttempts = newUpPathAttempts;
+		if (upPathAttempts.length == 0) break;
+
+		if (store.main.search.findNode_state === "inactive") break;
+	}
+
+	return upPathCompletions;
 }
