5d5eb8c7157bceb4409e7756824044eb2fd16058
Venryx <venryx@gmail.com>
2024-06-10 01:07:49 -0700

    * Some small ui/code tweaks.

--- Packages/client/Source/UI/@Shared/NavBar/NotificationsPanel.tsx
+++ Packages/client/Source/UI/@Shared/NavBar/NotificationsPanel.tsx
@@ -3,7 +3,7 @@ import {Button, CheckBox, Column, Div, Row, Select, Text} from "web-vcore/nm/rea
 import {InfoButton, Observer, RunInAction_Set} from "web-vcore";
 import {liveSkin} from "Utils/Styles/SkinManager";
 import {GetCommandRun, GetNotifications, MeID} from "dm_common";
-import {GetEntries} from "web-vcore/nm/js-vextensions.js";
+import {E, GetEntries} from "web-vcore/nm/js-vextensions.js";
 import {CommandRunUI} from "../../Social/StreamUI.js";
 import {RunCommand_UpdateNotification} from "../../../Utils/DB/Command.js";
 import {GetMapState} from "../../../Store/main/maps/mapStates/$mapState.js";
@@ -35,16 +35,17 @@ export class NotificationsPanel extends BaseComponent<{}, {}> {
 
 		return (
 			<Div style={{
-				width: 750, borderRadius: "0 0 0 5px",
-				overflow: "hidden",
+				width: 750, borderRadius: "0 0 0 5px", overflow: "hidden",
 				background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
 			}}>
 				{notifications.length == 0 && "No notifications."}
-				{notifications.length > 0 && <Column style={
+				{notifications.length > 0 &&
+				<Column style={
 					{padding: 5, display: "flex", flexFlow: "row nowrap", justifyContent: "space-between", alignItems: "center"}
 				}>
 					<span style={{fontSize: 18, fontWeight: "bold"}}>Notifications</span>
-					{unreadCount > 0 && <Button text="Mark as Read" onClick={()=>{
+					{unreadCount > 0 &&
+					<Button text="Mark all as read" onClick={()=>{
 						for (const notification of notifications_raw) {
 							if (notification.readTime == null) {
 								RunCommand_UpdateNotification({id: notification.id, updates: {
@@ -52,44 +53,39 @@ export class NotificationsPanel extends BaseComponent<{}, {}> {
 								}});
 							}
 						}
-					}}/>
-}
+					}}/>}
 				</Column>}
 				{notifications.Take(entryLimit).map((notification, index)=>{
-					return <Column style={{
-						position: "relative",
-						padding: 5,
-					}} key={notification.id}>
-						{!notification.readTime && <Div onClick={()=>{
-							RunCommand_UpdateNotification({id: notification.id, updates: {
-								readTime: Date.now(),
-							}});
-						}} style={{position: "absolute", top: 0, bottom: 0, right: 0, left: 0, backgroundColor: "rgba(0,0,0,0.15)"}}>
+					return (
+						<Column key={notification.id}
+							style={E(
+								{position: "relative", padding: 5},
+								!notification.readTime && {backgroundColor: "rgba(0,0,0,0.15)"},
+							)}
+							onClick={()=>{
+								RunCommand_UpdateNotification({id: notification.id, updates: {
+									readTime: Date.now(),
+								}});
+							}}
+						>
+							{!notification.readTime &&
 							<Div style={{
-								position: "absolute", top: 6, left: 6,
-								width: 8, height: 8,
-								stroke: "red",
-								display: "flex",
+								position: "absolute", top: 6, left: 6, width: 8, height: 8,
+								display: "flex", stroke: "red", pointerEvents: "none",
 							}}>
 								<svg width="100%" height="100%" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg">
 									<path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 								</svg>
-							</Div>
-						</Div>}
-						{notification.command &&
-							<CommandRunUI panel={false} key={index} run={notification.command!} index={index} last={index == notifications.length - 1} />
-						}
-					</Column>;
+							</Div>}
+							{notification.command &&
+							<CommandRunUI panel={true} key={index} run={notification.command!} index={index} last={index == notifications.length - 1}/>}
+						</Column>
+					);
 				})}
-				<Row style={{
-					padding: 5,
-					flexWrap: "nowrap", alignItems: "center", justifyContent: "space-between",
-				}}>
-					<Row style={{
-						display: "flex", flexWrap: "nowrap", alignItems: "center", padding: "8px 0px",
-					}}>
-						<CheckBox ml="auto" text="Paint Mode" value={mapState?.subscriptionPaintMode ?? false} onChange={val=>RunInAction_Set(this, ()=>{ if (mapState) mapState.subscriptionPaintMode = val; })} />
-						<InfoButton ml={5} text="When enabled, paint mode allows you to more simply select nodes you are subscribed to" />
+				<Row style={{padding: 5, alignItems: "center", justifyContent: "space-between"}}>
+					<Row center style={{padding: "8px 0px"}}>
+						<CheckBox ml="auto" text="Paint Mode" value={mapState?.subscriptionPaintMode ?? false} onChange={val=>RunInAction_Set(this, ()=>{ if (mapState) mapState.subscriptionPaintMode = val; })}/>
+						<InfoButton ml={5} text="When enabled, you can simply drag over nodes to set their subscription state."/>
 					</Row>
 					{mapState?.subscriptionPaintMode === true && <Row ml={10}>
 						<Text>Notification level:</Text>
