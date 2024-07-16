5226a59b620d8f8b9c9888b779571ed6788be498
Alvinosh <alvihysa89@gmail.com>
2024-07-16 17:10:31 +0200

    * Fixed Config for Dev Env

--- Packages/monitor-client/Scripts/Bin/Server.js
+++ Packages/monitor-client/Scripts/Bin/Server.js
@@ -2,6 +2,4 @@ import {Serve} from "web-vcore/Scripts_Dist/Bin/Server.js";
 import {webpackConfig} from "../Build/WebpackConfig.js";
 import {config} from "../Config.js";
 
-console.log(JSON.stringify(webpackConfig));
-
 Serve(config, webpackConfig);
--- Packages/monitor-client/rspack.config.js
+++ Packages/monitor-client/rspack.config.js
@@ -1,6 +1,5 @@
 const rspack = require("@rspack/core");
-const {DefinePlugin} = require("@rspack/core");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 /** @type {import('@rspack/core').Configuration} */
 const config = {
@@ -12,12 +11,53 @@ const config = {
     concatenateModules: false,
   },
   devServer: {
-    port: 5130,
+    devMiddleware: {
+      stats: {
+        colors: true,
+        chunks: false, // only on prod,
+        chunkModules: false, // only on prod,
+      },
+      writeToDisk: true,
+
+    },
+    port: 5131,
+    static: [
+    	{
+        directory: path.resolve(__dirname, "./Resources"),
+    	},
+    	{
+    	  directory: path.resolve(__dirname, "../client/Resources"),
+    	},
+    ],
+    historyApiFallback: {
+      index: "index.html",
+      verbose: true,
+      rewrites: [
+      	{
+          from: /^\/monitor\/.*$/,
+          to(context) {
+          	return context.parsedUrl.pathname.replace(/^\/monitor\/.*/, "/");
+          },
+      	},
+      ],
+
+    },
   },
   target: "web",
-  devtool: "cheap-source-map",
+  devtool: "source-map",
   resolve: {
-    modules: ["node_modules", "./Source"],
+    roots: [
+    	path.resolve(__dirname, "../../node_modules"),
+    	path.resolve(__dirname, "./node_modules"),
+    	path.resolve(__dirname, "../client/Resources"),
+    	path.resolve(__dirname, "./Resources"),
+    ],
+    modules: [
+    	path.resolve(__dirname, "../../node_modules"),
+    	path.resolve(__dirname, "./node_modules"),
+    	path.resolve(__dirname, "./Resources"),
+    	path.resolve(__dirname, "./Source"),
+    ],
     extensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".mjs"],
     extensionAlias: {
       ".js": [".ts", ".js", ".tsx", ".jsx"],
@@ -28,6 +68,9 @@ const config = {
     tsConfig: {
       configFile: "./tsconfig.json",
     },
+  },
+  experiments: {
+    css: true,
   },
   module: {
     rules: [
@@ -51,110 +94,79 @@ const config = {
         ],
     	},
     	{
-        test: /\.(ts?|tsx?|.js)$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
-        	{
-            loader: "builtin:swc-loader",
-            /**
-             * @type {import('@rspack/core').SwcLoaderOptions}
-             */
-            options: {
-              jsc: {
-                parser: {
-                  syntax: "typescript",
-                  decorators: true,
-                },
-              },
-            },
-        	},
-        	// extracts CSS into a separate file (with a <link> entry then being inserted into index.html, for runtime loading of it)
-        	MiniCssExtractPlugin.loader,
-        	// translates CSS into CommonJS
-        	{
-            loader: "css-loader",
-            options: {
-              url: false,
-            	//minimize: false, // cssnano already minifies
-            },
-        	},
-        	// is this needed? (I mean, I think it applies css minification, but that's not so important for a 13kb-over-CDNed-network file)
-        	{
-            loader: "postcss-loader",
-        	},
-        	// compiles Sass to CSS
         	{
             loader: "sass-loader",
             options: {
               sassOptions: {
-                includePaths: ["./Source"],
-              },
-              additionalData: (content, loaderContext)=>{
-              	// More information about available properties https://webpack.js.org/api/loaders/
-              	const {resourcePath, rootContext} = loaderContext;
-              	//const relativePath = path.relative(rootContext, resourcePath);
-              	console.log(
-                  `Sass-loader preprocessing. @resourcePath:${resourcePath}`,
-              	);
-              	//console.log(`Content:${content}`);
-
-              	//if (resourcePath.includes("node_modules/web-vcore") && resourcePath.endsWith("Main.scss") && wvcSymlinked) {
-              	/*const startPoint = content.indexOf("// [StartOfWVCMainSCSS]");
-                  const endPoint = content.includes("// [EndOfWVCMainSCSS]") ? content.indexOf("// [EndOfWVCMainSCSS]") + "// [EndOfWVCMainSCSS]".length : -1;
-                  // if wvc Main.scss file is part of this content instance, and wvc is symlinked, replace its top-level subdep imports with ones under wvc folder
-                  if (startPoint != -1 && endPoint != -1 && wvcSymlinked) {
-                    console.log("Found wvc Main.scss file. Applying fixes, since wvc is symlinked.");
-                    const wvcPart = content.slice(startPoint, endPoint);
-                    const wvcPart_fixed = wvcPart.replace(/@import "~/g, "@import \"~web-vcore/node_modules/");
-                    return content.slice(0, startPoint) + wvcPart_fixed + content.slice(endPoint);
-                  }*/
-
-              	//console.log("Includes uPlot css?:", content.includes(".u-legend th"));
-
-              	// if (
-              	// 	content.includes(
-              	// 		"web-vcore/Source/Utils/Styles/Entry_Base.scss",
-              	// 	) &&
-              	//   wvcSymlinked
-              	// ) {
-              	// 	return content.replace(
-              	// 		/Styles\/Entry_Base.scss/g,
-              	// 		"Styles/Entry_Symlinked.scss",
-              	// 	);
-              	// }
-
-              	return content;
+                includePaths: [path.resolve(__dirname, "./Source")],
               },
             },
         	},
         ],
+        type: "css",
+    	},
+    	{
+        test: /\.woff(\?.*)?$/,
+        type: "asset/inline",
+    	},
+    	{
+        test: /\.woff2(\?.*)?$/,
+        type: "asset/inline",
+    	},
+    	{
+        test: /\.otf(\?.*)?$/,
+        type: "asset/resource",
+    	},
+    	{
+        test: /\.ttf(\?.*)?$/,
+        type: "asset/inline",
+    	},
+    	{
+        test: /\.eot(\?.*)?$/,
+        type: "asset/resource",
+    	},
+    	{
+        test: /\.(png|jpg)$/,
+        type: "asset/inline",
+    	},
+    	{
+        test: /\.svg$/,
+        loader: "svg-sprite-loader",
     	},
     ],
   },
   externals: {fs: "root location"},
   entry: {
-    app: ["./Source/Main.tsx"],
+    app: [path.resolve(__dirname, "./Source/Main.tsx")],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./Dist"),
   },
   plugins: [
-  	new MiniCssExtractPlugin(),
   	new rspack.HtmlRspackPlugin({
       template: "./Source/index.html",
+      filename: "index.html",
+      inject: "body",
+      minify: false,
   	}),
-  	new DefinePlugin({
-      definitions: {
-        "globalThis.ENV": '"dev"',
-        "globalThis.DEV": "true",
-        "globalThis.PROD": "false",
-        "globalThis.TEST": "false",
-        ENV: '"dev"',
-        DEV: "true",
-        PROD: "false",
-        TEST: "false",
-        NODE_ENV: '"development"',
-        "process.env": {NODE_ENV: '"development"'},
-        __DEV__: "true",
-        __PROD__: "false",
-        __TEST__: "false",
-      },
+  	new rspack.ProgressPlugin({}),
+  	new rspack.DefinePlugin({
+      "globalThis.ENV": '"dev"',
+      "globalThis.DEV": "true",
+      "globalThis.PROD": "false",
+      "globalThis.TEST": "false",
+      ENV: '"dev"',
+      DEV: "true",
+      PROD: "false",
+      TEST: "false",
+      NODE_ENV: '"development"',
+      "process.env": {NODE_ENV: '"development"'},
+      __DEV__: "true",
+      __PROD__: "false",
+      __TEST__: "false",
   	}),
   ],
 };
