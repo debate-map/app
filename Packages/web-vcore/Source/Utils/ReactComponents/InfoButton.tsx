89b045d9ebd78958be1d75c9f3da967bf8a1a158
Venryx <venryx@gmail.com>
2025-03-10 18:37:55 -0700

    * Improved web-vcore's readme contents regarding usage of yalc/zalc. (still not great, but getting there) * Updated DefaultLoadingUI to work with new mobx-graphlink version. * Fixed error in VDateTime.

--- Packages/web-vcore/Source/Utils/ReactComponents/InfoButton.tsx
+++ Packages/web-vcore/Source/Utils/ReactComponents/InfoButton.tsx
@@ -1,4 +1,4 @@
-import {BaseComponent, cssHelper} from "react-vextensions";
+import {BaseComponent, ClassBasedStyles, cssHelper} from "react-vextensions";
 //import ReactTooltip from "react-tooltip";
 import Tooltip from "rc-tooltip";
 import {Button, ButtonProps} from "react-vcomponents";
@@ -20,7 +20,7 @@ class TooltipInfo {
 }
 const tooltips = [] as TooltipInfo[];
 
-export type InfoButtonProps = {text: string, effect?: EffectType, tooltipProps?: InTooltipProps, sel?: boolean} & ButtonProps;
+export type InfoButtonProps = {text: string, effect?: EffectType, tooltipProps?: InTooltipProps, sel?: boolean} & Omit<ButtonProps, "text">;
 
 let lastTipID = -1;
 export class InfoButton extends BaseComponent<InfoButtonProps, {}> {
@@ -58,10 +58,10 @@ export class InfoButton extends BaseComponent<InfoButtonProps, {}> {
 				{text}
 			</InTooltip>
 		);
-		const {css} = cssHelper(this);
+		//const {css} = cssHelper(this);
 		return (
 			<Tooltip placement="top" overlay={tooltip}>
-				<Button {...rest as any} size={13} iconSize={13} iconPath="/Images/Buttons/Info.png"
+				{/*<Button {...rest as any} size={13} iconSize={13} iconPath="/Images/Buttons/Info.png"
 						useOpacityForHover={true} style={css(
 							{
 								position: `relative`, zIndex: 1, marginLeft: 1, alignSelf: "center",
@@ -75,8 +75,37 @@ export class InfoButton extends BaseComponent<InfoButtonProps, {}> {
 						data-tip data-for={this.tooltip?.IDStr}>
 					{/*<ReactTooltip id={tipID} effect={effect}>
 						<Pre>{text}</Pre>
-					</ReactTooltip>*/}
-				</Button>
+					</ReactTooltip>*#/}
+				</Button>*/}
+
+				{/*<div {...rest as any}>
+					<Button size={13} iconSize={13} iconPath="/Images/Buttons/Info.png"
+						useOpacityForHover={true} style={css(
+							{
+								position: `relative`, zIndex: 1, marginLeft: 1, alignSelf: "center",
+								backgroundPosition: "center", // fsr, this is set to "-1px -1px" by default, which cuts off 1px of the image
+								backgroundColor: null, boxShadow: null, border: null,
+							},
+							//{marginTop: "auto", marginBottom: "auto"}, // maybe use this; it vertically-centers in parent, without need for {alignItems: "center"} on parent
+							style,
+						)}
+						//title={text}
+						data-tip data-for={this.tooltip?.IDStr}/>
+				</div>*/}
+
+				<div {...rest as any} data-tip data-for={this.tooltip?.IDStr}
+					className={[
+						"Button",
+						ClassBasedStyles({":hover": {opacity: 1}}),
+					].join(" ")}
+					style={E(
+						{
+							display: "inline-flex", alignItems: "center", justifyContent: "center", backgroundRepeat: "no-repeat", borderRadius: 5, color: "rgb(170,170,170)",
+							fontSize: 14, textAlign: "center", cursor: "pointer", opacity: .6, width: 13, height: 13, padding: 0, backgroundImage: `url("/Images/Buttons/Info.png")`, backgroundPosition: "center center", backgroundSize: "13px",
+							position: "relative", zIndex: 1, marginLeft: 1, alignSelf: "center",
+						},
+						style,
+					)}/>
 			</Tooltip>
 		);
 	}
