bada682f67152f2f19ae33a80ba53bdab4a66f39
rishadbaniya <rbdroid01@gmail.com>
2025-08-06 21:44:23 +0545

    refactor(Packages/client): convert HomeUI to func component

--- Packages/client/Source/UI/Home.tsx
+++ Packages/client/Source/UI/Home.tsx
@@ -1,26 +1,25 @@
+import React from "react";
 import {Switch} from "react-vcomponents";
-import {BaseComponentPlus} from "react-vextensions";
 import {store} from "Store";
-import {Observer, SubNavBar, SubNavBarButton} from "web-vcore";
+import {SubNavBar, SubNavBarButton} from "web-vcore";
 import {AboutUI} from "./Home/About.js";
 import {HomeUI2} from "./Home/Home.js";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class HomeUI extends BaseComponentPlus({} as {}, {}) {
-	render() {
-		const currentSubpage = store.main.home.subpage;
-		const page = "home";
-		return (
-			<>
-				<SubNavBar>
-					<SubNavBarButton page={page} subpage='home' text='Home'/>
-					<SubNavBarButton page={page} subpage='about' text='About'/>
-				</SubNavBar>
-				<Switch>
-					<HomeUI2/>
-					{currentSubpage === "about" && <AboutUI/>}
-				</Switch>
-			</>
-		);
-	}
-}
+export const HomeUI = observer_mgl(()=>{
+	const currentSubpage = store.main.home.subpage;
+	const page = "home";
+
+	return (
+		<>
+			<SubNavBar>
+				<SubNavBarButton page={page} subpage='home' text='Home'/>
+				<SubNavBarButton page={page} subpage='about' text='About'/>
+			</SubNavBar>
+			<Switch>
+				<HomeUI2/>
+				{currentSubpage === "about" && <AboutUI/>}
+			</Switch>
+		</>
+	);
+});
