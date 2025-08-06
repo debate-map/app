98bcb9542f9110cd69a589b3f1b04272656ba0c4
rishadbaniya <rbdroid01@gmail.com>
2025-08-06 13:13:01 +0545

    refactor(Packages/client): move LInkPreserver and all NavBar components to functional

--- Packages/client/Source/UI/@Root/LinkPreserver.tsx
+++ Packages/client/Source/UI/@Root/LinkPreserver.tsx
@@ -1,6 +1,5 @@
-import {BaseComponent} from "react-vextensions";
-import {Observer} from "web-vcore";
 import {gql, useSubscription} from "@apollo/client";
+import {observer_mgl} from "mobx-graphlink";
 
 export const LINK_PRESERVER_SUBSCRIPTION = gql`
 subscription($input: LinkPreserverInput!) {
@@ -10,28 +9,22 @@ subscription($input: LinkPreserverInput!) {
 	}
 }
 `;
+
 type LinkPreserverResult = {alive: boolean, pageRefreshRequested: boolean};
 
-@Observer
-export class LinkPreserver extends BaseComponent<{}, {}> {
-	render() {
-		let {} = this.props;
-
-		// Choose 45s as our update-interval; this avoids Cloudflare's "100 seconds of dormancy" timeout. (https://community.cloudflare.com/t/cloudflare-websocket-timeout/5865)
-		// (we use a <60s interval, so that it will reliably hit each 60s timer-interval that Chrome 88+ allows for hidden pages: https://developer.chrome.com/blog/timer-throttling-in-chrome-88/#intensive-throttling)
-		const updateInterval = 45000;
-
-		const {data, loading} = useSubscription(LINK_PRESERVER_SUBSCRIPTION, {
-			variables: {input: {updateInterval}},
-			onData: info=>{
-				const {alive, pageRefreshRequested} = info.data.data.linkPreserver as LinkPreserverResult;
-				if (pageRefreshRequested) {
-					console.log("Refreshing page due to server request.");
-					window.location.reload();
-				}
-			},
-		});
-
-		return null;
-	}
-}
+export const LinkPreserver = observer_mgl(()=>{
+	// Choose 45s as our update-interval; this avoids Cloudflare's "100 seconds of dormancy" timeout. (https://community.cloudflare.com/t/cloudflare-websocket-timeout/5865)
+	// (we use a <60s interval, so that it will reliably hit each 60s timer-interval that Chrome 88+ allows for hidden pages: https://developer.chrome.com/blog/timer-throttling-in-chrome-88/#intensive-throttling)
+	const updateInterval = 45000;
+	useSubscription(LINK_PRESERVER_SUBSCRIPTION, {
+		variables: {input: {updateInterval}},
+		onData: info=>{
+			const {pageRefreshRequested} = info.data.data.linkPreserver as LinkPreserverResult;
+			if (pageRefreshRequested) {
+				console.log("Refreshing page due to server request.");
+				window.location.reload();
+			}
+		},
+	});
+	return null;
+})
