ffac55afb8781a2ef91bbd24b797a674329add42
rishadbaniya <rbdroid01@gmail.com>
2025-08-07 12:41:46 +0545

    refactor(Package/client): convert all components of Users, Root and some components of RootStyles to functional

--- Packages/client/Source/UI/@Root/RootStyles.tsx
+++ Packages/client/Source/UI/@Root/RootStyles.tsx
@@ -1,63 +1,57 @@
 import {MeID} from "dm_common";
-import React from "react";
+import React, {useEffect} from "react";
 import {GetUserBackground} from "Store/db_ext/users/$user";
 import {liveSkin} from "Utils/Styles/SkinManager.js";
-import {Observer} from "web-vcore";
-import {BaseComponent} from "react-vextensions";
+import {observer_mgl} from "mobx-graphlink";
 
 //export const Button_background_dark = Chroma("rgba(90,100,110,0.9)").css(); // same as hover color, except .9 alpha instead of .8
 //export const Button_background_dark = Chroma(Button_styles.root.backgroundColor).darken(chroma_maxDarken * .05).css();
+// It is defined in user-project rather than web-vcore, so that it can use observer_mgl (ie. evaluate mobx-fields) while determining its final css.
+export const RootStyles = observer_mgl(()=>{
+	const background = GetUserBackground(MeID());
+	const firstExtantBackgroundURL_1920Plus = background.url_1920 || background.url_3840 || background.url_max;
+	const firstExtantBackgroundURL_3840Plus = background.url_3840 || background.url_max;
+	const firstExtantBackgroundURL_max = background.url_max;
+	const skin = liveSkin;
 
-// This class is defined in user-project rather than web-vcore, so that it can use @Observer (ie. evaluate mobx-fields) while determining its final css.
-@Observer
-export class RootStyles extends BaseComponent<{}, {}> {
-	ComponentWillMount() {
-		const skin = liveSkin;
+	useEffect(()=>{
 		skin.CSSHooks_Freeform();
-	}
-	loadingUI = ()=>null;
-	render() {
-		const background = GetUserBackground(MeID());
-		const firstExtantBackgroundURL_1920Plus = background.url_1920 || background.url_3840 || background.url_max;
-		const firstExtantBackgroundURL_3840Plus = background.url_3840 || background.url_max;
-		const firstExtantBackgroundURL_max = background.url_max;
-		const skin = liveSkin;
+	}, []);
 
-		return (
-			<style>{`
+	return (
+		<style>{`
 			${skin.RawCSS_ApplyScalarsAndStyles()}
 			${skin.RawCSS_Freeform()}
-
 			.background {
-			${
-				firstExtantBackgroundURL_1920Plus?.startsWith("background: ")
-					? `background: ${firstExtantBackgroundURL_1920Plus.replace("background: ", "")}`
-					: `background-color: ${background.color};
-						background-image: url(${firstExtantBackgroundURL_1920Plus});
-						background-position: ${background.position || "center center"};
-						background-size: ${background.size || "cover"};`
-			}
+				${
+					firstExtantBackgroundURL_1920Plus?.startsWith("background: ")
+						? `background: ${firstExtantBackgroundURL_1920Plus.replace("background: ", "")}`
+						: `
+							background-color: ${background.color};
+							background-image: url(${firstExtantBackgroundURL_1920Plus});
+							background-position: ${background.position || "center center"};
+							background-size: ${background.size || "cover"};
+						`
+				}
 			}
 			@media (min-width: 1921px) {
 				.background {
-				${
-					firstExtantBackgroundURL_3840Plus?.startsWith("background: ")
-						? `background: ${firstExtantBackgroundURL_3840Plus.replace("background: ", "")}`
-						: `background-image: url(${firstExtantBackgroundURL_3840Plus});`
-				}
+					${
+						firstExtantBackgroundURL_3840Plus?.startsWith("background: ")
+							? `background: ${firstExtantBackgroundURL_3840Plus.replace("background: ", "")}`
+							: `background-image: url(${firstExtantBackgroundURL_3840Plus});`
+					}
 				}
 			}
 			@media (min-width: 3841px) {
 				.background {
-				${
-					firstExtantBackgroundURL_max?.startsWith("background: ")
-						? `background: ${firstExtantBackgroundURL_max.replace("background: ", "")}`
-						: `background-image: url(${firstExtantBackgroundURL_max});`
-				}
+					${
+						firstExtantBackgroundURL_max?.startsWith("background: ")
+							? `background: ${firstExtantBackgroundURL_max.replace("background: ", "")}`
+							: `background-image: url(${firstExtantBackgroundURL_max});`
+					}
 				}
 			}
-
-			`}</style>
-		);
-	}
-}
+		`}</style>
+	);
+});
--- Packages/client/Source/UI/Database/Users.tsx
+++ Packages/client/Source/UI/Database/Users.tsx
@@ -1,8 +1,7 @@
-import {BaseComponent, BaseComponentPlus, cssHelper} from "react-vextensions";
 import {Row, Column} from "react-vcomponents";
 import Moment from "moment";
 import {ScrollView} from "react-vscrollview";
-import {Link, PageContainer, Observer, ES, AddNotificationMessage} from "web-vcore";
+import {Link, PageContainer, ES, css2} from "web-vcore";
 import {GetSelectedUser} from "Store/main/database";
 import {ToNumber} from "js-vextensions";
 import {GetUsers, GetUser, User} from "dm_common";
@@ -10,6 +9,8 @@ import {liveSkin} from "Utils/Styles/SkinManager";
 import {useMemo, useState} from "react";
 import {UserProfileUI} from "./Users/UserProfile.js";
 import {ColumnData, TableData, TableHeader} from "../@Shared/TableHeader/TableHeader.js";
+import {observer_mgl} from "mobx-graphlink";
+import React from "react";
 
 const columns: ColumnData[] = [{
 	key: "displayName" as const,
@@ -43,48 +44,43 @@ const columns: ColumnData[] = [{
 	allowSort: true,
 }];
 
-@Observer
-export class UsersUI extends BaseComponentPlus({} as {}, {}) {
-	render() {
-		const users = GetUsers();
-		// const userExtraInfoMap = GetUserExtraInfoMap();
-		const selectedUser = GetSelectedUser();
+export const UsersUI = observer_mgl(()=>{
+	const users = GetUsers();
+	const selectedUser = GetSelectedUser();
+	const [tableData, setTableData] = useState({columnSort: "", columnSortDirection: "", filters: []} as TableData);
 
-		// if (userExtraInfoMap == null) return <div/>;
-		if (selectedUser) {
-			return <UserProfileUI user={selectedUser}/>;
-		}
-
-		const onTableChange = (tableData:TableData)=>{
-			setTableData({
-				columnSort: tableData.columnSort,
-				columnSortDirection: tableData.columnSortDirection,
-				filters: [...tableData.filters],
-			});
-		};
-		const [tableData, setTableData] = useState({columnSort: "", columnSortDirection: "", filters: []} as TableData);
-
-		const sortedAndFilteredUsers = useMemo(()=>{
-			return sortAndFilterUsers(users, tableData);
-		}, [users, tableData]);
-
-		return (
-			<PageContainer style={{padding: 0, background: null}}>
-				<TableHeader columns={columns} onTableChange={onTableChange} tableData={tableData}/>
-				<ScrollView style={ES({flex: 1})} contentStyle={ES({
-					flex: 1, background: liveSkin.BasePanelBackgroundColor().alpha(1).css(), borderRadius: "0 0 10px 10px",
-				})}>
-					{users.length == 0 && <div style={{textAlign: "center", fontSize: 18}}>Loading...</div>}
-					{sortedAndFilteredUsers.map((user, index)=>{
-						return <UserRow key={user.id} index={index} last={index == sortedAndFilteredUsers.length - 1} user={user}/>;
-					})}
-				</ScrollView>
-			</PageContainer>
-		);
+	if (selectedUser) {
+		return <UserProfileUI user={selectedUser}/>;
 	}
-}
 
-function sortAndFilterUsers(users: User[], tableData: TableData) {
+	const onTableChange = (tD: TableData)=>{
+		setTableData({
+			columnSort: tD.columnSort,
+			columnSortDirection: tD.columnSortDirection,
+			filters: [...tD.filters],
+		});
+	};
+
+	const sortedAndFilteredUsers = useMemo(()=>{
+		return sortAndFilterUsers(users, tableData);
+	}, [users, tableData]);
+
+	return (
+		<PageContainer style={{padding: 0, background: undefined}}>
+			<TableHeader columns={columns} onTableChange={onTableChange} tableData={tableData}/>
+			<ScrollView style={ES({flex: 1})} contentStyle={ES({
+				flex: 1, background: liveSkin.BasePanelBackgroundColor().alpha(1).css(), borderRadius: "0 0 10px 10px",
+			})}>
+				{users.length == 0 && <div style={{textAlign: "center", fontSize: 18}}>Loading...</div>}
+				{sortedAndFilteredUsers.map((user, index)=>{
+					return <UserRow key={user.id} index={index} last={index == sortedAndFilteredUsers.length - 1} user={user}/>;
+				})}
+			</ScrollView>
+		</PageContainer>
+	);
+});
+
+const sortAndFilterUsers = (users: User[], tableData: TableData)=>{
 	let output: User[] = users.slice();
 
 	// start with the "default sorting"
@@ -148,30 +144,24 @@ function sortAndFilterUsers(users: User[], tableData: TableData) {
 	return output;
 }
 
-@Observer
-export class UserRow extends BaseComponent<{index: number, last: boolean, user: User}, {}> {
-	render() {
-		const {index, last, user} = this.props;
-
-		let {displayName} = user;
-		if (displayName.includes("@")) displayName = displayName.split("@")[0];
-		const {css} = cssHelper(this);
-		return (
-			<Column p="7px 10px" style={css(
-				{background: index % 2 == 0 ? liveSkin.ListEntryBackgroundColor_Light().css() : liveSkin.ListEntryBackgroundColor_Dark().css()},
-				last && {borderRadius: "0 0 10px 10px"},
-			)}>
-				<Row>
-					<Link text={displayName} actionFunc={s=>s.main.database.selectedUserID = user.id} style={{flex: columns[0].width, fontSize: 17}}/>
-					{/* <span style={{ flex: columnWidths[0] }}>{displayName}</span> */}
-					<span style={{flex: columns[1].width}}>{Moment(user.joinDate).format("YYYY-MM-DD")}</span>
-					<span style={{flex: columns[2].width}}>{user.edits || 0}</span>
-					<span style={{flex: columns[3].width}}>{user.lastEditAt ? Moment(user.lastEditAt).format("YYYY-MM-DD") : "n/a"}</span>
-					<span style={{flex: columns[4].width}}>
-						{["basic", "verified", "mod", "admin"].filter(a=>(user.permissionGroups || {})[a]).map(a=>a.replace(/^./, ch=>ch.toUpperCase())).join(", ")}
-					</span>
-				</Row>
-			</Column>
-		);
-	}
-}
+export const UserRow = observer_mgl(({ index, last, user }: { index: number, last: boolean, user: User })=>{
+	let {displayName} = user;
+	if (displayName.includes("@")) displayName = displayName.split("@")[0];
+	const css = css2;
+	return (
+		<Column p="7px 10px" style={css(
+			{background: index % 2 == 0 ? liveSkin.ListEntryBackgroundColor_Light().css() : liveSkin.ListEntryBackgroundColor_Dark().css()},
+			last && {borderRadius: "0 0 10px 10px"},
+		)}>
+			<Row>
+				<Link text={displayName} actionFunc={s=>s.main.database.selectedUserID = user.id} style={{flex: columns[0].width, fontSize: 17}}/>
+				<span style={{flex: columns[1].width}}>{Moment(user.joinDate).format("YYYY-MM-DD")}</span>
+				<span style={{flex: columns[2].width}}>{user.edits || 0}</span>
+				<span style={{flex: columns[3].width}}>{user.lastEditAt ? Moment(user.lastEditAt).format("YYYY-MM-DD") : "n/a"}</span>
+				<span style={{flex: columns[4].width}}>
+					{["basic", "verified", "mod", "admin"].filter(a=>(user.permissionGroups || {})[a]).map(a=>a.replace(/^./, ch=>ch.toUpperCase())).join(", ")}
+				</span>
+			</Row>
+		</Column>
+	);
+});
