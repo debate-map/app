c9e01ce3e9985f34abe6a4fcb01ab77a0b2710fa
rishadbaniya <rbdroid01@gmail.com>
2025-08-07 13:02:43 +0545

    refactor(Package/client): convert DatabaseUI to functional component

--- Packages/client/Source/UI/Database.tsx
+++ Packages/client/Source/UI/Database.tsx
@@ -1,36 +1,34 @@
 import {Switch} from "react-vcomponents";
-import {BaseComponentPlus} from "react-vextensions";
 import {store} from "Store";
-import {Observer, SubNavBar, SubNavBarButton} from "web-vcore";
+import {SubNavBar, SubNavBarButton} from "web-vcore";
 import React from "react";
 import {MediasUI} from "./Database/MediasUI.js";
 import {TermsUI} from "./Database/TermsUI.js";
 import {UsersUI} from "./Database/Users.js";
 import {PoliciesUI} from "./Database/PoliciesUI.js";
 import {SubscriptionsPaginatedUI} from "./Database/SubscriptionsPaginatedUI.js";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class DatabaseUI extends BaseComponentPlus({} as {}, {}) {
-	render() {
-		const currentSubpage = store.main.database.subpage;
-		const page = "database";
-		return (
-			<>
-				<SubNavBar>
-					<SubNavBarButton page={page} subpage="users" text="Users" actionFuncIfAlreadyActive={s=>s.main.database.selectedUserID = null}/>
-					<SubNavBarButton page={page} subpage="terms" text="Terms" /* actionIfAlreadyActive={() => new ACTTermSelect({ id: null })} *//>
-					<SubNavBarButton page={page} subpage="media" text="Media" /* actionIfAlreadyActive={() => new ACTImageSelect({ id: null })} *//>
-					<SubNavBarButton page={page} subpage="policies" text="Policies"/>
-					<SubNavBarButton page={page} subpage="subscriptions" text="Subscriptions"/>
-				</SubNavBar>
-				<Switch>
-					<UsersUI/>
-					{currentSubpage == "terms" && <TermsUI/>}
-					{currentSubpage == "media" && <MediasUI/>}
-					{currentSubpage == "policies" && <PoliciesUI/>}
-					{currentSubpage == "subscriptions" && <SubscriptionsPaginatedUI/>}
-				</Switch>
-			</>
-		);
-	}
-}
+export const DatabaseUI = observer_mgl(()=>{
+	const currentSubpage = store.main.database.subpage;
+	const page = "database";
+
+	return (
+		<>
+			<SubNavBar>
+				<SubNavBarButton page={page} subpage="users" text="Users" actionFuncIfAlreadyActive={s=>s.main.database.selectedUserID = null}/>
+				<SubNavBarButton page={page} subpage="terms" text="Terms" /* actionIfAlreadyActive={() => new ACTTermSelect({ id: null })} *//>
+				<SubNavBarButton page={page} subpage="media" text="Media" /* actionIfAlreadyActive={() => new ACTImageSelect({ id: null })} *//>
+				<SubNavBarButton page={page} subpage="policies" text="Policies"/>
+				<SubNavBarButton page={page} subpage="subscriptions" text="Subscriptions"/>
+			</SubNavBar>
+			<Switch>
+				<UsersUI/>
+				{currentSubpage == "terms" && <TermsUI/>}
+				{currentSubpage == "media" && <MediasUI/>}
+				{currentSubpage == "policies" && <PoliciesUI/>}
+				{currentSubpage == "subscriptions" && <SubscriptionsPaginatedUI/>}
+			</Switch>
+		</>
+	);
+});
