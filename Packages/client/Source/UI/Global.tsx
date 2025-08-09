21f7775d483827e40713b8df4a8c773275c68563
rishadbaniya <rbdroid01@gmail.com>
2025-08-09 18:49:23 +0545

    refactor(Packages/client): convert MapListUI and basic Forum, Global components to functional

--- Packages/client/Source/UI/Global.tsx
+++ Packages/client/Source/UI/Global.tsx
@@ -1,22 +1,13 @@
 import {Switch} from "react-vcomponents";
-import {BaseComponentPlus} from "react-vextensions";
-import {store} from "Store";
-import {SubNavBar, SubNavBarButton} from "web-vcore";
 import {GlobalMapUI} from "./Global/GlobalMapUI.js";
+import React from "react";
 
-export class GlobalUI extends BaseComponentPlus({} as {}, {}) {
-	render() {
-		/*const currentSubpage = store.main.global.subpage;
-		const page = "global";*/
-		return (
-			<>
-				{/*<SubNavBar>
-					<SubNavBarButton page={page} subpage="map" text="Map"/>
-				</SubNavBar>*/}
-				<Switch>
-					<GlobalMapUI/>
-				</Switch>
-			</>
-		);
-	}
-}
+export const GlobalUI = ()=>{
+	return (
+	    <>
+			<Switch>
+				<GlobalMapUI/>
+			</Switch>
+	    </>
+	)
+};
