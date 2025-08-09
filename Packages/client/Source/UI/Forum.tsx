0dd5992f121c9e4fb20a3a1eee0e2321d2928fa5
rishadbaniya <rbdroid01@gmail.com>
2025-08-09 18:49:23 +0545

    refactor(Packages/client): convert MapListUI and basic Forum, Global components to functional

--- Packages/client/Source/UI/Forum.tsx
+++ Packages/client/Source/UI/Forum.tsx
@@ -1,11 +1,3 @@
-// import { ForumUI as ForumUI_Base } from 'firebase-forum';
-import {BaseComponent} from "react-vextensions";
-
-export class ForumUI extends BaseComponent<{}, {}> {
-	render() {
-		/* return (
-			<ForumUI_Base/>
-		); */
-		return null;
-	}
-}
+export const ForumUI = ()=>{
+	return null;
+};
