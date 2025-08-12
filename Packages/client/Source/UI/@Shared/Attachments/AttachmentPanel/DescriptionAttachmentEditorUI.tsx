1d0befdd96b30461d564a6513b7c2eeecfac1b33
rishadbaniya <rbdroid01@gmail.com>
2025-08-12 18:49:03 +0545

    refactor(Packages/client): convert DescriptionAttachmentEditorUI and EquationEditorUI to functional component

--- Packages/client/Source/UI/@Shared/Attachments/AttachmentPanel/DescriptionAttachmentEditorUI.tsx
+++ Packages/client/Source/UI/@Shared/Attachments/AttachmentPanel/DescriptionAttachmentEditorUI.tsx
@@ -1,26 +1,31 @@
 import {DescriptionAttachment} from "dm_common";
-import {DetailsUI_Base} from "UI/@Shared/DetailsUI_Base.js";
+import {DetailsUIBaseProps, useDetailsUI} from "UI/@Shared/DetailsUI_Base.js";
 import {Column, Pre} from "react-vcomponents";
 import {MarkdownEditor, MarkdownToolbar} from "react-vmarkdown";
+import {useRef} from "react";
+import React from "react";
 
-export class DescriptionAttachmentEditorUI extends DetailsUI_Base<DescriptionAttachment, DescriptionAttachmentEditorUI> {
-	render() {
-		const {baseData} = this.props;
-		const {newData} = this.state;
-		const {Change, enabled} = this.helpers;
+export const DescriptionAttachmentEditorUI = (props: DetailsUIBaseProps<DescriptionAttachment, {}>)=>{
+	const {phase, baseData, onChange} = props;
+	const {newData, helpers} = useDetailsUI<DescriptionAttachment>({
+		baseData,
+		phase,
+		onChange,
+	});
+	const {enabled, Change} = helpers;
+	const editorRef = useRef<MarkdownEditor>(null);
 
-		return (
-			<Column>
-				<Column mt={/*showPreview ? 5 :*/ 0}>
-					<Pre>Quote text: </Pre>
-					{enabled && <MarkdownToolbar editor={()=>this.refs.editor} excludeCommands={["h1", "h2", "h3", "h4", "quote"]}/>}
-					<MarkdownEditor ref="editor" toolbar={false} value={newData.text} onChange={val=>Change(newData.text = val)} options={{
-						scrollbarStyle: "overlay",
-						lineWrapping: true,
-						readOnly: !enabled,
-					}}/>
-				</Column>
+	return (
+		<Column>
+			<Column mt={0}>
+				<Pre>Quote text: </Pre>
+				{enabled && <MarkdownToolbar editor={()=>editorRef.current} excludeCommands={["h1", "h2", "h3", "h4", "quote"]}/>}
+				<MarkdownEditor ref={editorRef} toolbar={false} value={newData.text} onChange={val=>Change(newData.text = val)} options={{
+					scrollbarStyle: "overlay",
+					lineWrapping: true,
+					readOnly: !enabled,
+				}}/>
 			</Column>
-		);
-	}
+		</Column>
+	);
 }
--- Packages/client/Source/UI/@Shared/Attachments/AttachmentPanel/EquationEditorUI.tsx
+++ Packages/client/Source/UI/@Shared/Attachments/AttachmentPanel/EquationEditorUI.tsx
@@ -1,47 +1,38 @@
-import {GetErrorMessagesUnderElement, Clone, CloneWithPrototypes} from "js-vextensions";
 import {CheckBox, Column, Pre, Row, RowLR, TextArea, TextInput} from "react-vcomponents";
-import {BaseComponent, GetDOM} from "react-vextensions";
-import {Attachment, EquationAttachment} from "dm_common";
-import {DetailsUI_Base} from "UI/@Shared/DetailsUI_Base";
+import {EquationAttachment} from "dm_common";
+import {DetailsUIBaseProps, useDetailsUI} from "UI/@Shared/DetailsUI_Base";
+import React from "react";
 
-export class EquationEditorUI extends DetailsUI_Base<EquationAttachment, {newData: EquationAttachment}> {
-	render() {
-		const {phase, style, onChange} = this.props;
-		const {newData} = this.state;
-		const {Change, enabled} = this.helpers;
+export const EquationEditorUI = (props: DetailsUIBaseProps<EquationAttachment, {}>)=>{
+	const {phase, style, onChange} = props;
+	const {newData, helpers} = useDetailsUI<EquationAttachment>({
+		baseData: props.baseData,
+		phase,
+		onChange,
+		getNewDataPostProcess: nD=>{ if (!nD.isStep) delete nD.explanation; }
+	});
+	const {Change, enabled} = helpers;
 
-		const splitAt = 100; // , width = 600;
-		return (
-			<Column style={style}>
-				<RowLR splitAt={splitAt}>
-					<Pre>LaTeX: </Pre>
-					<CheckBox enabled={enabled} style={{width: "100%"}}
-						value={newData.latex ?? false} onChange={val=>Change(val ? newData.latex = true : delete newData.latex)}/>
-				</RowLR>
+	const splitAt = 100;
+	return (
+		<Column style={style}>
+			<RowLR splitAt={splitAt}>
+				<Pre>LaTeX: </Pre>
+				<CheckBox enabled={enabled} style={{width: "100%"}} value={newData.latex ?? false} onChange={val=>Change(val ? newData.latex = true : delete newData.latex)}/>
+			</RowLR>
+			<RowLR mt={5} splitAt={splitAt}>
+				<Pre>Text: </Pre>
+				<TextArea required enabled={enabled} allowLineBreaks={newData.latex} autoSize={true} style={{width: "100%"}} value={newData.text} onChange={val=>Change(newData.text = val)}/>
+			</RowLR>
+			<Row mt={5} style={{display: "flex", alignItems: "center"}}>
+				<Pre>Step in series: </Pre>
+				<CheckBox enabled={enabled} value={newData.isStep ?? false} onChange={val=>Change(newData.isStep = val)}/>
+			</Row>
+			{newData.isStep &&
 				<RowLR mt={5} splitAt={splitAt}>
-					<Pre>Text: </Pre>
-					<TextArea required enabled={enabled} allowLineBreaks={newData.latex} autoSize={true} style={{width: "100%"}}
-						value={newData.text} onChange={val=>Change(newData.text = val)}/>
-				</RowLR>
-				<Row mt={5} style={{display: "flex", alignItems: "center"}}>
-					<Pre>Step in series: </Pre>
-					<CheckBox enabled={enabled} value={newData.isStep ?? false}
-						// onChange={val=>Change(val ? newLinkData.isStep = true : delete newLinkData.isStep)}/>
-						onChange={val=>Change(newData.isStep = val)}/>
-				</Row>
-				{newData.isStep &&
-					<RowLR mt={5} splitAt={splitAt}>
-						<Pre>Explanation: </Pre>
-						<TextInput enabled={enabled} style={{width: "100%"}}
-							value={newData.explanation} onChange={val=>Change(newData.explanation = val)}/>
-					</RowLR>}
-			</Column>
-		);
-	}
-
-	GetNewData_PostProcess(newData: EquationAttachment) {
-		if (!newData.isStep) {
-			delete newData.explanation;
-		}
-	}
-}
+					<Pre>Explanation: </Pre>
+					<TextInput enabled={enabled} style={{width: "100%"}} value={newData.explanation} onChange={val=>Change(newData.explanation = val)}/>
+				</RowLR>}
+		</Column>
+	);
+};
