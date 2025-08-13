25d2b795ec202d85a4b12fa0152e9f28f67045e5
rishadbaniya <rbdroid01@gmail.com>
2025-08-13 21:17:23 +0545

    refactor(Packages/client): convert GenericEntryInfoUI and components within UserPanel and MediaDetailsUI to functional

--- Packages/client/Source/UI/@Shared/CommonPropUIs/GenericEntryInfoUI.tsx
+++ Packages/client/Source/UI/@Shared/CommonPropUIs/GenericEntryInfoUI.tsx
@@ -1,64 +1,69 @@
 import Moment from "moment";
-import {Column, Pre, Row, Text} from "react-vcomponents";
-import {BaseComponent, BaseComponentPlus} from "react-vextensions";
-import {Link, Observer} from "web-vcore";
+import {Column, Row, Text} from "react-vcomponents";
+import {Link} from "web-vcore";
 import {E} from "js-vextensions";
 import {GetAccessPolicy, GetUser} from "dm_common";
+import {observer_mgl} from "mobx-graphlink";
+import React from "react";
 
-@Observer
-export class GenericEntryInfoUI extends BaseComponentPlus({singleLine: false} as {
-	id: string | number,
-	creatorID: string, createdAt: number,
-	accessPolicyID?: string, accessPolicyButton?: JSX.Element,
-	singleLine?: boolean,
-}, {}) {
-	render() {
-		const {id, creatorID, createdAt, accessPolicyID, accessPolicyButton, singleLine} = this.props;
-		const creator = GetUser(creatorID);
-		const accessPolicy = accessPolicyID ? GetAccessPolicy(accessPolicyID) : null;
-
-		const createdAtTimeStr = Moment(createdAt).format("YYYY-MM-DD HH:mm:ss");
-		const userLink = (
-			<Link text={creator == null ? "n/a" : creator.displayName}
-				actionFunc={s=>{
-					if (creator != null) {
-						s.main.page = "database";
-						s.main.database.subpage = "users";
-						s.main.database.selectedUserID = creatorID;
-					}
-				}}/>
-		);
-		const accessPolicyLink = accessPolicy == null ? null : (
-			<Link text={accessPolicy.name}
-				actionFunc={s=>{
-					s.main.page = "database";
-					s.main.database.subpage = "policies";
-					s.main.database.selectedPolicyID = accessPolicyID;
-				}}/>
-		);
-
-		return (
-			<Column sel style={E(singleLine && {fontSize: 14})}>
-				{singleLine &&
-					<Row center>
-						<Text>ID: {id} ({createdAtTimeStr}, by: </Text>
-						{userLink}
-						<Text>)</Text>
-					</Row>}
-				{!singleLine &&
-				<>
-					<Row>ID: {id}</Row>
-					<Row center>
-						<Text>Created at: {createdAtTimeStr} (by: </Text>
-						{userLink}
-						<Text>)</Text>
-					</Row>
-					{accessPolicy &&
-					<Row center>
-						<Text>Access-policy: </Text>{accessPolicyLink}{accessPolicyButton}
-					</Row>}
-				</>}
-			</Column>
-		);
-	}
+export type GenericEntryInfoUIProps = {
+	id: string | number;
+	creatorID: string;
+	createdAt: number;
+	accessPolicyID?: string;
+	accessPolicyButton?: React.JSX.Element;
+	singleLine?: boolean;
 }
+
+export const GenericEntryInfoUI = observer_mgl((props: GenericEntryInfoUIProps)=>{
+	const {id, creatorID, createdAt, accessPolicyID, accessPolicyButton, singleLine = false} = props;
+
+	const creator = GetUser(creatorID);
+	const accessPolicy = accessPolicyID ? GetAccessPolicy(accessPolicyID) : null;
+
+	const createdAtTimeStr = Moment(createdAt).format("YYYY-MM-DD HH:mm:ss");
+	const userLink = (
+		<Link text={creator == null ? "n/a" : creator.displayName}
+			actionFunc={s=>{
+				if (creator != null) {
+					s.main.page = "database";
+					s.main.database.subpage = "users";
+					s.main.database.selectedUserID = creatorID;
+				}
+			}}/>
+	);
+
+	const accessPolicyLink = accessPolicy == null ? null : (
+		<Link text={accessPolicy.name}
+			actionFunc={s=>{
+				s.main.page = "database";
+				s.main.database.subpage = "policies";
+				s.main.database.selectedPolicyID = accessPolicyID;
+			}}/>
+	);
+
+	return (
+		<Column sel style={E(singleLine && {fontSize: 14})}>
+			{singleLine &&
+				<Row center>
+					<Text>ID: {id} ({createdAtTimeStr}, by: </Text>
+					{userLink}
+					<Text>)</Text>
+				</Row>}
+
+			{!singleLine &&
+			<>
+				<Row>ID: {id}</Row>
+				<Row center>
+					<Text>Created at: {createdAtTimeStr} (by: </Text>
+					{userLink}
+					<Text>)</Text>
+				</Row>
+				{accessPolicy &&
+				<Row center>
+					<Text>Access-policy: </Text>{accessPolicyLink}{accessPolicyButton}
+				</Row>}
+			</>}
+		</Column>
+	);
+})
--- Packages/client/Source/UI/Database/Medias/MediaDetailsUI.tsx
+++ Packages/client/Source/UI/Database/Medias/MediaDetailsUI.tsx
@@ -1,88 +1,91 @@
-import {AddMedia, GetAccessPolicy, GetNiceNameForMediaType, GetUserHidden, Media, MediaType, Media_namePattern, MeID, GetFinalAccessPolicyForNewEntry} from "dm_common";
+import {GetAccessPolicy, GetNiceNameForMediaType, Media, MediaType, Media_namePattern, GetFinalAccessPolicyForNewEntry} from "dm_common";
 import React from "react";
 import {GenericEntryInfoUI} from "UI/@Shared/CommonPropUIs/GenericEntryInfoUI.js";
-import {DetailsUI_Base} from "UI/@Shared/DetailsUI_Base.js";
-import {ES, HSLA, Observer, observer_simple, ParseYoutubeVideoID, YoutubePlayerUI} from "web-vcore";
+import {DetailsUIBaseProps, useDetailsUI} from "UI/@Shared/DetailsUI_Base.js";
+import {ES, HSLA, ParseYoutubeVideoID, YoutubePlayerUI} from "web-vcore";
 import {E, GetEntries} from "js-vextensions";
 import {GetAsync, observer_mgl} from "mobx-graphlink";
 import {Button, Column, Pre, Row, RowLR, Select, Span, TextInput} from "react-vcomponents";
 import {BoxController, ShowMessageBox} from "react-vmessagebox";
-import {ScrollView} from "react-vscrollview";
-import {observer} from "mobx-react";
 import {RunCommand_AddMedia} from "Utils/DB/Command.js";
 import {PolicyPicker} from "../Policies/PolicyPicker.js";
-import {store} from "../../../Store/index.js";
 
-@Observer
-export class MediaDetailsUI extends DetailsUI_Base<Media, MediaDetailsUI> {
-	scrollView: ScrollView;
-	render() {
-		const {baseData, style, onChange} = this.props;
-		const {newData, dataError} = this.state;
-		const {Change, creating, editing} = this.helpers;
-		const videoID = ParseYoutubeVideoID(newData.url);
-		const accessPolicy = GetAccessPolicy(newData.accessPolicy);
-		const enabled = creating || editing;
+export type MediaDetailsUIProps = DetailsUIBaseProps<Media, {}>;
 
-		const splitAt = 100;
-		//const width = 600;
-		return (
-			<Column style={style}>
-				{!creating &&
-					<GenericEntryInfoUI id={baseData.id} creatorID={newData.creator} createdAt={newData.createdAt}/>}
-				<RowLR mt={5} splitAt={splitAt}>
-					<Pre>Name: </Pre>
-					<TextInput
-						pattern={Media_namePattern} required
-						enabled={enabled} style={{width: "100%"}}
-						value={newData.name} onChange={val=>Change(newData.name = val)}/>
-				</RowLR>
-				<RowLR mt={5} splitAt={splitAt}>
-					<Pre>Type: </Pre>
-					<Select options={GetEntries(MediaType, name=>GetNiceNameForMediaType(MediaType[name]))} enabled={enabled} style={ES({flex: 1})}
-						value={newData.type} onChange={val=>Change(newData.type = val)}/>
-				</RowLR>
-				<RowLR mt={5} splitAt={splitAt}>
-					<Pre>URL: </Pre>
-					<TextInput
-						/*pattern={Media_urlPattern}*/ required
-						enabled={enabled} style={{width: "100%"}}
-						value={newData.url} onChange={val=>Change(newData.url = val)}/>
-					{newData.type == MediaType.video && newData.url && videoID == null &&
-						<Span ml={5} style={{color: HSLA(30, 1, .6, 1), whiteSpace: "pre"}}>Only YouTube urls supported currently.</Span>}
-				</RowLR>
-				<RowLR mt={5} splitAt={splitAt} style={{width: "100%"}}>
-					<Pre>Description: </Pre>
-					<TextInput enabled={enabled} style={ES({flex: 1})}
-						value={newData.description} onChange={val=>Change(newData.description = val)}/>
-				</RowLR>
-				<Column mt={10}>
-					<Row style={{fontWeight: "bold"}}>Preview:</Row>
-						{newData.type == MediaType.image &&
-							<Row mt={5} style={{display: "flex", alignItems: "center"}}>
-								<img src={newData.url} style={{width: "100%"}}/>
-							</Row>}
-						{newData.type == MediaType.video &&
-							// use wrapper div (with video-id as key), to ensure element cleanup when video-id changes
-							<div key={videoID}>
-								{!videoID && <div>Invalid YouTube video url: {newData.url}</div>}
-								{videoID && <YoutubePlayerUI videoID={videoID} /*startTime={0}*/ heightVSWidthPercent={.5625}
-									onPlayerInitialized={player=>{
-										player.GetPlayerUI().style.position = "absolute";
-									}}/>}
-							</div>}
-				</Column>
-				<RowLR mt={5} splitAt={splitAt}>
-					<Pre>Access policy: </Pre>
-					<PolicyPicker value={newData.accessPolicy} onChange={val=>Change(newData.accessPolicy = val!)}>
-						<Button enabled={enabled} text={accessPolicy ? `${accessPolicy.name} (id: ${accessPolicy.id})` : "(click to select policy)"} style={{width: "100%"}}/>
-					</PolicyPicker>
-				</RowLR>
-				{dataError && dataError != "Please fill out this field." && <Row mt={5} style={{color: "rgba(200,70,70,1)"}}>{dataError}</Row>}
+export const MediaDetailsUI = observer_mgl((props: MediaDetailsUIProps)=>{
+	const {baseData, style, onChange, phase} = props;
+	const {newData, helpers, dataError, containerRef} = useDetailsUI<Media>({
+		baseData,
+		onChange,
+		phase
+	});
+	const {Change, creating, editing} = helpers;
+
+	const videoID = ParseYoutubeVideoID(newData.url);
+	const accessPolicy = GetAccessPolicy(newData.accessPolicy);
+	const enabled = creating || editing;
+
+	const splitAt = 100;
+	return (
+		<Column ref={containerRef as any} style={style}>
+			{!creating && <GenericEntryInfoUI id={baseData.id} creatorID={newData.creator} createdAt={newData.createdAt}/>}
+
+			<RowLR mt={5} splitAt={splitAt}>
+				<Pre>Name: </Pre>
+				<TextInput
+					pattern={Media_namePattern} required
+					enabled={enabled} style={{width: "100%"}}
+					value={newData.name} onChange={val=>Change(newData.name = val)}/>
+			</RowLR>
+
+			<RowLR mt={5} splitAt={splitAt}>
+				<Pre>Type: </Pre>
+				<Select options={GetEntries(MediaType, name=>GetNiceNameForMediaType(MediaType[name]))} enabled={enabled} style={ES({flex: 1})}
+					value={newData.type} onChange={val=>Change(newData.type = val)}/>
+			</RowLR>
+
+			<RowLR mt={5} splitAt={splitAt}>
+				<Pre>URL: </Pre>
+				<TextInput
+					enabled={enabled} style={{width: "100%"}} required
+					value={newData.url} onChange={val=>Change(newData.url = val)}/>
+				{newData.type == MediaType.video && newData.url && videoID == null &&
+					<Span ml={5} style={{color: HSLA(30, 1, .6, 1), whiteSpace: "pre"}}>Only YouTube urls supported currently.</Span>}
+			</RowLR>
+
+			<RowLR mt={5} splitAt={splitAt} style={{width: "100%"}}>
+				<Pre>Description: </Pre>
+				<TextInput enabled={enabled} style={ES({flex: 1})} value={newData.description} onChange={val=>Change(newData.description = val)}/>
+			</RowLR>
+
+			<Column mt={10}>
+				<Row style={{fontWeight: "bold"}}>Preview:</Row>
+					{newData.type == MediaType.image &&
+						<Row mt={5} style={{display: "flex", alignItems: "center"}}>
+							<img src={newData.url} style={{width: "100%"}}/>
+						</Row>}
+					{newData.type == MediaType.video &&
+						// use wrapper div (with video-id as key), to ensure element cleanup when video-id changes
+						<div key={videoID}>
+							{!videoID && <div>Invalid YouTube video url: {newData.url}</div>}
+							{videoID && <YoutubePlayerUI videoID={videoID} /*startTime={0}*/ heightVSWidthPercent={.5625}
+								onPlayerInitialized={player=>{
+									player.GetPlayerUI().style.position = "absolute";
+								}}/>}
+						</div>}
 			</Column>
-		);
-	}
-}
+
+			<RowLR mt={5} splitAt={splitAt}>
+				<Pre>Access policy: </Pre>
+				<PolicyPicker value={newData.accessPolicy} onChange={val=>Change(newData.accessPolicy = val!)}>
+					<Button enabled={enabled} text={accessPolicy ? `${accessPolicy.name} (id: ${accessPolicy.id})` : "(click to select policy)"} style={{width: "100%"}}/>
+				</PolicyPicker>
+			</RowLR>
+
+			{dataError && dataError != "Please fill out this field." && <Row mt={5} style={{color: "rgba(200,70,70,1)"}}>{dataError}</Row>}
+		</Column>
+	);
+})
 
 export async function ShowAddMediaDialog(initialData?: Partial<Media>, postAdd?: (id: string)=>void) {
 	const prep = await GetAsync(()=>{
@@ -95,17 +98,10 @@ export async function ShowAddMediaDialog(initialData?: Partial<Media>, postAdd?:
 		type: MediaType.image,
 		description: "",
 	}, initialData));
-	//const getCommand = ()=>new AddMedia({media: newEntry});
 
 	const boxController: BoxController = ShowMessageBox({
 		title: "Add media", cancelButton: true,
 		message: observer_mgl(()=>{
-			/*const tempCommand = getCommand();
-			boxController.options.okButtonProps = {
-				enabled: tempCommand.Validate_Safe() == null,
-				title: tempCommand.ValidateErrorStr,
-			};*/
-
 			return (
 				<Column style={{padding: "10px 0", width: 600}}>
 					<MediaDetailsUI baseData={newEntry} phase="create"
@@ -117,7 +113,6 @@ export async function ShowAddMediaDialog(initialData?: Partial<Media>, postAdd?:
 			);
 		}),
 		onOK: async()=>{
-			//const {id} = await getCommand().RunOnServer();
 			const {id} = await RunCommand_AddMedia(newEntry);
 			if (postAdd) postAdd(id);
 		},
