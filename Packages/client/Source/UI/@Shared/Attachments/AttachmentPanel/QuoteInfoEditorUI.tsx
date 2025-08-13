e696c4d60a5c22e7db0fa7fd123c92c1720a7401
rishadbaniya <rbdroid01@gmail.com>
2025-08-13 17:16:58 +0545

    refactor(Packages/client): convert EquationEditorUI and QuoteInfoEditorUI to functional

--- Packages/client/Source/UI/@Shared/Attachments/AttachmentPanel/QuoteInfoEditorUI.tsx
+++ Packages/client/Source/UI/@Shared/Attachments/AttachmentPanel/QuoteInfoEditorUI.tsx
@@ -1,55 +1,43 @@
 import {CleanUpdatedSourceChains, QuoteAttachment} from "dm_common";
-import {DetailsUI_Base} from "UI/@Shared/DetailsUI_Base.js";
+import {DetailsUIBaseProps, useDetailsUI} from "UI/@Shared/DetailsUI_Base.js";
 import {Column, Pre, Row} from "react-vcomponents";
 import {MarkdownEditor, MarkdownToolbar} from "react-vmarkdown";
 import {SourceChainsEditorUI} from "../../Maps/Node/SourceChainsEditorUI.js";
+import React from "react";
 
-//@ApplyBasicStyles
-export class QuoteInfoEditorUI extends DetailsUI_Base<QuoteAttachment, QuoteInfoEditorUI> {
-	render() {
-		const {baseData} = this.props;
-		const {newData} = this.state;
-		const {Change, enabled} = this.helpers;
+export const QuoteInfoEditorUI = (props: DetailsUIBaseProps<QuoteAttachment, {}>)=>{
+	const {phase, onChange, baseData} = props;
 
-		return (
-			<Column>
-				{/*showPreview &&
-				<>
-					<Row key={0}>Preview:</Row>,
-					<Column key={1} mt={5}>
-						<Pre style={{padding: 5, background: "rgba(255,255,255,.2)", borderRadius: 5}}>
-							{GetNodeDisplayText({type: NodeType.claim, current: {quote: CleanUpdatedQuoteAttachment(Clone(newData))}} as any, undefined, ClaimForm.base)}
-							<SubPanel_Quote attachment={newData} fontSize={15}/>
-						</Pre>
-					</Column>
-				</>*/}
-				<Column mt={/*showPreview ? 5 :*/ 0}>
-					<Pre>Quote text: </Pre>
-					{/* <TextInput style={ES({flex: 1})}
-						value={info.text} onChange={val=>Change(info.text = val)}/> */}
-					{enabled && <MarkdownToolbar editor={()=>this.refs.editor} excludeCommands={["h1", "h2", "h3", "h4", "quote"]}/>}
-					<MarkdownEditor ref="editor" toolbar={false} value={newData.content} onChange={val=>Change(newData.content = val)} options={{
-						scrollbarStyle: "overlay",
-						lineWrapping: true,
-						readOnly: !enabled,
-					}}/>
-				</Column>
-				<Row mt={5}>
-					<SourceChainsEditorUI ref={c=>this.chainsEditor = c} enabled={enabled} baseData={newData.sourceChains} onChange={val=>Change(newData.sourceChains = val)}/>
-				</Row>
+	const editorRef = React.useRef<MarkdownEditor>(null);
+	const chainsEditorRef = React.useRef<SourceChainsEditorUI | null>(null);
+
+	const {newData, helpers, containerRef} = useDetailsUI<QuoteAttachment>({
+		baseData,
+		phase,
+		onChange,
+		getNewDataPostProcess: nD=>CleanUpdatedQuoteAttachment(nD),
+		getValidationErrorExtras: ()=>chainsEditorRef.current?.GetValidationError(),
+	});
+	const {Change, enabled} = helpers;
+
+	return (
+		<Column ref={containerRef as any}>
+			<Column mt={0}>
+				<Pre>Quote text: </Pre>
+				{enabled && <MarkdownToolbar editor={()=>editorRef.current} excludeCommands={["h1", "h2", "h3", "h4", "quote"]}/>}
+				<MarkdownEditor ref={editorRef} toolbar={false} value={newData.content} onChange={val=>Change(newData.content = val)} options={{
+					scrollbarStyle: "overlay",
+					lineWrapping: true,
+					readOnly: !enabled,
+				}}/>
 			</Column>
-		);
-	}
-	chainsEditor: SourceChainsEditorUI|n;
-	GetValidationError_Extras() {
-		return this.chainsEditor?.GetValidationError();
-	}
-
-	GetNewData_PostProcess(newData: QuoteAttachment) {
-		CleanUpdatedQuoteAttachment(newData);
-	}
+			<Row mt={5}>
+				<SourceChainsEditorUI ref={chainsEditorRef} enabled={enabled} baseData={newData.sourceChains} onChange={val=>Change(newData.sourceChains = val)}/>
+			</Row>
+		</Column>
+	);
 }
 
-export function CleanUpdatedQuoteAttachment(attachment: QuoteAttachment) {
+export const CleanUpdatedQuoteAttachment = (attachment: QuoteAttachment)=>{
 	CleanUpdatedSourceChains(attachment.sourceChains);
-}
+};
