b9c83982d1135da54f8b9f6e11b9a908b431dad0
rishadbaniya <rbdroid01@gmail.com>
2025-08-14 13:56:33 +0545

    refactor(Packages/client): convert ButtonChain, StarsRating, TextArea_Div to functional component

--- Packages/client/Source/Utils/ReactComponents/StarsRating.tsx
+++ Packages/client/Source/Utils/ReactComponents/StarsRating.tsx
@@ -1,39 +1,45 @@
-import {BaseComponent} from "react-vextensions";
 import {Row, Button, RowProps} from "react-vcomponents";
-import {Icon} from "web-vcore";
 import {Range, E} from "js-vextensions";
+import React from "react";
 
 export type StarsRating_Props = {
-	value: number, onChange: (value: number)=>any, titleFunc?: (starValue: number)=>string,
+	value: number,
+	onChange: (value: number)=>any,
+	titleFunc?: (starValue: number)=>string,
 	rightClickAction?: "clear" | ((e: React.MouseEvent)=>any),
 } & RowProps;
 
-export class StarsRating extends BaseComponent<StarsRating_Props, {}> {
-	static defaultProps = {titleFunc: starValue=>`Rate ${starValue} stars (right-click to clear)`, rightClickAction: "clear"};
-	render() {
-		const {value, onChange, rightClickAction, titleFunc, style, ...rest} = this.props;
-		return (
-			<Row {...rest} style={E({alignItems: "center"}, style)}>
-				{Range(1, 5).map(starValue=>{
-					return (
-						<Button key={starValue} size={20} mdIcon={value >= starValue ? "star" : "star-outline"}
-							title={titleFunc ? titleFunc(starValue) : `Set rating to ${starValue} stars`}
-							onClick={()=>{
-								onChange(starValue);
-							}}
-							onContextMenu={e=>{
-								if (rightClickAction == "clear") {
-									e.preventDefault();
-									onChange(0);
-									return;
-								}
-								if (rightClickAction instanceof Function) {
-									return rightClickAction(e);
-								}
-							}}/>
-					);
-				})}
-			</Row>
-		);
-	}
-}
+export const StarsRating = (props: StarsRating_Props)=>{
+	const {
+		value,
+		titleFunc = starValue=>`Rate ${starValue} stars (right-click to clear)`,
+		rightClickAction = "clear",
+		style,
+		onChange,
+		...rest
+	} = props;
+
+	return (
+		<Row {...rest} style={E({alignItems: "center"}, style)}>
+			{Range(1, 5).map(starValue=>{
+				return (
+					<Button key={starValue} size={20} mdIcon={value >= starValue ? "star" : "star-outline"}
+						title={titleFunc ? titleFunc(starValue) : `Set rating to ${starValue} stars`}
+						onClick={()=>{
+							onChange(starValue);
+						}}
+						onContextMenu={e=>{
+							if (rightClickAction === "clear") {
+								e.preventDefault();
+								onChange(0);
+								return;
+							}
+							if (rightClickAction instanceof Function) {
+								return rightClickAction(e);
+							}
+						}}/>
+				);
+			})}
+		</Row>
+	);
+};
