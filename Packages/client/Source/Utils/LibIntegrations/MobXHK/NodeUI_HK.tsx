ee97d3336ec1db165d9dfa750cd3ba92a106060a
rishadbaniya <rbdroid01@gmail.com>
2025-08-14 15:05:08 +0545

    refactor(Packages/client): convert SLButton, TableFooter, NodeUI_HI, some components of Root and create hook for DetailsUIBase

--- Packages/client/Source/Utils/LibIntegrations/MobXHK/NodeUI_HK.tsx
+++ Packages/client/Source/Utils/LibIntegrations/MobXHK/NodeUI_HK.tsx
@@ -1,23 +1,18 @@
-import {Observer} from "web-vcore";
 import {Row} from "react-vcomponents";
-import {BaseComponent} from "react-vextensions";
-import {GetNode_HK} from "./HKStore";
+import {GetNode_HK} from "./HKStore.js";
+import {observer_mgl} from "mobx-graphlink";
+import React from "react";
 
-@Observer
-export class NodeUI_HK extends BaseComponent<{nodeID: string}, {}> {
-	render() {
-		const {nodeID} = this.props;
+export const NodeUI_HK_ = observer_mgl(({nodeID}: {nodeID: string})=>{
+	const node = GetNode_HK(nodeID);
+	if (node == null) return <div>Loading...</div>;
 
-		const node = GetNode_HK(nodeID);
-		if (node == null) return <div>Loading...</div>;
-
-		return (
-			<Row style={{
-				background: "rgba(0,0,0,.3)", borderRadius: 5, padding: "3px 7px",
-				marginLeft: 1000, marginTop: 500,
-			}}>
-				{node.title["@value"]}
-			</Row>
-		);
-	}
-}
+	return (
+		<Row style={{
+			background: "rgba(0,0,0,.3)", borderRadius: 5, padding: "3px 7px",
+			marginLeft: 1000, marginTop: 500,
+		}}>
+			{node.title["@value"]}
+		</Row>
+	);
+});
