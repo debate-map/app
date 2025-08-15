c777c1d1ef0cd3848c4abb5d46def450c316650b
rishadbaniya <rbdroid01@gmail.com>
2025-08-15 20:26:40 +0545

    refactor(Packages/client): convert all components within NodeDetailsUI to functional component

--- Packages/client/Source/UI/@Shared/Maps/Node/NodeDetailsUI/PermissionsPanel.tsx
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeDetailsUI/PermissionsPanel.tsx
@@ -1,27 +1,25 @@
 import {GetAccessPolicy} from "dm_common";
 import React from "react";
 import {PolicyPicker} from "UI/Database/Policies/PolicyPicker.js";
-import {Observer} from "web-vcore";
 import {Button, Pre, RowLR} from "react-vcomponents";
-import {BaseComponent} from "react-vextensions";
 import {NodeDetailsUI_SharedProps} from "../NodeDetailsUI.js";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class PermissionsPanel extends BaseComponent<Pick<NodeDetailsUI_SharedProps, "enabled" | "newData" | "Change">, {}> {
-	render() {
-		const {enabled, newData, Change} = this.props;
-		const accessPolicy = GetAccessPolicy(newData.accessPolicy);
+export type PermissionsPanel_Props = Pick<NodeDetailsUI_SharedProps, "enabled" | "newData" | "Change">;
 
-		const splitAt = 105;
-		return (
-			<>
-				<RowLR mt={5} splitAt={splitAt}>
-					<Pre>Access policy: </Pre>
-					<PolicyPicker value={newData.accessPolicy} onChange={val=>Change(newData.accessPolicy = val!)}>
-						<Button enabled={enabled} text={accessPolicy ? `${accessPolicy.name} (id: ${accessPolicy.id})` : "(click to select policy)"} style={{width: "100%"}}/>
-					</PolicyPicker>
-				</RowLR>
-			</>
-		);
-	}
-}
+export const PermissionsPanel = observer_mgl((props: PermissionsPanel_Props)=>{
+	const {enabled, newData, Change} = props;
+	const accessPolicy = GetAccessPolicy(newData.accessPolicy);
+
+	const splitAt = 105;
+	return (
+		<>
+			<RowLR mt={5} splitAt={splitAt}>
+				<Pre>Access policy: </Pre>
+				<PolicyPicker value={newData.accessPolicy} onChange={val=>Change(newData.accessPolicy = val!)}>
+					<Button enabled={enabled} text={accessPolicy ? `${accessPolicy.name} (id: ${accessPolicy.id})` : "(click to select policy)"} style={{width: "100%"}}/>
+				</PolicyPicker>
+			</RowLR>
+		</>
+	);
+});
