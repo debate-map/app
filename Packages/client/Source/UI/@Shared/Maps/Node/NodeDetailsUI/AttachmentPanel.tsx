e9d91e9fd1062506a35ebc351a616234fed72323
rishadbaniya <rbdroid01@gmail.com>
2025-08-15 13:02:08 +0545

    refactor(Packages/client): convert class components used for Node to functional components

--- Packages/client/Source/UI/@Shared/Maps/Node/NodeDetailsUI/AttachmentPanel.tsx
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeDetailsUI/AttachmentPanel.tsx
@@ -1,29 +1,21 @@
-import {AttachmentType, GetAttachmentType_Node} from "dm_common";
-import {DEL} from "js-vextensions";
+import {AttachmentType} from "dm_common";
 import React from "react";
-import {AttachmentEditorUI} from "UI/@Shared/Attachments/AttachmentEditorUI";
 import {DetailsUI_Phase} from "UI/@Shared/DetailsUI_Base";
 import {AttachmentsEditorUI} from "UI/Database/Terms/AttachmentsEditorUI";
-import {BaseComponent} from "react-vextensions";
 import {NodeDetailsUI_SharedProps} from "../NodeDetailsUI.js";
 
-/*export function CanNodeHaveAttachments(node: NodeL1) {
-	//return node.type == NodeType.claim;
-	// maybe temp; allow attachments on everything except arguments (disallowed since attachment should just be added to its premise in that case)
-	return node.type != NodeType.argument;
-}*/
-
-export function GetPhaseFromNodeDetailsUIProps(props: {forNew: boolean, enabled: boolean}): DetailsUI_Phase {
+export const GetPhaseFromNodeDetailsUIProps = (props: {forNew: boolean, enabled: boolean}): DetailsUI_Phase=>{
 	return props.enabled ? (props.forNew ? "create" : "edit") : "view";
 }
 
-export class AttachmentPanel extends BaseComponent<NodeDetailsUI_SharedProps & {}, {}> {
-	render() {
-		const {newData, newDataAsL2, newRevisionData, forNew, enabled, Change} = this.props;
-		//const attachmentType = GetAttachmentType_Node(newDataAsL2);
-		//const canHaveAttachments = CanNodeHaveAttachments(newData);
-		return <AttachmentsEditorUI phase={GetPhaseFromNodeDetailsUIProps({forNew, enabled: enabled!})} target="node"
-			baseData={newRevisionData.attachments} onChange={val=>Change(newRevisionData.attachments = val)}
-			allowedAttachmentTypes={[AttachmentType.references, AttachmentType.quote, AttachmentType.media, AttachmentType.equation, AttachmentType.description]}/>;
-	}
-}
+export const AttachmentPanel = (props: NodeDetailsUI_SharedProps)=>{
+	const {newRevisionData, forNew, enabled, Change} = props;
+
+	return <AttachmentsEditorUI
+		phase={GetPhaseFromNodeDetailsUIProps({forNew, enabled: enabled!})}
+		target="node"
+		baseData={newRevisionData.attachments}
+		onChange={val=>Change(newRevisionData.attachments = val)}
+		allowedAttachmentTypes={[AttachmentType.references, AttachmentType.quote, AttachmentType.media, AttachmentType.equation, AttachmentType.description]}
+	/>;
+};
--- Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_CloneNode.tsx
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_CloneNode.tsx
@@ -1,52 +1,46 @@
-import {GetNodeL3, GetParentNodeL3, NodeType, MeID, TransferType} from "dm_common";
+import {GetNodeL3, GetParentNodeL3, MeID, TransferType} from "dm_common";
 import React from "react";
 import {ShowSignInPopup} from "UI/@Shared/NavBar/UserPanel.js";
 import {liveSkin} from "Utils/Styles/SkinManager.js";
-import {Observer} from "web-vcore";
-import {SlicePath} from "mobx-graphlink";
-import {BaseComponent} from "react-vextensions";
 import {VMenuItem} from "react-vmenu";
 import {MI_SharedProps} from "../NodeUI_Menu.jsx";
 import {ShowTransferNodeDialog, TransferNodeNeedsWrapper} from "./Dialogs/TransferNodeDialog.js";
 import {GetTransferNodesInitialData} from "./Dialogs/TransferNodeDialog/TransferNodeData.js";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class MI_CloneNode extends BaseComponent<MI_SharedProps, {}> {
-	render() {
-		const {map, node, path} = this.props;
-		if (map == null) return null;
-		//const formForClaimChildren = node.type == NodeType.category ? ClaimForm.question : ClaimForm.base;
+export const MI_CloneNode = observer_mgl((props: MI_SharedProps)=>{
+	const {map, path} = props;
+	if (map == null) return null;
 
-		// we "initiate a clone" for the "outer" argument node, if there's a box combining an argument and claim (this is how the dialog expects such a case)
-		const pathToClone = path;
-		const nodeToClone = GetNodeL3(pathToClone);
-		if (nodeToClone == null) return null; // node just deleted?
+	// we "initiate a clone" for the "outer" argument node, if there's a box combining an argument and claim (this is how the dialog expects such a case)
+	const pathToClone = path;
+	const nodeToClone = GetNodeL3(pathToClone);
+	if (nodeToClone == null) return null; // node just deleted?
 
-		const parentOfNodeToClone = GetParentNodeL3(pathToClone);
-		if (parentOfNodeToClone == null || nodeToClone.link == null) return null; // cannot clone a map's root-node (for now anyway)
+	const parentOfNodeToClone = GetParentNodeL3(pathToClone);
+	if (parentOfNodeToClone == null || nodeToClone.link == null) return null; // cannot clone a map's root-node (for now anyway)
 
-		const [payload_initial, uiState_initial] = GetTransferNodesInitialData(map, nodeToClone, pathToClone, parentOfNodeToClone, nodeToClone.link.group, TransferType.clone);
-		if (payload_initial == null || uiState_initial == null) return;
+	const [payload_initial, uiState_initial] = GetTransferNodesInitialData(map, nodeToClone, pathToClone, parentOfNodeToClone, nodeToClone.link.group, TransferType.clone);
+	if (payload_initial == null || uiState_initial == null) return;
 
-		// if cloning, and its an arg+claim combo
-		if (payload_initial.nodes[0].transferType == TransferType.clone && payload_initial.nodes.length > 1) {
-			// maybe temp: if arg can be "shimmed", default to that; else, default to "ignore"
-			const argCanBeShim = TransferNodeNeedsWrapper(payload_initial.nodes[1], uiState_initial);
-			if (argCanBeShim) {
-				payload_initial.nodes[0].transferType = TransferType.shim;
-			} else {
-				payload_initial.nodes[0].transferType = TransferType.ignore;
-			}
+	// if cloning, and its an arg+claim combo
+	if (payload_initial.nodes[0].transferType == TransferType.clone && payload_initial.nodes.length > 1) {
+		// maybe temp: if arg can be "shimmed", default to that; else, default to "ignore"
+		const argCanBeShim = TransferNodeNeedsWrapper(payload_initial.nodes[1], uiState_initial);
+		if (argCanBeShim) {
+			payload_initial.nodes[0].transferType = TransferType.shim;
+		} else {
+			payload_initial.nodes[0].transferType = TransferType.ignore;
 		}
-
-		return (
-			<VMenuItem text={<span>Clone <span style={{fontSize: 10, opacity: 0.7}}>(for independent* duplicate)</span></span> as any}
-				style={liveSkin.Style_VMenuItem()} onClick={e=>{
-					if (e.button != 0) return;
-					if (MeID() == null) return ShowSignInPopup();
-
-					ShowTransferNodeDialog(payload_initial, uiState_initial, "Clone node in-place");
-				}}/>
-		);
 	}
-}
+
+	return (
+		<VMenuItem text={<span>Clone <span style={{fontSize: 10, opacity: 0.7}}>(for independent* duplicate)</span></span> as any}
+			style={liveSkin.Style_VMenuItem()} onClick={e=>{
+				if (e.button != 0) return;
+				if (MeID() == null) return ShowSignInPopup();
+
+				ShowTransferNodeDialog(payload_initial, uiState_initial, "Clone node in-place");
+			}}/>
+	);
+});
