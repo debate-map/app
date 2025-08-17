f764f772d06bee8618cb4e444e22e70fc52d78f5
rishadbaniya <rbdroid01@gmail.com>
2025-08-17 14:08:14 +0545

    refactor(Packages/client): convert Panels of NavBar to functional component

--- Packages/client/Source/UI/@Shared/NavBar/NotificationsPanel.tsx
+++ Packages/client/Source/UI/@Shared/NavBar/NotificationsPanel.tsx
@@ -1,6 +1,5 @@
-import {BaseComponent, UseMemo, cssHelper} from "react-vextensions";
 import {Button, CheckBox, Column, Div, Row, Select, Text} from "react-vcomponents";
-import {InfoButton, Observer, RunInAction_Set} from "web-vcore";
+import {InfoButton, RunInAction_Set} from "web-vcore";
 import {liveSkin} from "Utils/Styles/SkinManager";
 import {CommandRun, GetCommandRun, GetNotifications, MeID, Notification, SubscriptionLevel} from "dm_common";
 import {E, GetEntries} from "js-vextensions";
@@ -10,14 +9,31 @@ import {RunCommand_UpdateNotification} from "../../../Utils/DB/Command.js";
 import {GetMapState} from "../../../Store/main/maps/mapStates/$mapState.js";
 import {GetOpenMapID} from "../../../Store/main.js";
 import {store} from "../../../Store/index.js";
+import {observer_mgl} from "mobx-graphlink";
+import React, {useRef} from "react";
 
-@Observer
-export class NotificationsPanel extends BaseComponent<{}, {}, {
-	notifications: (Notification & { command: CommandRun|n })[];
-}> {
-	renderItem = (index, key)=>{
-		const {notifications} = this.stash;
-		const notification = notifications[index];
+export const NotificationsPanel = observer_mgl(()=>{
+	const notifRef = useRef<(Notification & { command: CommandRun|n})[]>([]);
+
+	const mapState = GetMapState(GetOpenMapID());
+	const uiState = store.main.notifications;
+
+	const notifications = GetNotifications(MeID()).map(a=>{
+		return {
+			...a,
+			command: a.commandRun ? GetCommandRun(a.commandRun) : null,
+		};
+	}).sort((a, b)=>{
+		if (a.readTime == null && b.readTime != null) return -1;
+		if (a.readTime != null && b.readTime == null) return 1;
+		return (b.command?.runTime ?? 0) - (a.command?.runTime ?? 0);
+	});
+	notifRef.current = notifications;
+	const unreadCount = notifications.filter(a=>a.readTime == null).length;
+
+	const renderItem = (index: number, key: string | number)=>{
+		const notifRefVal= notifRef.current;
+		const notification = notifRefVal[index];
 
 		return (
 			<Column key={key}
@@ -41,74 +57,54 @@ export class NotificationsPanel extends BaseComponent<{}, {}, {
 					</svg>
 				</Div>}
 				{notification.command &&
-				<CommandRunUI panel={true} key={index} run={notification.command!} index={index} last={index == notifications.length - 1}/>}
+				<CommandRunUI panel={true} key={index} run={notification.command!} index={index} last={index === notifRefVal.length - 1}/>}
 			</Column>
 		);
 	}
 
-	render() {
-		const mapState = GetMapState(GetOpenMapID());
-		const uiState = store.main.notifications;
-
-		const notifications = GetNotifications(MeID()).map(a=>{
-			return {
-				...a,
-				command: a.commandRun ? GetCommandRun(a.commandRun) : null,
-			};
-		}).sort((a, b)=>{
-			if (a.readTime == null && b.readTime != null) return -1;
-			if (a.readTime != null && b.readTime == null) return 1;
-			return (b.command?.runTime ?? 0) - (a.command?.runTime ?? 0);
-		});
-
-		this.Stash({notifications});
-
-		const unreadCount = notifications.filter(a=>a.readTime == null).length;
-
-		return (
-			<Div style={{
-				width: 750, borderRadius: "0 0 0 5px", overflow: "hidden",
-				background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
-			}}>
-				{notifications.length == 0 && "No notifications."}
-				{notifications.length > 0 &&
-				<Column style={
-					{padding: 5, display: "flex", flexFlow: "row nowrap", justifyContent: "space-between", alignItems: "center"}
-				}>
-					<span style={{fontSize: 18, fontWeight: "bold"}}>Notifications</span>
-					{unreadCount > 0 &&
-					<Button text="Mark all as read" onClick={()=>{
-						for (const notification of notifications) {
-							if (notification.readTime == null) {
-								RunCommand_UpdateNotification({id: notification.id, updates: {
-									readTime: Date.now(),
-								}});
-							}
+	return (
+		<Div style={{
+			width: 750, borderRadius: "0 0 0 5px", overflow: "hidden",
+			background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
+		}}>
+			{notifications.length == 0 && "No notifications."}
+			{notifications.length > 0 &&
+			<Column style={
+				{padding: 5, display: "flex", flexFlow: "row nowrap", justifyContent: "space-between", alignItems: "center"}
+			}>
+				<span style={{fontSize: 18, fontWeight: "bold"}}>Notifications</span>
+				{unreadCount > 0 &&
+				<Button text="Mark all as read" onClick={()=>{
+					for (const notification of notifications) {
+						if (notification.readTime == null) {
+							RunCommand_UpdateNotification({id: notification.id, updates: {
+								readTime: Date.now(),
+							}});
 						}
-					}}/>}
-				</Column>}
-				<Div style={{
-					overflowY: "auto", height: 500,
-				}}>
-					<ReactList
-						itemRenderer={this.renderItem}
-						length={notifications.length}
-						type='uniform'
-					/>
-				</Div>
-				{mapState && <Row style={{padding: 5, alignItems: "center", justifyContent: "space-between"}}>
-					<Row center style={{padding: "8px 0px"}}>
-						<CheckBox ml="auto" text="Paint Mode" value={mapState.subscriptionPaintMode} onChange={val=>RunInAction_Set(this, ()=>{ mapState.subscriptionPaintMode = val; })}/>
-						<InfoButton ml={5} text="When enabled, you can simply drag over nodes to set their subscription state."/>
-					</Row>
-					{mapState?.subscriptionPaintMode === true && <Row ml={10}>
-						<Text>Notification level:</Text>
-						<Select ml={5} displayType="button bar" options={GetEntries(SubscriptionLevel, "ui")} value={uiState.paintMode_notificationLevel} onChange={val=>{
-							RunInAction_Set(this, ()=>uiState.paintMode_notificationLevel = val);
-						}}/>
-					</Row>}
-				</Row>}
+					}
+				}}/>}
+			</Column>}
+			<Div style={{
+				overflowY: "auto", height: 500,
+			}}>
+				<ReactList
+					itemRenderer={renderItem}
+					length={notifications.length}
+					type='uniform'
+				/>
 			</Div>
-		);
-	}
-}
+			{mapState && <Row style={{padding: 5, alignItems: "center", justifyContent: "space-between"}}>
+				<Row center style={{padding: "8px 0px"}}>
+					<CheckBox ml="auto" text="Paint Mode" value={mapState.subscriptionPaintMode} onChange={val=>RunInAction_Set(()=>{ mapState.subscriptionPaintMode = val; })}/>
+					<InfoButton ml={5} text="When enabled, you can simply drag over nodes to set their subscription state."/>
+				</Row>
+				{mapState?.subscriptionPaintMode === true && <Row ml={10}>
+					<Text>Notification level:</Text>
+					<Select ml={5} displayType="button bar" options={GetEntries(SubscriptionLevel, "ui")} value={uiState.paintMode_notificationLevel} onChange={val=>{
+						RunInAction_Set(()=>uiState.paintMode_notificationLevel = val);
+					}}/>
+				</Row>}
+			</Row>}
+		</Div>
+	);
+});
