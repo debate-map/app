0d63593d86fd88187c62cea1a946761b0a146da2
rishadbaniya <rbdroid01@gmail.com>
2025-08-17 14:08:14 +0545

    refactor(Packages/client): convert Panels of NavBar to functional component

--- Packages/client/Source/UI/@Shared/NavBar/DebugPanel.tsx
+++ Packages/client/Source/UI/@Shared/NavBar/DebugPanel.tsx
@@ -1,46 +1,43 @@
-import React, {useEffect} from "react";
-import {StreamUI} from "UI/Social/StreamUI";
-import {BaseComponent, SimpleShouldUpdate} from "react-vextensions";
+import React, {useEffect, useReducer} from "react";
 import {liveSkin} from "Utils/Styles/SkinManager";
 import {CheckBox, Column, Row} from "react-vcomponents";
 import {Timer} from "js-vextensions";
-import {InfoButton, Observer, RunInAction_Set} from "web-vcore";
+import {InfoButton, RunInAction_Set} from "web-vcore";
 import {store} from "Store";
 import {GetMGLUnsubscribeDelay, graph} from "Utils/LibIntegrations/MobXGraphlink";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class DebugPanel extends BaseComponent<{}, {}> {
-	render() {
-		useEffect(()=>{
-			const timer = new Timer(200, ()=>{
-				this.Update();
-			}).Start();
-			return ()=>timer.Stop();
-		});
+export const DebugPanelFn = observer_mgl(()=>{
+	const [, rerender] = useReducer((c: number)=>c + 1, 0);
 
-		const stats = store.graphlink.GetStats();
+	useEffect(()=>{
+		const timer = new Timer(200, ()=>{
+			rerender();
+		}).Start();
+		return ()=>timer.Stop();
+	}, []);
 
-		return (
-			<Column style={{
-				width: 800, padding: 5, borderRadius: "0 0 5px 0",
-				background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
-			}}>
-				<Row>MobX Graphlink stats</Row>
-				<Row>Attached tree-nodes: {stats.attachedTreeNodes}</Row>
-				<Row>With requested-subs: {stats.nodesWithRequestedSubscriptions}</Row>
-				<Row>With requested-subs, waiting: {stats.nodesWithRequestedSubscriptions - stats.nodesWithFulfilledSubscriptions}</Row>
-				<Row>With requested-subs, fulfilled: {stats.nodesWithFulfilledSubscriptions}</Row>
-				<Row>
-					<CheckBox text="Block MobX unsubscribing (on data becoming unobserved)" value={store.main.blockMobXUnsubscribing} onChange={val=>{
-						RunInAction_Set(this, ()=>store.main.blockMobXUnsubscribing = val);
-						graph.options.unsubscribeTreeNodesAfter = GetMGLUnsubscribeDelay();
-					}}/>
-					<InfoButton ml={5} text="Requires page-reload to (fully) take effect."/>
-				</Row>
-				<Row>
-					<CheckBox text="Block cache-clearing (on user-data change/resend)" value={store.main.blockCacheClearing} onChange={val=>RunInAction_Set(this, ()=>store.main.blockCacheClearing = val)}/>
-				</Row>
-			</Column>
-		);
-	}
-}
+	const stats = store.graphlink.GetStats();
+	return (
+		<Column style={{
+			width: 800, padding: 5, borderRadius: "0 0 5px 0",
+			background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
+		}}>
+			<Row>MobX Graphlink stats</Row>
+			<Row>Attached tree-nodes: {stats.attachedTreeNodes}</Row>
+			<Row>With requested-subs: {stats.nodesWithRequestedSubscriptions}</Row>
+			<Row>With requested-subs, waiting: {stats.nodesWithRequestedSubscriptions - stats.nodesWithFulfilledSubscriptions}</Row>
+			<Row>With requested-subs, fulfilled: {stats.nodesWithFulfilledSubscriptions}</Row>
+			<Row>
+				<CheckBox text="Block MobX unsubscribing (on data becoming unobserved)" value={store.main.blockMobXUnsubscribing} onChange={val=>{
+					RunInAction_Set(()=>store.main.blockMobXUnsubscribing = val);
+					graph.options.unsubscribeTreeNodesAfter = GetMGLUnsubscribeDelay();
+				}}/>
+				<InfoButton ml={5} text="Requires page-reload to (fully) take effect."/>
+			</Row>
+			<Row>
+				<CheckBox text="Block cache-clearing (on user-data change/resend)" value={store.main.blockCacheClearing} onChange={val=>RunInAction_Set(()=>store.main.blockCacheClearing = val)}/>
+			</Row>
+		</Column>
+	);
+});
--- Packages/client/Source/UI/@Shared/NavBar/ReputationPanel.tsx
+++ Packages/client/Source/UI/@Shared/NavBar/ReputationPanel.tsx
@@ -1,15 +1,13 @@
-import {BaseComponent, SimpleShouldUpdate} from "react-vextensions";
 import {liveSkin} from "Utils/Styles/SkinManager";
+import React from "react";
 
-export class ReputationPanel extends BaseComponent<{auth?}, {}> {
-	render() {
-		return (
-			<div style={{
-				display: "flex", flexDirection: "column", padding: 5, borderRadius: "0 0 0 5px",
-				background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
-			}}>
-				Reputation panel is under development.
-			</div>
-		);
-	}
-}
+export const ReputationPanel = ()=>{
+	return (
+		<div style={{
+			display: "flex", flexDirection: "column", padding: 5, borderRadius: "0 0 0 5px",
+			background: liveSkin.NavBarPanelBackgroundColor().css(), border: liveSkin.OverlayBorder(),
+		}}>
+			Reputation panel is under development.
+		</div>
+	);
+};
