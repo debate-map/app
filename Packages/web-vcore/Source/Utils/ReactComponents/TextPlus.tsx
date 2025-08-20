60c5e370567837eab0594c4189456d9d2157d13e
rishadbaniya <rbdroid01@gmail.com>
2025-08-20 06:49:13 +0545

    refactor(Packages): convert TextPlus and TakeUI to functional component

--- Packages/web-vcore/Source/Utils/ReactComponents/TextPlus.tsx
+++ Packages/web-vcore/Source/Utils/ReactComponents/TextPlus.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import {ApplyBasicStyles, BaseComponent, BaseComponentPlus, BasicStyles} from "react-vextensions";
 import {TextProps, Row, Text} from "react-vcomponents";
 import {InfoButton, InfoButtonProps} from "./InfoButton.js";
 
-export class TextPlus extends BaseComponent<{info?: string, infoProps?: InfoButtonProps, sel?: boolean} & TextProps, {}> {
-	render() {
-		const {info, infoProps, sel, children, ...rest} = this.props;
+type TextPlusProps = {
+    info?: string;
+    infoProps?: InfoButtonProps;
+    sel?: boolean;
+} & TextProps;
 
-		const text = <Text {...rest}>{children}</Text>;
-		if (info) {
-			return (
-				<Row center>
-					{text}
-					<InfoButton {...infoProps} ml={5} text={info} sel={sel}/>
-				</Row>
-			);
-		}
-		return text;
+export const TextPlus = (props: TextPlusProps)=>{
+	const {info, infoProps, sel, children, ...rest} = props;
+
+	const text = <Text {...rest}>{children}</Text>;
+	if (info) {
+		return (
+            <Row center>
+                {text}
+                <InfoButton {...infoProps} ml={5} text={info} sel={sel} />
+            </Row>
+		);
 	}
+	return text;
 }
