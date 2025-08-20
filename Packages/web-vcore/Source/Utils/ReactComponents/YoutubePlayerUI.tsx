5b22492b4433a28f0a5015adbf14e20169a5839d
rishadbaniya <rbdroid01@gmail.com>
2025-08-20 06:40:48 +0545

    refactor(Packages/web-vcore): convert YoutubePlayerUI and Tooltip to functional component

--- Packages/web-vcore/Source/Utils/ReactComponents/YoutubePlayerUI.tsx
+++ Packages/web-vcore/Source/Utils/ReactComponents/YoutubePlayerUI.tsx
@@ -1,54 +1,53 @@
-import React, {Ref} from "react";
-import {BaseComponent, BaseComponentPlus} from "react-vextensions";
-import {Row} from "react-vcomponents";
-import {E} from "js-vextensions";
+import React, {useEffect, useRef} from "react";
 import {YoutubePlayer, PosChangeSource} from "../General/YoutubePlayer.js";
-import {cssHelper} from "react-vextensions";;
+import {css2} from "../UI/Styles.js";
 
-export function ParseYoutubeVideoID(url: string) {
+export const ParseYoutubeVideoID = (url: string)=>{
 	return url?.match(/v=([A-Za-z0-9_-]{11})/)?.[1];
 }
 
-export class YoutubePlayerUI extends BaseComponentPlus(
-	{heightVSWidthPercent: .75, autoplay: false, onPosChanged_callForStartTime: true} as {
-		videoID: string, startTime?: number, heightVSWidthPercent: number, autoplay?: boolean,
-		initPlayer?: (player: YoutubePlayer)=>any,
-		onPlayerInitialized?: (player: YoutubePlayer)=>any,
-		onPosChanged?: (position: number, source: PosChangeSource)=>any,
-		onPosChanged_callForStartTime?: boolean,
-		style?,
-	},
-) {
-	player: YoutubePlayer;
-	root: HTMLDivElement|null;
-	render() {
-		const {heightVSWidthPercent, style} = this.props;
-		const {css} = cssHelper(this);
-		return (
-			<div ref={c=>void(this.root = c)} style={css({position: "relative", paddingBottom: `${heightVSWidthPercent * 100}%`, height: 0}, style)}/>
-		);
-	}
+type Props = {
+	videoID: string,
+	startTime?: number,
+	heightVSWidthPercent: number,
+	autoplay?: boolean,
+	initPlayer?: (player: YoutubePlayer)=>any,
+	onPlayerInitialized?: (player: YoutubePlayer)=>any,
+	onPosChanged?: (position: number, source: PosChangeSource)=>any,
+	onPosChanged_callForStartTime?: boolean,
+	style?,
+};
 
-	async ComponentDidMount() {
-		const {videoID, startTime, autoplay, initPlayer, onPlayerInitialized, onPosChanged, onPosChanged_callForStartTime} = this.props;
+export const YoutubePlayerUI = (props: Props)=>{
+	const {videoID, onPosChanged, startTime, heightVSWidthPercent = .75, autoplay = false, onPosChanged_callForStartTime= true,
+		style, initPlayer, onPlayerInitialized} = props;
+
+	const containerRef = useRef<HTMLDivElement>(null);
+	const playerRef = useRef<YoutubePlayer>(null);
+	const readyRef = useRef(false);
+
+	useEffect(()=>{
 		const player = new YoutubePlayer();
 		if (initPlayer) initPlayer(player);
-		this.player = player;
+		playerRef.current = player;
 
-		player.containerUI = this.root!;
-		await player.EnsureReady();
-		if (!this.mounted) return;
+		player.containerUI = containerRef.current!;
+		(async()=>{
+			await player.EnsureReady();
+			if (onPlayerInitialized) onPlayerInitialized(player);
+			player.LoadVideo({videoID, startTime}, autoplay);
 
-		if (onPlayerInitialized) onPlayerInitialized(player);
-		player.LoadVideo({videoID, startTime}, autoplay);
-		/*await player.LoadVideo({videoID, startTime});
-		if (autoplay) player.Play();*/
-
-		if (onPosChanged) {
-			player.onPositionChanged = onPosChanged;
-			if (onPosChanged_callForStartTime && startTime != null) {
-				onPosChanged(startTime, "playback"); // it's not really either source, but it's closer to playback
+			if (onPosChanged) {
+				player.onPositionChanged = onPosChanged;
+				if (onPosChanged_callForStartTime && startTime != null) {
+					onPosChanged(startTime, "playback"); // it's not really either source, but it's closer to playback
+				}
 			}
-		}
-	}
-}
+		})();
+	}, []);
+
+	const css = css2;
+	return (
+		<div ref={containerRef} style={css({position: "relative", paddingBottom: `${heightVSWidthPercent * 100}%`, height: 0}, style)}/>
+	);
+};
