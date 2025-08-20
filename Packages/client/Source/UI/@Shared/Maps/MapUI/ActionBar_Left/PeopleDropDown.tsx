e66c449bb0a1b51fd545fbc4743f467bf7af942f
rishadbaniya <rbdroid01@gmail.com>
2025-08-20 05:57:52 +0545

    refactor(Packages/client): convert PeopleDropDown, NewShareUI and SignInButton_Google_Inner to functional component

--- Packages/client/Source/UI/@Shared/Maps/MapUI/ActionBar_Left/PeopleDropDown.tsx
+++ Packages/client/Source/UI/@Shared/Maps/MapUI/ActionBar_Left/PeopleDropDown.tsx
@@ -1,81 +1,75 @@
 import {CloneWithPrototypes} from "js-vextensions";
-import {Button, Column, DropDown, DropDownContent, DropDownTrigger, Row, Text, TextInput, Pre} from "react-vcomponents";
-import {BaseComponent} from "react-vextensions";
+import {Button, Column, DropDown, DropDownContent, DropDownTrigger, Row, Text} from "react-vcomponents";
 import {ShowMessageBox} from "react-vmessagebox";
 import {SLMode} from "UI/@SL/SL.js";
 import {Button_SL} from "UI/@SL/SLButton.js";
-import {InfoButton, RunInAction_Set, Observer} from "web-vcore";
-import {GetMapEditors, IsUserCreatorOrMod, MeID, UpdateMapDetails, DMap, PERMISSIONS} from "dm_common";
+import {InfoButton} from "web-vcore";
+import {GetMapEditors, MeID, DMap, PERMISSIONS} from "dm_common";
 import {UserPicker} from "UI/@Shared/Users/UserPicker.js";
 import {liveSkin} from "Utils/Styles/SkinManager";
 import {RunCommand_UpdateMap} from "Utils/DB/Command";
 import {zIndexes} from "Utils/UI/ZIndexes.js";
+import {observer_mgl} from "mobx-graphlink";
+import React from "react";
 
 export const userIDPlaceholder = "[user-id placeholder]";
 
-@Observer
-export class PeopleDropDown extends BaseComponent<{map: DMap}, {}> {
-	render() {
-		const {map} = this.props;
-		//const editors = GetMapEditors(map.id).filter(a=>a);
-		const editors = GetMapEditors(map.id);
+export const PeopleDropDown = observer_mgl(({map}: {map: DMap})=>{
+	const editors = GetMapEditors(map.id);
 
-		const Button_Final = SLMode ? Button_SL : Button;
-		const creatorOrMod = PERMISSIONS.Map.Modify(MeID(), map);
-		return (
-			<DropDown>
-				<DropDownTrigger><Button_Final ml={5} style={{height: "100%"}} text="People"/></DropDownTrigger>
-				<DropDownContent style={{zIndex: zIndexes.dropdown, position: "fixed", left: 0, width: 500, borderRadius: "0 0 5px 0"}} content={()=>(
-					<Column>
-						<Row center style={{justifyContent: "center"}}>
-							<Text style={{color: "red", fontSize: 13}}>{`Note: Actual access/edit permissions are set by nodes' access-policies.`}</Text>
-							<InfoButton ml={5} text={`
-								The map's "list of editors" is a legacy feature that has no "actual function" atm (other than for reference), though functionality will be added here in the future.
-								Note: You can set the "default access-policy" for new nodes in the map's Details panel.
-							`.AsMultiline(0)}/>
-						</Row>
-						<Row mt={5} center>
-							<Text>Editors:</Text>
-							{creatorOrMod &&
-							<Button ml="auto" text="Add editor" onClick={async()=>{
-								const newEditors = CloneWithPrototypes(map.editors || []);
-								newEditors.push(userIDPlaceholder);
-								//new UpdateMapDetails({id: map.id, updates: {editors: newEditors}}).RunOnServer();
-								await RunCommand_UpdateMap({id: map.id, updates: {editors: newEditors}});
-							}}/>}
-						</Row>
-						{map.editors.map((editorID, index)=>{
-							const editor = editors[index];
-							const displayName = editor?.displayName ?? "n/a";
-							return (
-								<Row key={index} mt={5}>
-									<UserPicker value={editorID} onChange={async val=>{
-										const newEditors = CloneWithPrototypes(map.editors);
-										newEditors[index] = val;
-										//new UpdateMapDetails({id: map.id, updates: {editors: newEditors}}).RunOnServer();
-										await RunCommand_UpdateMap({id: map.id, updates: {editors: newEditors}});
-									}}>
-										<Button enabled={creatorOrMod} text={editorID != userIDPlaceholder ? `${displayName} (id: ${editorID})` : "(click to select user)"} style={{width: "100%"}}/>
-									</UserPicker>
-									{creatorOrMod &&
-									<Button ml={5} text="X" style={{...liveSkin.Style_XButton()}} onClick={()=>{
-										ShowMessageBox({
-											title: `Remove editor "${displayName}"`, cancelButton: true,
-											message: `Remove editor "${displayName}" (id: ${editorID})?`,
-											onOK: async()=>{
-												const newEditors = CloneWithPrototypes(map.editors);
-												newEditors.RemoveAt(index);
-												//new UpdateMapDetails({id: map.id, updates: {editors: newEditors}}).RunOnServer();
-												await RunCommand_UpdateMap({id: map.id, updates: {editors: newEditors}});
-											},
-										});
-									}}/>}
-								</Row>
-							);
-						})}
-					</Column>
-				)}/>
-			</DropDown>
-		);
-	}
-}
+	const Button_Final = SLMode ? Button_SL : Button;
+	const creatorOrMod = PERMISSIONS.Map.Modify(MeID(), map);
+
+	return (
+		<DropDown>
+			<DropDownTrigger><Button_Final ml={5} style={{height: "100%"}} text="People"/></DropDownTrigger>
+			<DropDownContent style={{zIndex: zIndexes.dropdown, position: "fixed", left: 0, width: 500, borderRadius: "0 0 5px 0"}} content={()=>(
+				<Column>
+					<Row center style={{justifyContent: "center"}}>
+						<Text style={{color: "red", fontSize: 13}}>{`Note: Actual access/edit permissions are set by nodes' access-policies.`}</Text>
+						<InfoButton ml={5} text={`
+							The map's "list of editors" is a legacy feature that has no "actual function" atm (other than for reference), though functionality will be added here in the future.
+							Note: You can set the "default access-policy" for new nodes in the map's Details panel.
+						`.AsMultiline(0)}/>
+					</Row>
+					<Row mt={5} center>
+						<Text>Editors:</Text>
+						{creatorOrMod &&
+						<Button ml="auto" text="Add editor" onClick={async()=>{
+							const newEditors = CloneWithPrototypes(map.editors || []);
+							newEditors.push(userIDPlaceholder);
+							await RunCommand_UpdateMap({id: map.id, updates: {editors: newEditors}});
+						}}/>}
+					</Row>
+					{map.editors.map((editorID, index)=>{
+						const editor = editors[index];
+						const displayName = editor?.displayName ?? "n/a";
+						return (
+							<Row key={index} mt={5}>
+								<UserPicker value={editorID} onChange={async val=>{
+									const newEditors = CloneWithPrototypes(map.editors);
+									newEditors[index] = val;
+									await RunCommand_UpdateMap({id: map.id, updates: {editors: newEditors}});
+								}}>
+									<Button enabled={creatorOrMod} text={editorID != userIDPlaceholder ? `${displayName} (id: ${editorID})` : "(click to select user)"} style={{width: "100%"}}/>
+								</UserPicker>
+								{creatorOrMod &&
+								<Button ml={5} text="X" style={{...liveSkin.Style_XButton()}} onClick={()=>{
+									ShowMessageBox({
+										title: `Remove editor "${displayName}"`, cancelButton: true,
+										message: `Remove editor "${displayName}" (id: ${editorID})?`,
+										onOK: async()=>{
+											const newEditors = CloneWithPrototypes(map.editors);
+											newEditors.RemoveAt(index);
+											await RunCommand_UpdateMap({id: map.id, updates: {editors: newEditors}});
+										},
+									});
+								}}/>}
+							</Row>
+						);
+					})}
+				</Column>
+			)}/>
+		</DropDown>
+	);
+});
--- Packages/client/Source/UI/@Shared/Maps/MapUI/ActionBar_Right/ShareDropDown/NewShareUI.tsx
+++ Packages/client/Source/UI/@Shared/Maps/MapUI/ActionBar_Right/ShareDropDown/NewShareUI.tsx
@@ -1,129 +1,108 @@
-import {AddShare, GetMap, GetShares, MeID, Share, ShareType, Timeline, UpdateShare} from "dm_common";
+import {GetMap, GetShares, MeID, Share, ShareType, Timeline} from "dm_common";
 import {store} from "Store";
 import {GetMapView} from "Store/main/maps/mapViews/$mapView.js";
 import {ExpandType, ScrollToType} from "Store/main/shareUI.js";
 import {RunCommand_AddShare, RunCommand_UpdateShare} from "Utils/DB/Command.js";
-import {Observer, RunInAction_Set} from "web-vcore";
-import {CopyText, GetEntries, ToJSON, WaitXThenRun} from "js-vextensions";
+import {RunInAction_Set} from "web-vcore"; import {CopyText, GetEntries, ToJSON, WaitXThenRun} from "js-vextensions";
 import {Button, CheckBox, Column, Pre, Row, RowLR, Select, Text, TextArea, TextInput} from "react-vcomponents";
-import {BaseComponentPlus} from "react-vextensions";
 import {GetShareLongURL, GetShareShortURL} from "../ShareDropDown.js";
+import {observer_mgl} from "mobx-graphlink";
+import React, {useState} from "react";
 
-@Observer
-export class NewShareUI extends BaseComponentPlus({} as {mapID: string}, {timeline: null as Timeline|n, justCopied_type: null as string|n}) {
-	render() {
-		const {mapID} = this.props;
-		const {timeline, justCopied_type} = this.state;
-		const uiState = store.main.shareUI;
-		const userSharesForMap = GetShares(MeID.NN(), mapID);
+export const NewShareUI = observer_mgl(({mapID}: {mapID: string})=>{
+	const [timeline, setTimeline] = useState<Timeline|n>(null);
+	const [justCopied_type, setJustCopied_type] = useState<string|n>(null);
 
-		const currentShare = userSharesForMap.OrderByDescending(a=>a.createdAt)[0];
-		const currentShare_shortURL = GetShareShortURL(currentShare);
-		const currentShare_longURL = GetShareLongURL(currentShare);
+	const uiState = store.main.shareUI;
+	const userSharesForMap = GetShares(MeID.NN(), mapID);
 
-		const map = GetMap.NN(mapID);
-		//const timelines = GetMapTimelines(map);
+	const currentShare = userSharesForMap.OrderByDescending(a=>a.createdAt)[0];
+	const currentShare_shortURL = GetShareShortURL(currentShare);
+	const currentShare_longURL = GetShareLongURL(currentShare);
 
-		const newShareData = new Share({
-			name: map.name,
-			type: ShareType.map,
-			mapID,
-			mapView: GetMapView(mapID),
-		});
-		Object.defineProperty(newShareData, "createdAt", {enumerable: false, value: Date.now()}); // overridden by server, but added for informational convenience, for mere copy-pastes
-		/*if (uiState.expandType == ExpandType.MapDefault) {
-			// todo
-		} else if (uiState.expandType == ExpandType.ToSelectedNode) {
-			// todo
-		}*/
-		const newShareJSON = ToJSON(newShareData, undefined, 2);
-		//const newShare_updatesFromCurrent = GetUpdates(currentShare, newShareData, true).IncludeKeys("mapView");
-		const newShare_updatesFromCurrent = {};
-		if (currentShare) {
-			/*const currentMapView_normalized = WithFirestoreNormalization(currentShare.mapView);
-			const newMapView_normalized = WithFirestoreNormalization(newShareData.mapView);*/
-			const currentMapView_normalized = currentShare.mapView;
-			const newMapView_normalized = newShareData.mapView;
-			/*const mapViewChanges = GetUpdates(currentMapView_normalized, newMapView_normalized, true);
-			if (mapViewChanges.length) {*/
-			if (ToJSON(newMapView_normalized) != ToJSON(currentMapView_normalized)) {
-				newShare_updatesFromCurrent["mapView"] = newShareData.mapView;
-			}
+	const map = GetMap.NN(mapID);
+	const newShareData = new Share({
+		name: map.name,
+		type: ShareType.map,
+		mapID,
+		mapView: GetMapView(mapID),
+	});
+	Object.defineProperty(newShareData, "createdAt", {enumerable: false, value: Date.now()}); // overridden by server, but added for informational convenience, for mere copy-pastes
+
+	const newShareJSON = ToJSON(newShareData, undefined, 2);
+	const newShare_updatesFromCurrent = {};
+	if (currentShare) {
+		const currentMapView_normalized = currentShare.mapView;
+		const newMapView_normalized = newShareData.mapView;
+		if (ToJSON(newMapView_normalized) != ToJSON(currentMapView_normalized)) {
+			newShare_updatesFromCurrent["mapView"] = newShareData.mapView;
 		}
-
-		const splitAt = 120;
-		return (
-			<Column>
-				{/*<RowLR splitAt={splitAt}>
-					<Text>Show timeline:</Text>
-					<Select options={[{name: "None", value: null} as any].concat(timelines)} value={timeline} onChange={val=>this.SetState({timeline: val})}/>
-				</RowLR>*/}
-				<RowLR mt={5} splitAt={splitAt}>
-					<Text>Expand type:</Text>
-					<Select options={GetEntries(ExpandType, "ui")}
-						value={uiState.expandType} onChange={val=>RunInAction_Set(this, ()=>uiState.expandType = val)}/>
-				</RowLR>
-				<RowLR mt={5} splitAt={splitAt}>
-					<Text>Scroll-to type:</Text>
-					<Select options={GetEntries(ScrollToType, "ui")}
-						value={uiState.scrollToType} onChange={val=>RunInAction_Set(this, ()=>uiState.scrollToType = val)}/>
-				</RowLR>
-				<Row mt={5}>
-					<CheckBox text="Show new JSON" value={uiState.showJSON} onChange={val=>RunInAction_Set(this, ()=>uiState.showJSON = val)}/>
-				</Row>
-				{uiState.showJSON &&
-				<Row>
-					<TextArea style={{height: 200, resize: "vertical"}} editable={false} value={newShareJSON}/>
-				</Row>}
-				<Row mt={5}>
-					<Button text="Update current" enabled={currentShare != null && newShare_updatesFromCurrent.VKeys().length > 0} onClick={async()=>{
-						//new UpdateShare({id: currentShare.id, updates: newShare_updatesFromCurrent}).RunOnServer();
-						await RunCommand_UpdateShare({id: currentShare.id, updates: newShare_updatesFromCurrent});
-					}}/>
-					<Button ml={5} text="Create new share" onClick={async()=>{
-						const share = new Share(newShareData);
-						//new AddShare({share}).RunOnServer();
-						await RunCommand_AddShare(share);
-					}}/>
-					<Text ml={5}>Changes: {currentShare == null ? "n/a" : (newShare_updatesFromCurrent.VKeys().length ? newShare_updatesFromCurrent.VKeys().join(", ") : "none")}</Text>
-				</Row>
-
-				{currentShare &&
-				<>
-					<Row mt={15}>
-						<Text>Current share ID: {currentShare?.id ?? "none"}</Text>
-					</Row>
-					<RowLR mt={5} splitAt={80}>
-						<Text>Name:</Text>
-						<TextInput enabled={currentShare != null} style={{flex: 1}} value={currentShare?.name ?? ""} onChange={async val=>{
-							//new UpdateShare({id: currentShare.id, updates: {name: val}}).RunOnServer();
-							await RunCommand_UpdateShare({id: currentShare.id, updates: {name: val}});
-						}}/>
-					</RowLR>
-					<RowLR mt={5} splitAt={80}>
-						<Pre>Short URL: </Pre>
-						<Row style={{width: "100%"}}>
-							<TextInput value={currentShare_shortURL.toString({domain: true})} editable={false} style={{flex: 1}}/>
-							<Button text={justCopied_type == "short" ? "Copied!" : "Copy"} ml={5} enabled={currentShare != null} onClick={()=>{
-								CopyText(currentShare_shortURL.toString({domain: true}));
-								this.SetState({justCopied_type: "short"});
-								WaitXThenRun(1000, ()=>this.SetState({justCopied_type: null}));
-							}}/>
-						</Row>
-					</RowLR>
-					<RowLR mt={5} splitAt={80}>
-						<Pre>Long URL: </Pre>
-						<Row style={{width: "100%"}}>
-							<TextInput value={currentShare_longURL.toString({domain: true})} editable={false} style={{flex: 1}}/>
-							<Button text={justCopied_type == "long" ? "Copied!" : "Copy"} ml={5} enabled={currentShare != null} onClick={()=>{
-								CopyText(currentShare_longURL.toString({domain: true}));
-								this.SetState({justCopied_type: "long"});
-								WaitXThenRun(1000, ()=>this.SetState({justCopied_type: null}));
-							}}/>
-						</Row>
-					</RowLR>
-				</>}
-			</Column>
-		);
 	}
-}
+
+	const splitAt = 120;
+
+	return (
+		<Column>
+			<RowLR mt={5} splitAt={splitAt}>
+				<Text>Expand type:</Text>
+				<Select options={GetEntries(ExpandType, "ui")} value={uiState.expandType} onChange={val=>RunInAction_Set(()=>uiState.expandType = val)}/>
+			</RowLR>
+			<RowLR mt={5} splitAt={splitAt}>
+				<Text>Scroll-to type:</Text>
+				<Select options={GetEntries(ScrollToType, "ui")} value={uiState.scrollToType} onChange={val=>RunInAction_Set(()=>uiState.scrollToType = val)}/>
+			</RowLR>
+			<Row mt={5}>
+				<CheckBox text="Show new JSON" value={uiState.showJSON} onChange={val=>RunInAction_Set(()=>uiState.showJSON = val)}/>
+			</Row>
+			{uiState.showJSON &&
+			<Row>
+				<TextArea style={{height: 200, resize: "vertical"}} editable={false} value={newShareJSON}/>
+			</Row>}
+			<Row mt={5}>
+				<Button text="Update current" enabled={currentShare != null && newShare_updatesFromCurrent.VKeys().length > 0} onClick={async()=>{
+					await RunCommand_UpdateShare({id: currentShare.id, updates: newShare_updatesFromCurrent});
+				}}/>
+				<Button ml={5} text="Create new share" onClick={async()=>{
+					const share = new Share(newShareData);
+					await RunCommand_AddShare(share);
+				}}/>
+				<Text ml={5}>Changes: {currentShare == null ? "n/a" : (newShare_updatesFromCurrent.VKeys().length ? newShare_updatesFromCurrent.VKeys().join(", ") : "none")}</Text>
+			</Row>
+
+			{currentShare &&
+			<>
+				<Row mt={15}>
+					<Text>Current share ID: {currentShare?.id ?? "none"}</Text>
+				</Row>
+				<RowLR mt={5} splitAt={80}>
+					<Text>Name:</Text>
+					<TextInput enabled={currentShare != null} style={{flex: 1}} value={currentShare?.name ?? ""} onChange={async val=>{
+						await RunCommand_UpdateShare({id: currentShare.id, updates: {name: val}});
+					}}/>
+				</RowLR>
+				<RowLR mt={5} splitAt={80}>
+					<Pre>Short URL: </Pre>
+					<Row style={{width: "100%"}}>
+						<TextInput value={currentShare_shortURL.toString({domain: true})} editable={false} style={{flex: 1}}/>
+						<Button text={justCopied_type == "short" ? "Copied!" : "Copy"} ml={5} enabled={currentShare != null} onClick={()=>{
+							CopyText(currentShare_shortURL.toString({domain: true}));
+							setJustCopied_type("short");
+							WaitXThenRun(1000, ()=>setJustCopied_type(null));
+						}}/>
+					</Row>
+				</RowLR>
+				<RowLR mt={5} splitAt={80}>
+					<Pre>Long URL: </Pre>
+					<Row style={{width: "100%"}}>
+						<TextInput value={currentShare_longURL.toString({domain: true})} editable={false} style={{flex: 1}}/>
+						<Button text={justCopied_type == "long" ? "Copied!" : "Copy"} ml={5} enabled={currentShare != null} onClick={()=>{
+							CopyText(currentShare_longURL.toString({domain: true}));
+							setJustCopied_type("long");
+							WaitXThenRun(1000, ()=>setJustCopied_type(null));
+						}}/>
+					</Row>
+				</RowLR>
+			</>}
+		</Column>
+	);
+});
