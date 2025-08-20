66c4090a70285eec4c37d63cc6a486ba067688e3
rishadbaniya <rbdroid01@gmail.com>
2025-08-20 06:41:26 +0545

    fix(Packages/client): extend props with BaseProps to apply BasicStyles in StarsRating

--- Packages/client/Source/Utils/ReactComponents/StarsRating.tsx
+++ Packages/client/Source/Utils/ReactComponents/StarsRating.tsx
@@ -1,13 +1,14 @@
 import {Row, Button, RowProps} from "react-vcomponents";
 import {Range, E} from "js-vextensions";
 import React from "react";
+import {BaseProps, BasicStyles} from "react-vextensions";
 
 export type StarsRating_Props = {
 	value: number,
 	onChange: (value: number)=>any,
 	titleFunc?: (starValue: number)=>string,
 	rightClickAction?: "clear" | ((e: React.MouseEvent)=>any),
-} & RowProps;
+} & RowProps & BaseProps;
 
 export const StarsRating = (props: StarsRating_Props)=>{
 	const {
@@ -20,7 +21,7 @@ export const StarsRating = (props: StarsRating_Props)=>{
 	} = props;
 
 	return (
-		<Row {...rest} style={E({alignItems: "center"}, style)}>
+		<Row {...rest} style={E({alignItems: "center"}, style, BasicStyles(rest))}>
 			{Range(1, 5).map(starValue=>{
 				return (
 					<Button key={starValue} size={20} mdIcon={value >= starValue ? "star" : "star-outline"}
