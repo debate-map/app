d71610f7d58ac6480dae7141a5f1709fe02ab61a
rishadbaniya <rbdroid01@gmail.com>
2025-08-23 10:44:57 +0545

    refactor(Packages/client): convert components of timelines and NodeUI_Menu to functional component

--- Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_SubtreeOps.tsx
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_SubtreeOps.tsx
@@ -1,29 +1,25 @@
 import {liveSkin} from "Utils/Styles/SkinManager.js";
 import {GetNodeDisplayText, HasModPermissions, MeID} from "dm_common";
 import React from "react";
-import {BaseComponentPlus} from "react-vextensions";
 import {VMenuItem} from "react-vmenu";
 import {ShowMessageBox} from "react-vmessagebox";
-import {Observer} from "web-vcore";
 import {MI_SharedProps} from "../NodeUI_Menu.js";
 import {SubtreeOpsDialog} from "./Dialogs/SubtreeOpsDialog.js";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class MI_SubtreeOps extends BaseComponentPlus({} as MI_SharedProps, {}) {
-	render() {
-		const {node, path, map} = this.props;
-		const sharedProps = this.props as MI_SharedProps;
-		if (!HasModPermissions(MeID())) return null; // for now, require mod permissions (since no quotas or other restrictions are in place)
-		return (
-			<VMenuItem text="Export subtree (+other ops)" style={liveSkin.Style_VMenuItem()} onClick={async e=>{
-				if (e.button != 0) return;
-				let ui: SubtreeOpsDialog|n;
-				const controller = ShowMessageBox({
-					title: `Subtree operations, for nodes under: "${GetNodeDisplayText(node, path, map)}"`,
-					okButton: false, buttonBarStyle: {display: "none"},
-					message: ()=><SubtreeOpsDialog ref={c=>ui = c} {...sharedProps} controller={controller}/>,
-				});
-			}}/>
-		);
-	}
-}
+export const MI_SubtreeOps = observer_mgl((props: MI_SharedProps)=>{
+	const {node, path, map} = props;
+	const sharedProps = props as MI_SharedProps;
+	if (!HasModPermissions(MeID())) return null; // for now, require mod permissions (since no quotas or other restrictions are in place)
+	return (
+		<VMenuItem text="Export subtree (+other ops)" style={liveSkin.Style_VMenuItem()} onClick={async e=>{
+			if (e.button != 0) return;
+			let ui: SubtreeOpsDialog|n;
+			const controller = ShowMessageBox({
+				title: `Subtree operations, for nodes under: "${GetNodeDisplayText(node, path, map)}"`,
+				okButton: false, buttonBarStyle: {display: "none"},
+				message: ()=><SubtreeOpsDialog ref={c=>{ui = c}} {...sharedProps} controller={controller}/>,
+			});
+		}}/>
+	);
+});
