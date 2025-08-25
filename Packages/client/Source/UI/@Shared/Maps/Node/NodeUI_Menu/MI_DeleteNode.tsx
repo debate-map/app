9f681c0ae7847a1ab5d9e1268f47035380d9616b
rishadbaniya <rbdroid01@gmail.com>
2025-08-25 12:09:23 +0545

    refactor(Packages/client): convert MI_MoveUpOrDown nd MI_DeleteNode to functional component

--- Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_DeleteNode.tsx
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_DeleteNode.tsx
@@ -1,42 +1,38 @@
-import {BaseComponentPlus} from "react-vextensions";
 import {VMenuItem} from "react-vmenu";
 import {ShowMessageBox} from "react-vmessagebox";
-import {Observer} from "web-vcore";
-import {E} from "js-vextensions";
-import {MeID, GetNodeDisplayText, DeleteNode, ChildGroup, CheckUserCanDeleteNode, NodeType, GetNodeChildren, NodeL2, GetNodeL2, PERMISSIONS} from "dm_common";
+import {MeID, GetNodeDisplayText, CheckUserCanDeleteNode, NodeType, GetNodeChildren, PERMISSIONS} from "dm_common";
 import {liveSkin} from "Utils/Styles/SkinManager.js";
 import {RunCommand_DeleteNode} from "Utils/DB/Command.js";
 import {MI_SharedProps} from "../NodeUI_Menu.js";
+import {observer_mgl} from "mobx-graphlink";
+import React from "react";
 
-@Observer
-export class MI_DeleteNode extends BaseComponentPlus({} as MI_SharedProps, {}) {
-	render() {
-		const {map, mapID, node, path} = this.props;
-		if (!PERMISSIONS.Node.Delete(MeID(), node)) return null;
-		const nodeText = GetNodeDisplayText(node, path, map);
+export const MI_DeleteNode = observer_mgl((props: MI_SharedProps)=>{
+	const {map, mapID, node, path} = props;
+	if (!PERMISSIONS.Node.Delete(MeID(), node)) return null;
+	const nodeText = GetNodeDisplayText(node, path, map);
 
-		const topLevelCommentNodes = GetNodeChildren(node.id).filter(n=>n.type === NodeType.comment);
-		const nodeError = CheckUserCanDeleteNode(MeID(), node, {childrenToIgnore: topLevelCommentNodes.map(n=>n.id)});
+	const topLevelCommentNodes = GetNodeChildren(node.id).filter(n=>n.type === NodeType.comment);
+	const nodeError = CheckUserCanDeleteNode(MeID(), node, {childrenToIgnore: topLevelCommentNodes.map(n=>n.id)});
 
-		return (
-			<VMenuItem text="Delete"
-				//enabled={command.Validate_Safe() == null} title={command.ValidateErrorStr}
-				enabled={nodeError == null} title={nodeError}
-				style={liveSkin.Style_VMenuItem()} onClick={e=>{
-					if (e.button != 0) return;
+	return (
+		<VMenuItem text="Delete"
+			//enabled={command.Validate_Safe() == null} title={command.ValidateErrorStr}
+			enabled={nodeError == null} title={nodeError}
+			style={liveSkin.Style_VMenuItem()} onClick={e=>{
+				if (e.button != 0) return;
 
-					//const contextStr = IsNodeSubnode(node) ? ", and its placement in-layer" : ", and its link with 1 parent";
-					const contextStr = ", and its link with 1 parent";
+				//const contextStr = IsNodeSubnode(node) ? ", and its placement in-layer" : ", and its link with 1 parent";
+				const contextStr = ", and its link with 1 parent";
 
-					ShowMessageBox({
-						title: `Delete "${nodeText}"`, cancelButton: true,
-						message: `Delete the node "${nodeText}"${contextStr}?`,
-						onOK: async()=>{
-							//await command.RunOnServer();
-							await RunCommand_DeleteNode({mapID, nodeID: node.id});
-						},
-					});
-				}}/>
-		);
-	}
-}
+				ShowMessageBox({
+					title: `Delete "${nodeText}"`, cancelButton: true,
+					message: `Delete the node "${nodeText}"${contextStr}?`,
+					onOK: async()=>{
+						//await command.RunOnServer();
+						await RunCommand_DeleteNode({mapID, nodeID: node.id});
+					},
+				});
+			}}/>
+	);
+});
--- Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_MoveUpOrDown.tsx
+++ Packages/client/Source/UI/@Shared/Maps/Node/NodeUI_Menu/MI_MoveUpOrDown.tsx
@@ -1,71 +1,60 @@
-import {ChildGroup, ChildOrdering, GetChildOrdering_Final, GetNodeLinks, GetNodeL2, GetNodeL3, GetParentNodeID, GetParentPath, IsUserCreatorOrMod, MeID, Polarity, UpdateLink, OrderKey, PERMISSIONS} from "dm_common";
+import {ChildGroup, ChildOrdering, GetChildOrdering_Final, GetNodeLinks, GetNodeL2, GetParentNodeID, MeID, Polarity, OrderKey, PERMISSIONS} from "dm_common";
 import React from "react";
 import {store} from "Store";
-import {ImportResource} from "Utils/DataFormats/DataExchangeFormat.js";
 import {RunCommand_UpdateNodeLink} from "Utils/DB/Command.js";
 import {liveSkin} from "Utils/Styles/SkinManager.js";
-import {Observer} from "web-vcore";
-import {BaseComponent} from "react-vextensions";
 import {VMenuItem} from "react-vmenu";
 import {MI_SharedProps} from "../NodeUI_Menu.js";
+import {observer_mgl} from "mobx-graphlink";
 
-@Observer
-export class MI_MoveUpOrDown extends BaseComponent<MI_SharedProps & {direction: "up" | "down"}, {}, ImportResource> {
-	//lastController: BoxController;
-	render() {
-		const {direction, map, node, path} = this.props;
-		const childGroup = node.link?.group;
-		if (map == null || childGroup == null) return null;
+export const MI_MoveUpOrDown = observer_mgl((props: MI_SharedProps & {direction: "up" | "down"})=>{
+	const {direction, map, node, path} = props;
+	const childGroup = node.link?.group;
+	if (map == null || childGroup == null) return null;
 
-		const orderingParentID = GetParentNodeID(path);
-		if (orderingParentID == null) return null;
-		const orderingParent = GetNodeL2(orderingParentID);
-		const orderingParent_childOrdering = orderingParent?.current ? GetChildOrdering_Final(orderingParent, childGroup, map, store.main.maps.childOrdering) : null;
-		const orderingParent_childLinks_ordered = GetNodeLinks(orderingParentID);
-		const ownIndexAmongPeers = orderingParent_childLinks_ordered.findIndex(a=>a.child == node.id);
-		if (ownIndexAmongPeers == -1) return null; // defensive; this shouldn't happen, but if it does, cancel rendering until data resolves properly
+	const orderingParentID = GetParentNodeID(path);
+	if (orderingParentID == null) return null;
+	const orderingParent = GetNodeL2(orderingParentID);
+	const orderingParent_childOrdering = orderingParent?.current ? GetChildOrdering_Final(orderingParent, childGroup, map, store.main.maps.childOrdering) : null;
+	const orderingParent_childLinks_ordered = GetNodeLinks(orderingParentID);
+	const ownIndexAmongPeers = orderingParent_childLinks_ordered.findIndex(a=>a.child == node.id);
+	if (ownIndexAmongPeers == -1) return null; // defensive; this shouldn't happen, but if it does, cancel rendering until data resolves properly
 
-		let towardMin = direction == "up";
-		const directionInDataIsOpposite = node.link?.polarity == Polarity.supporting && node.link?.group != ChildGroup.freeform; // todo: make sure this is correct
-		if (directionInDataIsOpposite) towardMin = !towardMin;
+	let towardMin = direction == "up";
+	const directionInDataIsOpposite = node.link?.polarity == Polarity.supporting && node.link?.group != ChildGroup.freeform; // todo: make sure this is correct
+	if (directionInDataIsOpposite) towardMin = !towardMin;
 
-		// todo: make-so this jumping occurs relative to only the peers in the same rendered-child-group (though might be full list, eg. if "flat" view is used)
+	// todo: make-so this jumping occurs relative to only the peers in the same rendered-child-group (though might be full list, eg. if "flat" view is used)
 
-		const orderKeyToJumpPast = orderingParent_childLinks_ordered[ownIndexAmongPeers + (towardMin ? -1 : 1)]?.orderKey;
-		let newOrderKey: string|n;
-		//let error: string|n;
-		if (orderKeyToJumpPast != null) {
-			const orderKeyToJumpTo = orderingParent_childLinks_ordered[ownIndexAmongPeers + (towardMin ? -2 : 2)]?.orderKey;
-			const jumpPast = new OrderKey(orderKeyToJumpPast);
-			if (orderKeyToJumpTo) {
-				const jumpTo = new OrderKey(orderKeyToJumpTo);
-				if (jumpPast.key == jumpTo.key) {
-					newOrderKey = towardMin ? jumpPast.prev().key : jumpPast.next().key;
-				} else {
-					//newOrderKey = VLexoRank.between(jumpPast.decimal, VLexoRank.parse(orderKeyToJumpTo));
-					newOrderKey = jumpPast.between(new OrderKey(orderKeyToJumpTo)).key;
-				}
-			} else {
+	const orderKeyToJumpPast = orderingParent_childLinks_ordered[ownIndexAmongPeers + (towardMin ? -1 : 1)]?.orderKey;
+	let newOrderKey: string|n;
+	//let error: string|n;
+	if (orderKeyToJumpPast != null) {
+		const orderKeyToJumpTo = orderingParent_childLinks_ordered[ownIndexAmongPeers + (towardMin ? -2 : 2)]?.orderKey;
+		const jumpPast = new OrderKey(orderKeyToJumpPast);
+		if (orderKeyToJumpTo) {
+			const jumpTo = new OrderKey(orderKeyToJumpTo);
+			if (jumpPast.key == jumpTo.key) {
 				newOrderKey = towardMin ? jumpPast.prev().key : jumpPast.next().key;
+			} else {
+				//newOrderKey = VLexoRank.between(jumpPast.decimal, VLexoRank.parse(orderKeyToJumpTo));
+				newOrderKey = jumpPast.between(new OrderKey(orderKeyToJumpTo)).key;
 			}
-			/*} catch (ex) {
-				console.error(ex);
-				error = ex;
-			}*/
+		} else {
+			newOrderKey = towardMin ? jumpPast.prev().key : jumpPast.next().key;
 		}
-
-		return (
-			<VMenuItem text={`Move ${direction}`} style={liveSkin.Style_VMenuItem()}
-				enabled={Boolean(node.link && PERMISSIONS.NodeLink.Modify(MeID(), node.link) && orderingParent_childOrdering == ChildOrdering.manual && newOrderKey != null)}
-				//title={error ? `Error calculating new order-key: ${error}` : undefined}
-				onClick={async e=>{
-					if (e.button != 0) return;
-					/*new UpdateLink({
-						linkID: node.link!.id,
-						linkUpdates: {orderKey: newOrderKey!},
-					}).RunOnServer();*/
-					await RunCommand_UpdateNodeLink({id: node.link!.id, updates: {orderKey: newOrderKey!}});
-				}}/>
-		);
+		/*} catch (ex) {
+			console.error(ex);
+			error = ex;
+		}*/
 	}
-}
+
+	return (
+		<VMenuItem text={`Move ${direction}`} style={liveSkin.Style_VMenuItem()}
+			enabled={Boolean(node.link && PERMISSIONS.NodeLink.Modify(MeID(), node.link) && orderingParent_childOrdering == ChildOrdering.manual && newOrderKey != null)}
+			onClick={async e=>{
+				if (e.button != 0) return;
+				await RunCommand_UpdateNodeLink({id: node.link!.id, updates: {orderKey: newOrderKey!}});
+			}}/>
+	);
+});
