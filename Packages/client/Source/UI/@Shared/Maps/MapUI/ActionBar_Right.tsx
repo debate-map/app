7c9a50e2e920cf8a580036f5d549c68bb894b0c2
rishadbaniya <rbdroid01@gmail.com>
2025-09-01 01:22:59 +0545

    refactor(Packages/client): make use of MapUIEl which is the global ref of MapUI

--- Packages/client/Source/UI/@Shared/Maps/MapUI/ActionBar_Right.tsx
+++ Packages/client/Source/UI/@Shared/Maps/MapUI/ActionBar_Right.tsx
@@ -8,7 +8,7 @@ import {liveSkin} from "Utils/Styles/SkinManager";
 import {GetMapView} from "Store/main/maps/mapViews/$mapView.js";
 import {LayoutDropDown} from "./ActionBar_Right/LayoutDropDown.js";
 import {ShareDropDown} from "./ActionBar_Right/ShareDropDown.js";
-import {MapUI} from "../MapUI.js";
+import {currentMapUI} from "../MapUI.js";
 import {actionBarHeight} from "./ActionBar_Left.js";
 import {observer_mgl} from "mobx-graphlink";
 import React from "react";
@@ -25,27 +25,26 @@ export const ActionBar_Right = observer_mgl((props: Props)=>{
 
 	const changeZoom = (newZoom: number)=>{
 		const oldZoom = mapState.zoomLevel;
-		const mapUI = MapUI.CurrentMapUI;
-		const mapCenter_unzoomed = mapUI?.GetMapCenter_AsUnzoomed(oldZoom);
+		const mapUI = currentMapUI();
+		const mapCenter_unzoomed = mapUI?.getMapCenter_AsUnzoomed(oldZoom);
 		if (mapUI == null || mapCenter_unzoomed == null) return;
 
 		RunInAction_Set(()=>{
 			mapState.zoomLevel = newZoom;
-			mapUI.AdjustMapScrollToPreserveCenterPoint(mapCenter_unzoomed, newZoom);
+			mapUI.adjustMapScrollToPreserveCenterPoint(mapCenter_unzoomed, newZoom);
 
 			// There is a bug in Chrome where if you change the transform-scaling of an element, it doesn't update the "valid scrollbar range" for the scroll-container until something triggers a recalc.
 			// This causes the map's scrolling to be "forced to the left" at some point, which we don't want. (we want the map-center to stay centered)
 			// To fix this, we force a recalc of the "valid scrollbar range", by using the procedure below (see: https://stackoverflow.com/a/41426352), followed by a re-attempt of the center-point restoration.
 			// UPDATE: This procedure *also* causes the node-uis to be "rerasterized" after a zoom-change, which is needed to fix the issue of node-uis being blurry after zooming-in.
 			//         (thus I've removed the workaround in MapUI.tsx, which wasn't great since it reduced scroll performance when many nodes were open)
-			mapUI.ScheduleAfterNextRender(()=>{
-				const mapEl = mapUI.mapUIEl;
-				if (mapEl == null) return;
-				const oldDisplay = mapEl.style.display;
-				mapEl.style.display = "none"; // hides the element
-				mapEl.offsetHeight; // let's the browser "catch up" on the code so it gets redrawn
-				mapEl.style.display = oldDisplay; // shows the element again
-				mapUI.AdjustMapScrollToPreserveCenterPoint(mapCenter_unzoomed, newZoom);
+			mapUI.scheduleAfterNextRender(()=>{
+				if (mapUI == null) return;
+				const oldDisplay = mapUI.style.display;
+				mapUI.style.display = "none"; // hides the element
+				mapUI.offsetHeight; // let's the browser "catch up" on the code so it gets redrawn
+				mapUI.style.display = oldDisplay; // shows the element again
+				mapUI.adjustMapScrollToPreserveCenterPoint(mapCenter_unzoomed, newZoom);
 			});
 		});
 	};
