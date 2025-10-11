97763735f2ee930763feb752a6402dd446ca0951
rishadbaniya <rbdroid01@gmail.com>
2025-10-11 21:59:48 +0545

    fix(Scripts): add quotation on nodeToLabel constraints

--- Scripts/InitDB/Tables/nodeToLabel.sql
+++ Scripts/InitDB/Tables/nodeToLabel.sql
@@ -1,13 +1,11 @@
 -- M:N relationship table between node and label(text)
 CREATE TABLE app."nodeToLabel" (
-	"nodeId" text NOT NULL,
-	"label" text NOT NULL,
-	"createdAt" bigint NOT NULL,
-	"creator" text NOT NULL,
-
-    CONSTRAINT node_to_label_pk PRIMARY KEY (nodeId, label),
-    CONSTRAINT node_to_label_node_fkey
-        FOREIGN KEY (nodeId) REFERENCES app.nodes(id) ON DELETE CASCADE
+    "nodeId" text NOT NULL,
+    "label" text NOT NULL,
+    "createdAt" bigint NOT NULL,
+    "creator" text NOT NULL,
+    CONSTRAINT "node_to_label_pk" PRIMARY KEY ("nodeId", "label"),
+    CONSTRAINT "node_to_label_node_fkey" FOREIGN KEY ("nodeId") REFERENCES app.nodes(id) ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS idx_node_to_label__label ON app."nodeToLabel" ("label");
+CREATE INDEX IF NOT EXISTS "idx_node_to_label__label" ON app."nodeToLabel" ("label");
